87d116debf6c2def42b6dfa9179eeb40dfb24b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:18:49 +0000

    Add package pyrubi/1.4.3/pyrubi-1.4.3-py3-none-any.whl

---
+++ packages/pyrubi/pyrubi-1.4.3-py3-none-any.whl/pyrubi/__init__.py
@@ -0,0 +1,15 @@
+from .methods import methods as Bot
+from .message import message as Message
+from .tools import tools as Tools
+
+library_info = {
+
+    "version": "1.4.3",
+
+    "author": "Ali Ganji zadeh",
+
+    "github": "https://github.com/AliGanji1/pyrubi",
+
+    "rubika": "@pyrubika"
+    
+}
