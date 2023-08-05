5243048bd458aeb7b5d1ad853d0a9b8329b8dc11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:41 +0000

    Add package batframework/0.1.4/batframework-0.1.4.tar.gz

---
+++ packages/batframework/batframework-0.1.4.tar.gz/batframework-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['batframework']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pygame>=2.1.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'batframework',
+    'version': '0.1.4',
+    'description': 'A pygame framework for making games easier',
+    'long_description': '# Example Package\n\nPygame framework for making games easier.\n',
+    'author': 'Baturay',
+    'author_email': 'baturayturan@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
