fa279d7343ec77a7e3cc7285982f25a4e3dcc6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:41 +0000

    Add package PyQtGuiLib/2.3.18.9/PyQtGuiLib-2.3.18.9-py3-none-any.whl

---
+++ packages/PyQtGuiLib/PyQtGuiLib-2.3.18.9-py3-none-any.whl/PyQtGuiLib/tests/test_colorPalette.py
@@ -0,0 +1,38 @@
+from PyQtGuiLib.header import (
+    PYQT_VERSIONS,
+    QApplication,
+    sys,
+    QWidget,
+    QPushButton
+)
+
+'''
+   调色板 测试用例
+'''
+
+from PyQtGuiLib.core import ColorPalette
+
+class Test_ColorPalette(QWidget):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(600,600)
+
+        self.cp = ColorPalette(self)
+        self.cp.setStyleMode(ColorPalette.Style_Black)
+        self.cp.move(10,10)
+        self.cp.clicked.connect(lambda t,c:print(t,c))
+        self.cp.colorNamed.connect(self.test)
+
+    def test(self,rgba):
+        print("-->",self.cp.getRGBA())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Test_ColorPalette()
+    win.show()
+
+    if PYQT_VERSIONS in ["PyQt6","PySide6"]:
+        sys.exit(app.exec())
+    else:
+        sys.exit(app.exec_())
