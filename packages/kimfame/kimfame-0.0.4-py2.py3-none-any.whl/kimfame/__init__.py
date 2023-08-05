2fe9b27e1da10b23bcf33d39a722ffd096c64102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package kimfame/0.0.4/kimfame-0.0.4-py2.py3-none-any.whl

---
+++ packages/kimfame/kimfame-0.0.4-py2.py3-none-any.whl/kimfame/__init__.py
@@ -0,0 +1,13 @@
+from kimfame.aaa import aaa
+from kimfame.bbb import bbb
+from kimfame.ccc import ccc
+from kimfame.ddd import ddd
+from kimfame.kimfame import kimfame
+
+__all__ = [
+    "aaa",
+    "bbb",
+    "ccc",
+    "ddd",
+    "kimfame",
+]
