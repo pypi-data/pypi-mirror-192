09851b83a017c56a28c911feb26f0aaca4a54fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package octoml-profile/0.1.3rc7/octoml_profile-0.1.3rc7.tar.gz

---
+++ packages/octoml-profile/octoml_profile-0.1.3rc7.tar.gz/octoml_profile-0.1.3rc7/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['octoml_profile', 'octoml_profile.interceptors', 'octoml_profile.protos']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'octoml-profile',
+    'version': '0.1.3rc7',
+    'description': 'Client package for OctoML Profile service',
+    'long_description': None,
+    'author': 'Greg Bonik',
+    'author_email': 'gbonik@octoml.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
