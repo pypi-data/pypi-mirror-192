ffa5d6a9615d1a861044ac2ce24a7e442ab22e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:25 +0000

    Add package pymovements/0.1.3/pymovements-0.1.3.tar.gz

---
+++ packages/pymovements/pymovements-0.1.3.tar.gz/pymovements-0.1.3/src/pymovements/utils/checks.py
@@ -0,0 +1,44 @@
+"""
+This module holds basic checks which will be reused in other modules.
+"""
+from typing import Any
+
+import numpy as np
+
+
+def check_no_zeros(variable: Any, name: str = 'variable'):
+    """
+    Check if variable, or if it is iterable, any of its components are zero.
+    """
+    # construct error message first
+    error_message = f'{name} must not be zero'
+
+    # ducktyping check if variable is iterable
+    try:
+        _ = iter(variable)
+
+    # variable is not iterable, simple check for zero
+    except TypeError as exception:
+        if variable == 0:
+            raise ValueError(error_message) from exception
+
+    # variable is iterable, check each element for zero
+    else:
+        error_message = 'each component in ' + error_message
+
+        for variable_component in variable:
+            if variable_component == 0:
+                raise ValueError(error_message)
+
+
+def check_nan_both_channels(arr: np.ndarray):
+    """
+    Checks if all nans occur at the same time steps for both channels.
+    """
+    # sanity check: horizontal and vertical gaze coordinates missing
+    # values at the same time (Eyelink eyetracker never records only
+    # one coordinate)
+    if not np.array_equal(np.isnan(arr[:, 0]), np.isnan(arr[:, 1])):
+        raise ValueError(
+            'nans have to occur at the same steps of horizontal and vertical direction',
+        )
