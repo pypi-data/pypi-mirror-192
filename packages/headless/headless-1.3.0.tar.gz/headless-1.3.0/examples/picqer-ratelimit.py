cc69e9f828d1cb126fd60aed2e4e66751ec32a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:03 +0000

    Add package headless/1.3.0/headless-1.3.0.tar.gz

---
+++ packages/headless/headless-1.3.0.tar.gz/headless-1.3.0/examples/picqer-ratelimit.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import asyncio
+import os
+from typing import Any
+from typing import Awaitable
+
+from headless.ext.picqer import Client
+from headless.ext.picqer import User
+
+
+async def main():
+    params: dict[str, Any]  = {
+        'api_key': os.environ['MOLANO_PICQER_API_KEY'],
+        'api_email': 'test@headless.python.dev.unimatrixone.io',
+        'api_url': 'https://molano.picqer.com/api'
+    }
+    async with Client(**params) as client:
+        requests: list[Awaitable[User]] = []
+        for _ in range(600):
+            requests.append(client.retrieve(User, 13631))
+        await asyncio.gather(*requests)
+
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ packages/headless/headless-1.3.0.tar.gz/headless-1.3.0/headless/types/icredential.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from typing import Any
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .iclient import IClient
+from .iresponse import IResponse
+from .irequest import IRequest
+
+
+class ICredential:
+    """The base class for all HTTP credential implementations."""
+    __module__: str = 'headless.types'
+
+
+    async def add_to_request(self, request: IRequest[Any]) -> None:
+        """Modify a :class:`Request` instance to include this
+        credential.
+        """
+        pass
+
+    async def preprocess_json(
+        self,
+        json: dict[str, Any] | list[Any]
+    ) -> dict[str, Any] | list[Any]:
+        """Preprocess the JSON payload of a request."""
+        return json
+
+    async def send(
+        self,
+        client: 'IClient[Any, Any]',
+        request: IRequest[Any]
+    ) -> IResponse[Any, Any]:
+        response = await client.send(request)
+        return await self.process_response(client, response.request, response)
+
+    async def process_response(
+        self,
+        client: 'IClient[Any, Any]',
+        request: IRequest[Any],
+        response: IResponse[Any, Any]
+    ) -> IResponse[Any, Any]:
+        return response
