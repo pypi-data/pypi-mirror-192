7bbb954014d9a8ae3e0fe2b37a74ddf2c796d900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:43 +0000

    Add package aircraft-list/1.7/aircraft_list-1.7-py3-none-any.whl

---
+++ packages/aircraft-list/aircraft_list-1.7-py3-none-any.whl/aircraft_list/aircraft_list.py
@@ -0,0 +1,17 @@
+import csv
+import pkg_resources
+
+def split_string(stringa):
+    return stringa.split(',')
+
+def aircraft_models():
+    with pkg_resources.resource_stream(__name__, 'aircraft_model_list.csv') as f:
+        reader = csv.reader(f, delimiter=';')
+        aircrafts = []
+        for row in reader:
+            aircrafts.append(row)
+        print(aircrafts)
+        data = [dict(zip(['manufacturer', 'model', 'icao', 'type', 'engine',
+                     'engine_number', 'wake'], split_string(sublist[0] for sublist in aircrafts)))]
+        return data
+
