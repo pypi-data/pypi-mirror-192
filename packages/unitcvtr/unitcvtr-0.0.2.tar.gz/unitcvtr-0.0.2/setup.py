7bce7ebaf0fc8d24e2709c5ae9cb425f3bdd34a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:31 +0000

    Add package unitcvtr/0.0.2/unitcvtr-0.0.2.tar.gz

---
+++ packages/unitcvtr/unitcvtr-0.0.2.tar.gz/unitcvtr-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'package to convert units'
+LONG_DESCRIPTION = 'package to convert units long version'
+
+# Setting up
+setup(
+    name="unitcvtr",
+    version=VERSION,
+    author="UndefinedRekian (Rekian Dewandaru)",
+    author_email="<rekian.fajar@aqi.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'unit', 'math', 'convert'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
