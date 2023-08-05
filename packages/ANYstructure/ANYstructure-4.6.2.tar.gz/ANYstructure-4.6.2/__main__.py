8d4c6dfee6b894ea99cc6db61be8e417d3d1ea26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:00 +0000

    Add package ANYstructure/4.6.2/ANYstructure-4.6.2.tar.gz

---
+++ packages/ANYstructure/ANYstructure-4.6.2.tar.gz/ANYstructure-4.6.2/__main__.py
@@ -0,0 +1,29 @@
+import sys, multiprocessing, ctypes
+import tkinter as tk
+try:
+    from main_application import Application
+except ModuleNotFoundError:
+    from ANYstructure.main_application import Application
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    multiprocessing.freeze_support()
+    errorCode = ctypes.windll.shcore.SetProcessDpiAwareness(2)
+    root = tk.Tk()
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
+    root.geometry(f'{width}x{height}')
+    my_app = Application(root)
+    root.mainloop()
+
+    # Do argument parsing here (eg. with argparse) and anything else
+    # you want your project to do.
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ImportError:
+        ANYstructure.main()
