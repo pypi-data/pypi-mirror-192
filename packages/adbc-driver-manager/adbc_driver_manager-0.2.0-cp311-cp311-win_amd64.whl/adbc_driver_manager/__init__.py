e3ce06acefd161df837714cb62f32d9df561eeeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:09 +0000

    Add package adbc-driver-manager/0.2.0/adbc_driver_manager-0.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/adbc-driver-manager/adbc_driver_manager-0.2.0-cp311-cp311-win_amd64.whl/adbc_driver_manager/__init__.py
@@ -0,0 +1,94 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Low-level ADBC bindings for Python.
+
+The root module provides a fairly direct, 1:1 mapping to the C API
+definitions in Python.  For a higher-level interface, use
+:mod:`adbc_driver_manager.dbapi`.  (This requires PyArrow.)
+"""
+
+
+# start delvewheel patch
+def _delvewheel_init_patch_1_2_0():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'adbc_driver_manager.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    if not is_pyinstaller or os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_init_patch_1_2_0()
+del _delvewheel_init_patch_1_2_0
+# end delvewheel patch
+
+
+
+from ._lib import (
+    INGEST_OPTION_MODE,
+    INGEST_OPTION_MODE_APPEND,
+    INGEST_OPTION_MODE_CREATE,
+    INGEST_OPTION_TARGET_TABLE,
+    AdbcConnection,
+    AdbcDatabase,
+    AdbcInfoCode,
+    AdbcStatement,
+    AdbcStatusCode,
+    ArrowArrayHandle,
+    ArrowArrayStreamHandle,
+    ArrowSchemaHandle,
+    DatabaseError,
+    DataError,
+    Error,
+    GetObjectsDepth,
+    IntegrityError,
+    InterfaceError,
+    InternalError,
+    NotSupportedError,
+    OperationalError,
+    ProgrammingError,
+    Warning,
+)
+from ._version import __version__
+
+__all__ = [
+    "__version__",
+    "INGEST_OPTION_MODE",
+    "INGEST_OPTION_MODE_APPEND",
+    "INGEST_OPTION_MODE_CREATE",
+    "INGEST_OPTION_TARGET_TABLE",
+    "AdbcConnection",
+    "AdbcDatabase",
+    "AdbcInfoCode",
+    "AdbcStatement",
+    "AdbcStatusCode",
+    "ArrowArrayHandle",
+    "ArrowArrayStreamHandle",
+    "ArrowSchemaHandle",
+    "DatabaseError",
+    "DataError",
+    "Error",
+    "GetObjectsDepth",
+    "IntegrityError",
+    "InterfaceError",
+    "InternalError",
+    "NotSupportedError",
+    "OperationalError",
+    "ProgrammingError",
+    "Warning",
+]
