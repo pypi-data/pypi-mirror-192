2050b1af99914e494a0ea7935b662750b44c74a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:13 +0000

    Add package dojo-ds/0.1.0/dojo_ds-0.1.0.tar.gz

---
+++ packages/dojo-ds/dojo_ds-0.1.0.tar.gz/dojo_ds-0.1.0/dojo_ds/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for dojo-ds."""
+
+__author__ = """James Irving"""
+__email__ = 'jirving@codingdojo.com'
+__version__ = '0.1.0'
+
+from . import model_insights
+from . import data_enrichment
+from . import utils
+
+from dojo_ds import *
