93b9772b8fab00bfe4263443b7e7f92184efdaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:50 +0000

    Add package gopro-sync/0.0.11/gopro_sync-0.0.11-py3-none-any.whl

---
+++ packages/gopro-sync/gopro_sync-0.0.11-py3-none-any.whl/gopro_sync/utils/arguments.py
@@ -0,0 +1,46 @@
+from argparse import ArgumentParser
+
+
+def argparsing():
+    parser = ArgumentParser(
+        prog="gopro-sync",
+        description="Syncronize your GoPro videos easly.",
+        usage="gopro-sync PROJECT [-i IN_FOLDER] [-o OUT_FOLDER]",
+        epilog="Example: gopro-sync Summer23 -o Videos --no-lrv --save-thm",
+    )
+    parser.add_argument("name", type=str, help="project name", metavar=("PROJECT"))
+    parser.add_argument(
+        "-i",
+        type=str,
+        help="input folder. Default: current folder",
+        metavar=("IN_FOLDER"),
+    )
+    parser.add_argument(
+        "-o",
+        type=str,
+        help="output folder. Default: current folder",
+        metavar=("OUT_FOLDER"),
+    )
+    parser.add_argument(
+        "--no-lrv",
+        action="store_true",
+        help="do NOT save .LRV (Low Resolution Video) files",
+    )
+    parser.add_argument(
+        "--save-thm",
+        action="store_true",
+        help="save .THM (video thumbnails) files",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="test run to check if everything is correct",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="script version",
+        action="version",
+        version="gopro-sync v0.0.11",
+    )
+    return parser.parse_args()
