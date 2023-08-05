a7b9048bebc66d530f5486785161b1d9f02e0427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:02 +0000

    Add package types-DateTimeRange/2.0.0.1/types-DateTimeRange-2.0.0.1.tar.gz

---
+++ packages/types-DateTimeRange/types-DateTimeRange-2.0.0.1.tar.gz/types-DateTimeRange-2.0.0.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-DateTimeRange"
+description = "Typing stubs for DateTimeRange"
+long_description = '''
+## Typing stubs for DateTimeRange
+
+This is a PEP 561 type stub package for the `DateTimeRange` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`DateTimeRange`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/DateTimeRange. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `7180d0223b3cfb0275bdc4dff902439536dd4e3d`.
+'''.lstrip()
+
+setup(name=name,
+      version="2.0.0.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/DateTimeRange.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-python-dateutil'],
+      packages=['datetimerange-stubs'],
+      package_data={'datetimerange-stubs': ['__init__.pyi', '__version__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
