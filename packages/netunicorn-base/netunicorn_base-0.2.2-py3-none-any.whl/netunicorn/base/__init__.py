3c1773af632672838f46b97da8448da148c0d3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package netunicorn-base/0.2.2/netunicorn_base-0.2.2-py3-none-any.whl

---
+++ packages/netunicorn-base/netunicorn_base-0.2.2-py3-none-any.whl/netunicorn/base/__init__.py
@@ -0,0 +1,6 @@
+from returns.result import Result, Success, Failure
+from returns.pipeline import is_successful
+
+from .experiment import Experiment
+from .pipeline import Pipeline
+from .task import Task, TaskDispatcher
