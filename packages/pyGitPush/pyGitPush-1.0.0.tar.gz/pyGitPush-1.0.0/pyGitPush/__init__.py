25aa7330547912e77256820f6d3635f07438b8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package pyGitPush/1.0.0/pyGitPush-1.0.0.tar.gz

---
+++ packages/pyGitPush/pyGitPush-1.0.0.tar.gz/pyGitPush-1.0.0/pyGitPush/__init__.py
@@ -0,0 +1,17 @@
+
+#############################################################################################################################
+#   filename:pygit.py                                                       
+#   created: 2023-02-15                                                              
+#   import your librarys below                                                    
+#############################################################################################################################
+
+
+
+from .pyGitPush import gitPublish
+
+#key_acess = "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"
+#user_name = 'batestin1'
+#local_remote = 'C:/Users/Bates/Documents/Repositorios/batata'
+#title = "batataPython"
+
+#pygit(key_acess, user_name, local_remote, title, description=None, public=True)
