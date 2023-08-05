c42c542efd2be638fcfdd2f36b684e6e3ce0d736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:32 +0000

    Add package devopspipelinepoc/0.1.5/devopspipelinepoc-0.1.5.tar.gz

---
+++ packages/devopspipelinepoc/devopspipelinepoc-0.1.5.tar.gz/devopspipelinepoc-0.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+  
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+  
+long_description = 'POC in command line packages using python'
+  
+setup(
+        name ='devopspipelinepoc',
+        version ='0.1.5',
+        author ='Puneet Udhayan',
+        author_email ='puneetudhayan@gmail.com',
+        url ='https://github.com/PuneetUdhayanGraphene/devops-pipeline-poc',
+        description ='POC in command line packages using python',
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        packages = find_packages(),
+        entry_points ={
+            'console_scripts': [
+                'devopspipelinepoc = devopspipelinepoc.main:app'
+            ]
+        },
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords ='python package',
+        install_requires = requirements,
+        package_data={'devopspipelinepoc': ['data/*.txt']},
+        zip_safe = False
+)
