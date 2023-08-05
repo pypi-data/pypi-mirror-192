69ca1a2843ec4e25feb043d89d24dc44d71ec013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:09 +0000

    Add package neo3-boa/0.12.1/neo3_boa-0.12.1-py3-none-any.whl

---
+++ packages/neo3-boa/neo3_boa-0.12.1-py3-none-any.whl/boa3/model/builtin/interop/oracle/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['OracleResponseCodeType',
+           'OracleClass',
+           ]
+
+from boa3.model.builtin.interop.oracle.oracleresponsecodetype import OracleResponseCodeType
+from boa3.model.builtin.native.oracleclass import OracleClass
---
+++ packages/neo3-boa/neo3_boa-0.12.1-py3-none-any.whl/boa3/neo3/contracts/__init__.py
@@ -0,0 +1,6 @@
+from boa3.neo3.contracts.contracttypes import CallFlags, TriggerType
+from boa3.neo3.contracts.findoptions import FindOptions
+
+__all__ = ['CallFlags',
+           'FindOptions',
+           'TriggerType']
