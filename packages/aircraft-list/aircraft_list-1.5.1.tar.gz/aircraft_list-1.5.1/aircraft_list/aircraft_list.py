996d57c4f2a4340ff311e06ea06f1acbfbb8e9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:13 +0000

    Add package aircraft-list/1.5.1/aircraft_list-1.5.1.tar.gz

---
+++ packages/aircraft-list/aircraft_list-1.5.1.tar.gz/aircraft_list-1.5.1/aircraft_list/aircraft_list.py
@@ -0,0 +1,21 @@
+import csv
+
+def split_string(stringa):
+    return stringa.split(',')
+
+def aircraft_models():
+    import csv
+    import os
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'aircraft_model_list.csv')
+    
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f, delimiter=';')
+        aircrafts = []
+        for row in reader:
+            aircrafts.append(row)
+        print(aircrafts)
+        data = [dict(zip(['manufacturer', 'model', 'icao', 'type', 'engine',
+                     'engine_number', 'wake'], split_string(sublist[0] for sublist in aircrafts)))]
+        return data
+
