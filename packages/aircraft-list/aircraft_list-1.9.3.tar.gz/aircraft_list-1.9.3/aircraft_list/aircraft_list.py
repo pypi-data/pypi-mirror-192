65f247f0a15ca08f350e4d3407b38648b1956ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:29 +0000

    Add package aircraft-list/1.9.3/aircraft_list-1.9.3.tar.gz

---
+++ packages/aircraft-list/aircraft_list-1.9.3.tar.gz/aircraft_list-1.9.3/aircraft_list/aircraft_list.py
@@ -0,0 +1,25 @@
+import csv
+
+def split_string(stringa):
+    return stringa.split(',')
+
+def aircraft_models():
+    import pkg_resources
+    data_file = pkg_resources.resource_filename(__name__, 'aircraft_model_list.csv')
+    with open(data_file, 'r') as f:
+        # reader = csv.reader(f, delimiter=';')
+        # aircrafts = []
+        # for row in reader:
+        #     aircrafts.append(row)
+        # data = [dict(zip(['manufacturer', 'model', 'icao', 'type', 'engine',
+        #              'engine_number', 'wake'], split_string(sublist[0]))) for sublist in aircrafts]
+        # return data
+        reader = csv.reader(f, delimiter=';')
+        aircrafts = []
+        for row in reader:
+            aircrafts.append(row)
+        data = []
+        for sublist in aircrafts:
+            if len(sublist) == 7:
+                data.append(dict(zip(['manufacturer', 'model', 'icao', 'type', 'engine', 'engine_number', 'wake'], split_string(sublist[0]))))
+        return data
