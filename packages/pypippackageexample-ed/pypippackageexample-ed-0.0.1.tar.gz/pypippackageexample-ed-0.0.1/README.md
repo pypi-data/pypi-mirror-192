52aaebe85ab22800fdbeef7e3e333d8e68d20409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:48 +0000

    Add package pypippackageexample-ed/0.0.1/pypippackageexample-ed-0.0.1.tar.gz

---
+++ packages/pypippackageexample-ed/pypippackageexample-ed-0.0.1.tar.gz/pypippackageexample-ed-0.0.1/README.md
@@ -0,0 +1,3 @@
+# py-pip-package-example
+
+Este es mi primer package de ejemplo en python
---
+++ packages/pypippackageexample-ed/pypippackageexample-ed-0.0.1.tar.gz/pypippackageexample-ed-0.0.1/economypackage/mathseconomy/mymathsUtils.py
@@ -0,0 +1,6 @@
+
+def sumaSimple(num, num2):
+  return num + num2
+
+def restSimple(num, num2):
+  return num - num2
