d9aec94fe1bd352e6a5d44be03f5ff2b10bd8efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:18 +0000

    Add package Scopul/0.0.5/scopul-0.0.5-py3-none-any.whl

---
+++ packages/Scopul/scopul-0.0.5-py3-none-any.whl/Scopul/__init__.py
@@ -0,0 +1,7 @@
+from Scopul.scopul import Scopul
+from Scopul.scopul_exception import InvalidFileFormatError, InvalidMusicElementError
+from Scopul.TimeSignature import TimeSignature
+from Scopul.Tempo import Tempo
+from Scopul.Sequence import Part, Note, Rest, Chord
+from Scopul.conversions import note_to_number, number_to_note
+from Scopul.config_musescore import config_musescore
