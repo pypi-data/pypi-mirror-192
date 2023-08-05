75e4ee8b2af97b7cbe2eef8abfd28a43e9a8dcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:29 +0000

    Add package cnnftsp/2.0.1/cnnftsp-2.0.1.tar.gz

---
+++ packages/cnnftsp/cnnftsp-2.0.1.tar.gz/cnnftsp-2.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cnnftsp',
+      version='2.0.1',
+      description='CNN for TSP',
+      author='Sikorski Philipp',
+      author_email='philipp.sikorski@uni-jena.de',
+      license='MIT',
+      packages=['cnnfortsp'],
+      zip_safe=False)
