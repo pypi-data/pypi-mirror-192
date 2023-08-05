2a29b1e313cf93fbc112cea61bf6abd3fd7bd18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:39 +0000

    Add package dummy-notebookutils/1.5.1/dummy_notebookutils-1.5.1-py3-none-any.whl

---
+++ packages/dummy-notebookutils/dummy_notebookutils-1.5.1-py3-none-any.whl/notebookutils/mssparkutils/credentials.py
@@ -0,0 +1,30 @@
+
+def getToken(audience, name=''):
+    return ""
+
+def getConnectionStringOrCreds(linkedService):
+    return ""
+
+def getFullConnectionString(linkedService):
+    return ""
+
+def getPropertiesAll(linkedService):
+    return ""
+
+def getSecret(akvName, secret, linkedService=''):
+    return ""
+
+def getSecretWithLS(linkedService, secret):
+    return ""
+
+def putSecret(akvName, secretName, secretValue, linkedService=''):
+    return ""
+
+def putSecretWithLS(linkedService, secretName, secretValue):
+    return ""
+
+def isValidToken(token):
+    return False
+
+def help():
+    pass
---
+++ packages/dummy-notebookutils/dummy_notebookutils-1.5.1-py3-none-any.whl/notebookutils/mssparkutils/notebook.py
@@ -0,0 +1,12 @@
+
+def updateNBSEndpoint(endpoint):
+    pass
+
+def help(method_name=None):
+    pass
+
+def run(path, timeout_seconds=90, arguments={}):
+    return ""
+
+def exit(value):
+    pass
