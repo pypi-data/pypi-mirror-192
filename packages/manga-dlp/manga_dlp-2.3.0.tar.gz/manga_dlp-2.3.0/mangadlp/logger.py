b1a4538e679bbe93a381980bc78d9f333e2a5337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:40 +0000

    Add package manga-dlp/2.3.0/manga_dlp-2.3.0.tar.gz

---
+++ packages/manga-dlp/manga_dlp-2.3.0.tar.gz/manga_dlp-2.3.0/mangadlp/logger.py
@@ -0,0 +1,46 @@
+import logging
+import sys
+
+from loguru import logger
+
+LOGURU_FMT = "{time:%Y-%m-%dT%H:%M:%S%z} | <level>[{level: <7}]</level> [{name: <10}] [{function: <20}]: {message}"
+
+
+# from loguru docs
+class InterceptHandler(logging.Handler):
+    """
+    Intercept python logging messages and log them via loguru.logger
+    """
+
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+# init logger with format and log level
+def prepare_logger(loglevel: int = 20) -> None:
+    config: dict = {
+        "handlers": [
+            {
+                "sink": sys.stdout,
+                "level": loglevel,
+                "format": LOGURU_FMT,
+            }
+        ],
+    }
+
+    logging.basicConfig(handlers=[InterceptHandler()], level=loglevel)
+    logger.configure(**config)
