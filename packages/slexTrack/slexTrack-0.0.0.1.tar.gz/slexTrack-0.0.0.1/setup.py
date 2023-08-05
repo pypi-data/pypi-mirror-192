6c785bdc2dc24b3c49ed29c6830881ccd8519c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:45 +0000

    Add package slexTrack/0.0.0.1/slexTrack-0.0.0.1.tar.gz

---
+++ packages/slexTrack/slexTrack-0.0.0.1.tar.gz/slexTrack-0.0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='slexTrack',
+      version='0.0.0.1',
+      description='Test package',
+      author='Jiangyan Yang',
+      packages=['slexTrack'],
+     zip_safe = False)
---
+++ packages/slexTrack/slexTrack-0.0.0.1.tar.gz/slexTrack-0.0.0.1/slexTrack/track.py
@@ -0,0 +1,8 @@
+class tracking:
+
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def addition(self):
+        return self.a+self.b
