44390fadbd6d8613d3590749d2a535e21e035353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:43 +0000

    Add package NeuralNMF/0.0.11/NeuralNMF-0.0.11.tar.gz

---
+++ packages/NeuralNMF/NeuralNMF-0.0.11.tar.gz/NeuralNMF-0.0.11/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='NeuralNMF',
+    version='0.0.11',
+    license='MIT',
+    author="Joshua Vendrow",
+    author_email='jvendrow@ucla.edu',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/jvendrow/NeuralNMF',
+    keywords='neural nmf',
+    install_requires=[
+          'torch',
+          'matplotlib',
+          'scipy',
+          'numpy',
+          'fnnls',
+          'tqdm',
+      ],
+
+)
