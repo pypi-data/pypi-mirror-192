bf32b93be4782aa23dfcf28e33e761cf3d620e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:23 +0000

    Add package robin-sd-upload/0.2.1/robin_sd_upload-0.2.1.tar.gz

---
+++ packages/robin-sd-upload/robin_sd_upload-0.2.1.tar.gz/robin_sd_upload-0.2.1/tests/test.py
@@ -0,0 +1,5 @@
+#To run your unit tests, you can use the unittest command-line interface. 
+# From the root directory of your package, you can run the following command: python -m unittest discover
+#This will discover and run all of the unit tests in your package. If you want to run a specific test file, you can use the following command:
+#python -m unittest tests.test_module_1
+#This will run the unit tests in the test_module_1.py file.
