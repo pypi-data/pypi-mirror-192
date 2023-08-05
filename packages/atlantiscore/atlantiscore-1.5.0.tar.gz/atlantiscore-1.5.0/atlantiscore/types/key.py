5378213f9fc4a6b77deae78e8727057c04b795ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:47 +0000

    Add package atlantiscore/1.5.0/atlantiscore-1.5.0.tar.gz

---
+++ packages/atlantiscore/atlantiscore-1.5.0.tar.gz/atlantiscore-1.5.0/atlantiscore/types/key.py
@@ -0,0 +1,12 @@
+from atlantiscore.types.evm import EVMAddress
+from coincurve import PrivateKey as CoinCurvePrivateKey
+from eth_utils import keccak
+
+NUMBER_OF_BYTES_IN_ADDRESS = 20
+
+
+class PrivateKey(CoinCurvePrivateKey):
+    @property
+    def public_address(self) -> EVMAddress:
+        public_key = self.public_key.format(compressed=False)[1:]
+        return EVMAddress(keccak(public_key)[-NUMBER_OF_BYTES_IN_ADDRESS:])
