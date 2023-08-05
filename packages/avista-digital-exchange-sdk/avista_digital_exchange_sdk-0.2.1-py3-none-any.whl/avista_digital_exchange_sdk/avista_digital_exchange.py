7ec288e3cc03ee69bb95ae3c72161173ebcee548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:19 +0000

    Add package avista-digital-exchange-sdk/0.2.1/avista_digital_exchange_sdk-0.2.1-py3-none-any.whl

---
+++ packages/avista-digital-exchange-sdk/avista_digital_exchange_sdk-0.2.1-py3-none-any.whl/avista_digital_exchange_sdk/avista_digital_exchange.py
@@ -0,0 +1,48 @@
+import os
+from .exceptions import *
+from .iotUtil import *
+from .dataStoreUtil import *
+from .collaborativeUtil import *
+
+from .data_types.user import User
+from .graphql_queries.user_getUserSession import user_getUserSession
+from .client import Client
+from . import globals
+
+
+# from .graphql_queries.storage_getDataStoreFileDownloadUrl import storage_getDataStoreFileDownloadUrl
+
+class AvistaDigitalExchange(object):
+
+    def __init__(self, token, debug=False):
+        if type(debug) is not bool:
+            raise InvalidParameterException(
+                "AvistaDigitalExchange debug parameter must be a bool")
+        self._stage = "PRODUCTION"
+        self._debug = debug
+        self._token = token
+        self._client = Client(self._token, self._stage, self._debug)
+        self.iot = IoTUtil(self._debug, self._client)
+        # self.collaboratives = CollaborativeUtil(self._debug, self._client)
+        self.dataStores = DataStoreUtil(self._debug, self._client)
+        print("SDK initialized!")
+
+    def getUserInfo(self) -> User:
+        """Retrieves user information associated with the authentication token."""
+        query = user_getUserSession(self._client, self._debug)
+        result = query.performQuery()
+        return result
+
+
+def isValidISO8601Timestamp(timestamp):
+    import re
+
+    matched = re.match("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z", timestamp)
+    isMatch = bool(matched)
+
+    return isMatch
+    # try:
+    #     datetime.fromisoformat(timestamp.replace('Z', '+00:00'))
+    # except:
+    #     return False
+    # return True
