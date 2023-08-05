8ae42f1168434b2cdb5d2accc7194190cf007333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:06 +0000

    Add package physicool/0.2.4/physicool-0.2.4-py3-none-any.whl

---
+++ packages/physicool/physicool-0.2.4-py3-none-any.whl/physicool/optimization.py
@@ -0,0 +1,301 @@
+"""A module for model calibration and optimization routines."""
+from dataclasses import dataclass, field
+from pathlib import Path
+import platform
+import subprocess
+import logging
+from typing import List, Dict, Optional, Tuple, Union
+from distutils.dir_util import copy_tree, remove_tree
+
+import numpy as np
+
+from physicool.updaters import ParamsUpdater
+from physicool.processing import (
+    OutputProcessor,
+    ErrorQuantification,
+    compute_mean_squared_error,
+    NEW_OUTPUTS_VERSION,
+)
+from physicool.plotting import SweeperPlot
+
+
+LOG_FILE = "debug.log"
+logging.basicConfig(
+    level=logging.WARNING,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[logging.FileHandler(LOG_FILE), logging.StreamHandler()],
+)
+
+
+def _create_project_command(project_name: str) -> str:
+    """
+    Creates a project command based on the current OS.
+
+    Parameters
+    -----------
+    project_name:
+        The base name of the PhysiCell executable file.
+
+    Returns
+    --------
+    str
+        The full command to be called in order to run the
+        executable in the shell, adapted to the current OS.
+    """
+    if platform.system() == "Windows":
+        return f"{project_name}.exe"
+    return f"./{project_name}"
+
+
+def clean_outputs() -> None:
+    """Removes the files from the output folder and creates it again (make data-cleanup)."""
+    if Path("output").is_dir():
+        with open(LOG_FILE, "a") as log_file:
+            subprocess.run(
+                "make data-cleanup",
+                shell=True,
+                stdout=log_file,
+                stderr=log_file,
+                text=True,
+            )
+
+
+def clean_tmp_files() -> None:
+    """Removes the temp folder if it exists."""
+    if Path("temp").is_dir():
+        remove_tree("temp")
+
+
+def compile_project() -> None:
+    """Compiles the current project by calling make."""
+    logging.info("compiling project...")
+    with open(LOG_FILE, "a") as log_file:
+        subprocess.run("make", shell=True, stdout=log_file, stderr=log_file, text=True)
+
+
+@dataclass
+class PhysiCellBlackBox:
+    """
+    A class to run PhysiCell models through Python as a black box.
+    Models can be run as they are, and it's possible to run replicates.
+
+    Alternatively, users can decide to include a ParamsUpdater object to
+    change values in the XML file before the model is run and/or a
+    OutputProcessor to extract data from the output files and return a given
+    user-defined metric.
+
+    Output files can be kept or discarded. If kept, they will be stored
+    inside a new "temp" folder.
+    """
+
+    updater: Optional[ParamsUpdater] = None
+    processor: Optional[OutputProcessor] = None
+    project_name: str = "project"
+    project_command: str = field(init=False)
+    version: str = NEW_OUTPUTS_VERSION
+
+    def __post_init__(self):
+        """Create the right command to call the PhysiCell project based on the OS."""
+        self.project_command = _create_project_command(self.project_name)
+
+    def run(
+        self,
+        params: Optional[Dict[str, float]] = None,
+        number_of_replicates: int = 1,
+        keep_files: bool = True,
+    ) -> Optional[np.ndarray]:
+        """
+        Runs the black box pipeline.
+
+        Parameters
+        ----------
+        params
+            The new parameter values, to be updated in the XML file
+            by the ParamsUpdater class.
+        number_of_replicates
+            The number of simulation replicates to be run.
+        keep_files
+            If the output files should be stored in a tmp folder.
+
+        Returns
+        -------
+        Optional[np.ndarray]
+            The output metrics computed by the OutputProcessor class
+        """
+        # Create a new directory to store the output files
+        if keep_files:
+            storage_folder = "temp"
+            Path(storage_folder).mkdir(exist_ok=True)
+
+        # Update the XML configuration file with the passed values
+        if (self.updater is not None) and (params is not None):
+            self.updater.update(new_values=params)
+
+        # Create an array to store the metrics computed by the processor
+        if self.processor:
+            output_metrics = []
+
+        # Run the PhysiCell model for each replicate
+        # Create a new directory, run the model and save the files to this
+        # location and compute and store the model output metrics
+        for i in range(number_of_replicates):
+            if (number_of_replicates > 1) & keep_files:
+                storage_folder = f"temp/replicate{i}"
+                Path(storage_folder).mkdir()
+
+            log_status = f"running project with command {self.project_command}..."
+            logging.info(log_status)
+
+            with open(LOG_FILE, "a") as log_file:
+                subprocess.run(
+                    self.project_command,
+                    shell=True,
+                    stdout=log_file,
+                    stderr=subprocess.PIPE,
+                )
+
+            if self.processor:
+                output_metrics.append(self.processor(version=self.version))
+
+            if keep_files:
+                copy_tree("output", storage_folder)
+
+        # Delete the files from the "output" folder
+        clean_outputs()
+
+        if self.processor:
+            if number_of_replicates == 1:
+                return output_metrics[0]
+
+            return np.asarray(output_metrics)
+
+
+def run_sweep(
+    black_box: PhysiCellBlackBox, name: str, bounds: Tuple[float, float], step: float
+) -> np.ndarray:
+    input_values = np.arange(bounds[0], bounds[1], step)
+    output_metrics = []
+    for value in input_values:
+        output_metrics.append(black_box.run({name: value}))
+
+    return np.asarray(output_metrics)
+
+
+@dataclass
+class MultiLevelSweep:
+    black_box: PhysiCellBlackBox
+    target_data: np.ndarray
+    n_levels: int
+    points_dir: int
+    percentage_dir: float
+    parameters: List[str]
+    error_estimator: ErrorQuantification = compute_mean_squared_error
+    plotter: SweeperPlot = field(init=False)
+    results: np.ndarray = field(init=False)
+    current_level: int = field(init=False)
+    current_opt_point: Tuple[float, float] = field(init=False)
+    param_bounds: List[Tuple[Union[None, float], Union[None, float]]] = field(
+        init=False
+    )
+
+    def __post_init__(self) -> None:
+        """Sets up everything needed for the sweeper."""
+        self.results = np.zeros((self.n_levels, self.points_dir, self.points_dir))
+        self.current_level = 0
+        self.current_opt_point = (0.0, 0.0)
+        self.param_bounds = [(None, None), (None, None)]
+        self.plotter = SweeperPlot()
+
+    def set_param_bounds(
+        self,
+        param1_bounds: Optional[Tuple[Union[None, float], Union[None, float]]] = None,
+        param2_bounds: Optional[Tuple[Union[None, float], Union[None, float]]] = None,
+    ) -> None:
+        """Defines the parameter bounds to be considered when creating a parameter space."""
+        if param1_bounds is None:
+            param1_bounds = (None, None)
+
+        if param2_bounds is None:
+            param2_bounds = (None, None)
+
+        self.param_bounds = [param1_bounds, param2_bounds]
+
+    def get_parameter_range(self, factor: float, idx: int) -> np.ndarray:
+        """Returns the parameter values to be tested based on the current level and bounds."""
+        min_limit = self.current_opt_point[idx] - factor * self.current_opt_point[idx]
+        max_limit = self.current_opt_point[idx] + factor * self.current_opt_point[idx]
+
+        if any(self.param_bounds[idx]):
+            if self.param_bounds[idx][0] is not None:
+                if min_limit < self.param_bounds[idx][0]:
+                    min_limit = self.param_bounds[idx][0]
+
+            if self.param_bounds[idx][1] is not None:
+                if max_limit > self.param_bounds[idx][1]:
+                    max_limit = self.param_bounds[idx][1]
+
+        return np.linspace(min_limit, max_limit, self.points_dir)
+
+    def get_parameter_space(self):
+        """Returns the parameter values to be tested at the current level."""
+        factor = self.percentage_dir / (self.current_level * 2 + 1)
+        x = self.get_parameter_range(factor=factor, idx=0)
+        y = self.get_parameter_range(factor=factor, idx=1)
+        return x, y
+
+    def get_optimal_idx(self) -> Tuple[int, int]:
+        """Returns the indexes for the smallest error in the current level."""
+        best_result = np.argmin(self.results[self.current_level])
+        i = int(np.floor(best_result / self.points_dir))
+        j = int(best_result - self.points_dir * i)
+
+        return i, j
+
+    def compute_objective(self, x: np.ndarray, y: np.ndarray) -> None:
+        """
+        Runs the black box for each cell of the parameter space defined by x and y.
+        Also chooses the best optimal point found in the parameter space.
+        """
+        for i, x_value in enumerate(x):
+            for j, y_value in enumerate(y):
+                clean_tmp_files()
+                # Select parameters and run the model
+                results = self.black_box.run(
+                    {self.parameters[0]: x_value, self.parameters[1]: y_value}
+                )
+
+                # Compute error between simulated data and target data
+                self.results[self.current_level][i][j] = self.error_estimator(
+                    results, self.target_data
+                )
+
+        i, j = self.get_optimal_idx()
+        self.current_opt_point = (x[i], y[j])
+
+    def run_level(self) -> None:
+        """Run a single level of the multilevel sweep."""
+        # Find the new parameter space for the current level
+        x, y = self.get_parameter_space()
+        # Add the bounds of the parameter space to the plot
+        self.plotter.add_bounds_to_ax(x, y, self.current_level)
+
+        # RUn the model for each cell of the parameter space and save the results
+        self.compute_objective(x, y)
+        # Plot the results as a heatmap
+        self.plotter.plot_level_results(
+            x, y, self.current_level, self.results[self.current_level]
+        )
+
+    def run_sweep(self, initial_point: Tuple[float, float]) -> Tuple[float, float]:
+        """Runs all sweep levels from an initial value and returns the best values found."""
+        self.current_opt_point = initial_point
+        self.plotter.set_up_plotter(
+            n_levels=self.n_levels, param_labels=self.parameters
+        )
+
+        while self.current_level < self.n_levels:
+            self.run_level()
+            self.current_level += 1
+
+        return self.current_opt_point[0], self.current_opt_point[1]
---
+++ packages/physicool/physicool-0.2.4-py3-none-any.whl/physicool/processing.py
@@ -0,0 +1,358 @@
+"""A module to process output PhysiCell files and extract metrics from the data."""
+from pathlib import Path
+from typing import Callable, Union, List, Tuple
+from xml.etree import ElementTree
+
+import numpy as np
+import pandas as pd
+from scipy import io as sio
+
+NEW_OUTPUTS_VERSION = "1.10.3"
+CELL_OUTPUT_LABELS = [
+    "ID",
+    "position_x",
+    "position_y",
+    "position_z",
+    "total_volume",
+    "cell_type",
+    "cycle_model",
+    "current_phase",
+    "elapsed_time_in_phase",
+    "nuclear_volume",
+    "cytoplasmic_volume",
+    "fluid_fraction",
+    "calcified_fraction",
+    "orientation_x",
+    "orientation_y",
+    "orientation_z",
+    "polarity",
+    "migration_speed",
+    "motility_vector_x",
+    "motility_vector_y",
+    "motility_vector_z",
+    "migration_bias",
+    "motility_bias_direction_x",
+    "motility_bias_direction_y",
+    "motility_bias_direction_z",
+    "persistence_time",
+    "motility_reserved",
+]
+
+
+class Microenvironment:
+    def __init__(self, time_point: int, path: Union[Path, str]):
+        self.storage = path
+
+        self.time = time_point
+        self.substances = self.get_substances()
+        self.mesh = self.get_mesh()
+        self.mesh_shape = (len(self.mesh[1]), len(self.mesh[0]))
+
+        self.data = self.get_data()
+
+    def get_substances(self):
+        """Returns a list of the substances stored in the XML output files."""
+
+        # Open the first XML file to get list of stored substances
+        tree = ElementTree.parse(self.storage / "output00000000.xml")
+        root = tree.getroot()
+        var_node = root.find("microenvironment/domain/variables")
+        var_children = var_node.findall("variable")
+        variables = [var.get("name") for var in var_children]
+
+        return variables
+
+    def get_mesh(self):
+        """Returns a list with the coordinates of the microenvironment mesh."""
+
+        # Open the first XML file to get list of stored substances
+        tree = ElementTree.parse(self.storage / "output00000000.xml")
+        root = tree.getroot()
+        mesh_node = root.find("microenvironment/domain/mesh")
+
+        # Get x, y and z coordinates
+        # X coordinates
+        coord_str = mesh_node.find("x_coordinates").text
+        delimiter = mesh_node.find("x_coordinates").get("delimiter")
+        x_coords = np.array(coord_str.split(delimiter), dtype=float)
+        # Y coordinates
+        coord_str = mesh_node.find("y_coordinates").text
+        delimiter = mesh_node.find("y_coordinates").get("delimiter")
+        y_coords = np.array(coord_str.split(delimiter), dtype=float)
+        # Z coordinates
+        coord_str = mesh_node.find("z_coordinates").text
+        delimiter = mesh_node.find("z_coordinates").get("delimiter")
+        z_coords = np.array(coord_str.split(delimiter), dtype=float)
+
+        return [x_coords, y_coords, z_coords]
+
+    def get_substance_data(self, substance):
+        """Returns an array with the substance concentrations for all the planes of the domain."""
+
+        timestep = str(self.time).zfill(8)
+
+        me_file = self.storage / "output{}_microenvironment0.mat".format(timestep)
+
+        # Load substance data
+        me_data = sio.loadmat(me_file)["multiscale_microenvironment"]
+
+        # Select the data corresponding to the chosen substance
+        substance_index = self.substances.index(substance)
+        substance_data = np.array(
+            [
+                np.reshape(
+                    me_data[substance_index + 4, me_data[2, :] == z_level],
+                    self.mesh_shape,
+                )
+                for z_level in self.mesh[2]
+            ]
+        )
+
+        return substance_data
+
+    def get_data(self):
+        """Returns a dictionary with the data for all the substances in the simulation."""
+
+        me_data = {
+            substance: self.get_substance_data(substance)
+            for substance in self.substances
+        }
+
+        return me_data
+
+
+def read_mat_file_cells(path: str, variables: List[str]) -> pd.DataFrame:
+    """Loads the data from the output mat files into a Pandas DataFrame."""
+    # Make sure that the variables can be found in the file
+    if any([var not in CELL_OUTPUT_LABELS for var in variables]):
+        raise ValueError("The passed variables are not valid names.")
+
+    cell_data = sio.loadmat(path)["cells"]
+    # Select and save the variables of interest
+    variables_indexes = [CELL_OUTPUT_LABELS.index(var) for var in variables]
+    cells = pd.DataFrame.from_dict(
+        {var: cell_data[index, :] for var, index in zip(variables, variables_indexes)}
+    )
+
+    return cells
+
+
+def read_physicell_version() -> str:
+    """Reads the PhysiCell version number from VERSION.TXT."""
+    with open("VERSION.TXT", "r") as file:
+        return file.read()
+
+
+def convert_version_str_to_tuple(version: str) -> Tuple[int]:
+    """Converts a string with the version number to a tuple to ease version comparison."""
+    return tuple([int(x) for x in version.split(".")])
+
+
+def check_version_status(version: str) -> bool:
+    """Compares the passed version to the first version with the output*_cells.mat format."""
+    return convert_version_str_to_tuple(version) >= convert_version_str_to_tuple(
+        NEW_OUTPUTS_VERSION
+    )
+
+
+def get_cell_file_name(version: str) -> str:
+    """Returns the expected file name for cell files based on PhysiCell version number."""
+    if check_version_status(version):
+        return "output{}_cells.mat"
+    return "output{}_cells_physicell.mat"
+
+
+def get_cell_file_num(output_path: Path, version: str) -> str:
+    pattern = (
+        "output*_cells.mat"
+        if check_version_status(version)
+        else "output*_cells_physicell.mat"
+    )
+    return len([file for file in output_path.glob(pattern)])
+
+
+def get_cell_data(
+    timestep: int,
+    variables: List[str],
+    output_path: Union[str, Path] = Path("output"),
+    version: str = NEW_OUTPUTS_VERSION,
+) -> pd.DataFrame:
+    """
+    Reads the PhysiCell output data into a Pandas DataFrame.
+
+    Parameters
+    ----------
+    timestep
+        The time point to be read.
+    variables
+        The variables to be extracted from the file.
+    output_path
+        The path to where the output files can be found.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame with the passed variables for every cell.
+    """
+    # Create path name
+    if isinstance(output_path, str):
+        output_path = Path(output_path)
+
+    time_str = str(timestep).zfill(8)
+    file_stem = get_cell_file_name(version=version)
+    file_name = file_stem.format(time_str)
+    path_name = output_path / file_name
+
+    # Make sure that the timestep has been recorded and saved
+    if path_name not in output_path.glob(file_stem.format("*")):
+        raise ValueError("The passed time point does not match any file.")
+
+    # Read output file into a DataFrame
+    # (changing Path to a string with its absolute path. loadmat takes strings as input)
+    cells = read_mat_file_cells(
+        path=path_name.absolute().as_posix(), variables=variables
+    )
+
+    cells["timestep"] = timestep
+
+    return cells
+
+
+def get_cells_in_z_slice(data: pd.DataFrame, size: float) -> pd.DataFrame:
+    """
+    Returns the cells inside a z-axis slice and returns them.
+    The slice will be centered at 0 and have the passed size.
+
+    Parameters
+    ----------
+    data
+        A DataFrame with the cells' coordinates
+        (must contain a column called "position_z").
+    size
+        The size of the z-slice.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame that only contains the cells inside the slice.
+    """
+    if "position_z" not in data.columns:
+        raise ValueError("The DataFrame doesn't include the cells' z coordinates.")
+
+    return data[
+        (data["position_z"] >= -size / 2) & (data["position_z"] <= size / 2)
+    ].copy()
+
+
+def get_cell_trajectories(
+    output_path: Union[str, Path], version: str = NEW_OUTPUTS_VERSION
+):
+    """
+    Reads the PhysiCell output data into a Pandas DataFrame.
+
+    Parameters
+    ----------
+    output_path
+        The path to where the output files can be found.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame with the passed variables for every cell.
+    """
+    # Create path name
+    if isinstance(output_path, str):
+        output_path = Path(output_path)
+
+    variables = ["ID", "position_x", "position_y", "position_z"]
+    data = []
+    number_of_timepoints = get_cell_file_num(output_path=output_path, version=version)
+
+    for i in range(number_of_timepoints):
+        cells = get_cell_data(
+            timestep=i, variables=variables, output_path=output_path, version=version
+        )
+        cells["timestep"] = i
+        data.append(cells)
+
+    new_data = pd.concat(data)
+    trajectories = [
+        new_data[new_data["ID"] == cell_id][["position_x", "position_y", "position_z"]]
+        for cell_id in new_data["ID"].unique()
+    ]
+
+    return trajectories
+
+
+OutputProcessor = Callable[[Path, str], Union[float, np.ndarray]]
+
+
+def get_cell_numbers_over_time(
+    output_path: Path = Path("output"), version: str = NEW_OUTPUTS_VERSION
+) -> np.ndarray:
+    """
+    Returns the number of cells over time (one value for each simulation time point).
+
+    Parameters
+    ----------
+    output_path
+        The path to where the output files can be found.
+
+    Returns
+    -------
+    np.ndarray
+        An array with the number of cells at every simulation time point.
+    """
+    if isinstance(output_path, str):
+        output_path = Path(output_path)
+
+    number_of_timepoints = get_cell_file_num(output_path=output_path, version=version)
+    number_of_cells = np.empty(shape=(number_of_timepoints,))
+
+    for i in range(number_of_timepoints):
+        cells = get_cell_data(
+            timestep=i, variables=["ID"], output_path=output_path, version=version
+        )
+        number_of_cells[i] = cells["ID"].size
+
+    return number_of_cells
+
+
+def get_final_y_position(
+    output_path: Path = Path("output"), version: str = NEW_OUTPUTS_VERSION
+) -> np.ndarray:
+    """
+    Returns the number of cells over time (one value for each simulation time point).
+
+    Parameters
+    ----------
+    output_path
+        The path to where the output files can be found.
+
+    Returns
+    -------
+    np.ndarray
+        An array with the number of cells at every simulation time point.
+    """
+    if isinstance(output_path, str):
+        output_path = Path(output_path)
+
+    last_point = get_cell_file_num(output_path=output_path, version=version)
+    cells = get_cell_data(
+        timestep=last_point - 1,
+        variables=["position_y"],
+        output_path=output_path,
+        version=version,
+    )
+
+    return cells["position_y"].values
+
+
+ErrorQuantification = Callable[[np.ndarray, np.ndarray], float]
+
+
+def compute_mean_squared_error(
+    model_data: np.ndarray, reference_data: np.ndarray
+) -> float:
+    """Returns the mean squared error value between the reference and simulated datasets."""
+    return ((model_data - reference_data) ** 2).sum()
