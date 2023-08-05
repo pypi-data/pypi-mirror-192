770546da790441156bf724f000dbc721946a1d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:17:38 +0000

    Add package ScopeSim-Templates/0.4.3/ScopeSim_Templates-0.4.3.tar.gz

---
+++ packages/ScopeSim-Templates/ScopeSim_Templates-0.4.3.tar.gz/ScopeSim_Templates-0.4.3/scopesim_templates/version.py
@@ -0,0 +1,15 @@
+version = '0.4.3'
+date    = '2023-02-15 17:00:00 GMT'
+yaml_descriptions = """
+- version : 0.4.3
+  date : 2023-02-15
+  comment : Add pinhole mask and minor bugfixes
+
+- version : 0.4.2
+  date : 2022-07-12
+  comment : Updated DLC server to point to scopesim.univie.ac.at
+
+- version : 0.4.1
+  date : 2022-04-22
+  comment : Updated package structure
+"""
