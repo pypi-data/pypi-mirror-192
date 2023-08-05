473893aacd2fe6fc4d366f63d4b0d43073e6d5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:50 +0000

    Add package heflow/1.1.0/heflow-1.1.0.tar.gz

---
+++ packages/heflow/heflow-1.1.0.tar.gz/heflow-1.1.0/heflow/__init__.py
@@ -0,0 +1,43 @@
+import mlflow.pyfunc
+
+
+def load_model(model_uri: str):
+    return mlflow.pyfunc.load_model(model_uri).unwrap_python_model().unwrap()
+
+
+def log_model(model):
+
+    class PythonModel(mlflow.pyfunc.PythonModel):
+
+        def __init__(self, model):
+            self.model = model
+
+        def predict(self, context, model_input):
+            if isinstance(model_input, tuple):
+                return self.model.__infer__(*model_input)
+            else:
+                return self.model.__infer__(model_input)
+
+        def unwrap(self):
+            return self.model
+
+    return mlflow.pyfunc.log_model('model', python_model=PythonModel(model))
+
+
+def save_model(path, model):
+
+    class PythonModel(mlflow.pyfunc.PythonModel):
+
+        def __init__(self, model):
+            self.model = model
+
+        def predict(self, context, model_input):
+            if isinstance(model_input, tuple):
+                return self.model.__infer__(*model_input)
+            else:
+                return self.model.__infer__(model_input)
+
+        def unwrap(self):
+            return self.model
+
+    return mlflow.pyfunc.save_model(path, python_model=PythonModel(model))
