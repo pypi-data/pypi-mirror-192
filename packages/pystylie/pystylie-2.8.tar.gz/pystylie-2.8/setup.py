842dc07c4b970cab126658fa99af331abe2fb83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:27 +0000

    Add package pystylie/2.8/pystylie-2.8.tar.gz

---
+++ packages/pystylie/pystylie-2.8.tar.gz/pystylie-2.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+setup(
+    name="pystylie",
+    version='2.8',
+    license='Eclipse Public License 2.0',
+    authors=["billtheGoat356", "loTus01", "BlueRed"],
+    author_email="<billythegoat356@gmail.com>",
+    description="by billythegoat356, loTus01 and BlueRed",
+    long_description='Documentation: https://github.com/billythegoat356/',
+    packages=['pystylie']
+)
+
+
