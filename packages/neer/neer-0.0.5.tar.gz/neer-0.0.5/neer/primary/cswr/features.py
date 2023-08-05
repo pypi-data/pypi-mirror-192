9b585c4e76b9f924960ef2aec5cc6b768d78921e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:04 +0000

    Add package neer/0.0.5/neer-0.0.5.tar.gz

---
+++ packages/neer/neer-0.0.5.tar.gz/neer-0.0.5/neer/primary/cswr/features.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Sequence
+from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, UUID, TIMESTAMP, TIME, JSONB
+from sqlalchemy.types import VARCHAR, BIGINT, DATE, INTEGER, NUMERIC, BOOLEAN, TEXT
+from base import PrimaryBase
+
+class Features(PrimaryBase):
+    __tablename__ = 'features'
+    __table_args__ = {'schema': 'cswr'}
+    
+    id = Column(UUID, primary_key=True, autoincrement=False)
+    fid = Column(INTEGER, nullable=False, autoincrement=False)
+    service = Column(TEXT, nullable=False, autoincrement=False)
+    layer = Column(TEXT, nullable=False, autoincrement=False)
+    geometry = Column(TEXT, autoincrement=False)
+    properties = Column(JSONB, autoincrement=False)
+    created_at = Column(TIMESTAMP, nullable=False, autoincrement=False)
+    category = Column(VARCHAR(19), autoincrement=False)
