83245aab3fe7d098f905e899ddf4b72c781d1356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:19 +0000

    Add package scaled/0.20/scaled-0.20-py3-none-any.whl

---
+++ packages/scaled/scaled-0.20-py3-none-any.whl/scaled/entry_points/scheduler.py
@@ -0,0 +1,74 @@
+import argparse
+import asyncio
+import signal
+import threading
+
+from scaled.io.config import (
+    DEFAULT_FUNCTION_RETENTION_SECONDS,
+    DEFAULT_PER_WORKER_QUEUE_SIZE,
+    DEFAULT_WORKER_TIMEOUT_SECONDS,
+)
+from scaled.scheduler.main import Scheduler
+from scaled.utility.event_loop import EventLoopType, register_event_loop
+from scaled.utility.zmq_config import ZMQConfig
+from scaled.utility.logging.utility import setup_logger
+
+stop_event = threading.Event()
+
+
+def get_args():
+    parser = argparse.ArgumentParser("scaled scheduler", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "--worker-timeout-seconds",
+        "-wt",
+        type=int,
+        default=DEFAULT_WORKER_TIMEOUT_SECONDS,
+        help="discard worker when timeout seconds reached",
+    )
+    parser.add_argument(
+        "--function-retention-seconds",
+        "-ft",
+        type=int,
+        default=DEFAULT_FUNCTION_RETENTION_SECONDS,
+        help="discard function in scheduler when timeout seconds " "reached",
+    )
+    parser.add_argument(
+        "--per-worker-queue-size",
+        "-qs",
+        type=int,
+        default=DEFAULT_PER_WORKER_QUEUE_SIZE,
+        help="specify per worker queue size",
+    )
+    parser.add_argument(
+        "--event-loop", "-e", default="builtin", choices=EventLoopType.allowed_types(), help="select event loop type"
+    )
+    parser.add_argument("address", type=ZMQConfig.from_string, help="scheduler address to connect to")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    setup_logger()
+
+    __register_signal()
+
+    scheduler = Scheduler(
+        address=args.address,
+        stop_event=stop_event,
+        per_worker_queue_size=args.per_worker_queue_size,
+        worker_timeout_seconds=args.worker_timeout_seconds,
+        function_retention_seconds=args.function_retention_seconds,
+    )
+    register_event_loop(args.event_loop)
+    asyncio.run(scheduler.loop())
+
+
+def __register_signal():
+    signal.signal(signal.SIGINT, __handle_signal)
+    signal.signal(signal.SIGTERM, __handle_signal)
+
+
+def __handle_signal(*args):
+    assert args is not None
+    stop_event.set()
