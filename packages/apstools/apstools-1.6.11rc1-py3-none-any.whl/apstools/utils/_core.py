d16f8584a3f88d37c48c9ef0c9152bb57c5d6fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:19:21 +0000

    Add package apstools/1.6.11rc1/apstools-1.6.11rc1-py3-none-any.whl

---
+++ packages/apstools/apstools-1.6.11rc1-py3-none-any.whl/apstools/utils/_core.py
@@ -0,0 +1,29 @@
+import databroker._drivers.mongo_normalized
+import databroker._drivers.msgpack
+import intake
+
+FIRST_DATA = "1995-01-01"
+LAST_DATA = "2100-12-31"
+
+CATALOG_CLASSES = (
+    databroker.Broker,
+    databroker._drivers.mongo_normalized.BlueskyMongoCatalog,
+    databroker._drivers.msgpack.BlueskyMsgpackCatalog,
+    intake.Catalog,
+)
+MONGO_CATALOG_CLASSES = (
+    databroker.Broker,
+    databroker._drivers.mongo_normalized.BlueskyMongoCatalog,
+    # intake.Catalog,
+)
+MAX_EPICS_STRINGOUT_LENGTH = 40
+
+# -----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     jemian@anl.gov
+# :copyright: (c) 2017-2023, UChicago Argonne, LLC
+#
+# Distributed under the terms of the Argonne National Laboratory Open Source License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
