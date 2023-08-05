d620e1e0cf5f86ad9a433e7ca1c65c856d960835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:00 +0000

    Add package use-case-registry/1.1.0/use_case_registry-1.1.0.tar.gz

---
+++ packages/use-case-registry/use_case_registry-1.1.0.tar.gz/use_case_registry-1.1.0/use_case_registry/base/__init__.py
@@ -0,0 +1,5 @@
+from .command import ICommand
+
+__all__ = [
+    "ICommand",
+]
---
+++ packages/use-case-registry/use_case_registry-1.1.0.tar.gz/use_case_registry-1.1.0/use_case_registry/errors.py
@@ -0,0 +1,5 @@
+"""Custom defined errors."""
+
+
+class CommandInputValidationError(Exception):
+    """Raised when command input values does pass validation check."""
