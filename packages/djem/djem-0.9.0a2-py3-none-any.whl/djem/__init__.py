c71ea065b87ef29a1ba6cb6c833b761b74e1252c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:39 +0000

    Add package djem/0.9.0a2/djem-0.9.0a2-py3-none-any.whl

---
+++ packages/djem/djem-0.9.0a2-py3-none-any.whl/djem/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '0.9.0a2'
+
+# Shortcuts to quick-use utils
+from djem.utils.inspect import pp  # NOQA
+from djem.utils.mon import M, Mon, mon  # NOQA
+
+
+class Undefined:
+    
+    def __bool__(self):
+        
+        return False
+    
+    def __str__(self):
+        
+        return '<undefined>'
+
+
+UNDEFINED = Undefined()
