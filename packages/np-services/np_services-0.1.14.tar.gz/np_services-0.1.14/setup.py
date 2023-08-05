bb9285cd765d04af310f175a1699ca59489b6528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:06 +0000

    Add package np-services/0.1.14/np_services-0.1.14.tar.gz

---
+++ packages/np-services/np_services-0.1.14.tar.gz/np_services-0.1.14/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['np_services']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['backoff',
+ 'fabric>=2.7,<3.0',
+ 'np_session>=0.2',
+ 'pydantic>=1.10,<2.0',
+ 'pyzmq',
+ 'requests>=2,<3']
+
+setup_kwargs = {
+    'name': 'np-services',
+    'version': '0.1.14',
+    'description': 'Tools for interfacing with devices and services used in Mindscope Neuropixels experiments at the Allen Institute.',
+    'long_description': '# service usage\n![Services](./services.drawio.svg)',
+    'author': 'bjhardcastle',
+    'author_email': 'ben.hardcastle@alleninstitute.org',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
