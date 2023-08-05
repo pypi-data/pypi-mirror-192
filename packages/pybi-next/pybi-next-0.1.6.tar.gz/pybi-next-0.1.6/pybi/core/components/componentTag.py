a9ffceb020af7f13ea34e3dca899e6372b113002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:06 +0000

    Add package pybi-next/0.1.6/pybi-next-0.1.6.tar.gz

---
+++ packages/pybi-next/pybi-next-0.1.6.tar.gz/pybi-next-0.1.6/pybi/core/components/componentTag.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ComponentTag(Enum):
+    App = "App"
+    DataSource = "DataSource"
+    Slicer = "Slicer"
+    Table = "Table"
+    EChart = "EChart"
+    Box = "Box"
+    Text = "Text"
+    Upload = "Upload"
+    TextValue = "TextValue"
+    ColBox = "ColBox"
+    FlowBox = "FlowBox"
+    GridBox = "GridBox"
