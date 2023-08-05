48f44329165a588714135e9a4cfceb81d2b24e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:33 +0000

    Add package bloock/2.4.0/bloock-2.4.0-cp38-cp38-win_amd64.whl

---
+++ packages/bloock/bloock-2.4.0-cp38-cp38-win_amd64.whl/bloock/_config/config.py
@@ -0,0 +1,20 @@
+from bloock._bridge.proto.config_pb2 import ConfigData, Configuration
+import bloock
+
+
+class Config:
+    @staticmethod
+    def default() -> ConfigData:
+        return ConfigData(
+            config=Configuration(
+                library_name="Python",
+                api_key=bloock.api_key,
+                host=bloock.api_host,
+                disable_analytics=bloock.disable_analytics,
+            ),
+            networks_config=bloock.network_config,
+        )
+
+    @staticmethod
+    def new(config_data: ConfigData) -> ConfigData:
+        return config_data
