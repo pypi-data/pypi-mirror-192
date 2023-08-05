f8a537206693b7f607c8e248f790490b5b68c7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:34 +0000

    Add package robin-sd-upload/0.2.16/robin_sd_upload-0.2.16.tar.gz

---
+++ packages/robin-sd-upload/robin_sd_upload-0.2.16.tar.gz/robin_sd_upload-0.2.16/robin_sd_upload/supportive_scripts/version_checker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pypi_simple
+import sys
+
+from robin_sd_upload.supportive_scripts import logger
+from robin_sd_upload.supportive_scripts import yaml_parser
+from robin_sd_upload import _version
+
+def check_latest_version():
+    """Check if the latest version is installed and document how to update if needed."""
+    config = yaml_parser.parse_config()
+
+    # package_name = config['static']['app_name']
+    package_name = "robin_sd_upload"
+    current_version = _version.__version__
+    
+    latest_version = pypi_simple.package(package_name).version
+
+    if current_version == latest_version:
+        logger.log(message="Latest version detected, proceeding.", log_level="info", to_file=True, to_terminal=False)
+    else:
+        logger.log(message="Newer version detected, please update.", log_level="error", to_file=True, to_terminal=True)
+        logger.log(message="Current version: " + current_version, log_level="error", to_file=True, to_terminal=True)
+        logger.log(message="Latest version: " + latest_version, log_level="error", to_file=True, to_terminal=True)
+        logger.log(message="To update, run: pip3 install --upgrade " + package_name, log_level="error", to_file=True, to_terminal=True)
+        sys.exit(1)
