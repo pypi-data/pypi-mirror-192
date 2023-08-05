62f617104fa4dd0c2f22bfae375cdfe378ccacf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:58 +0000

    Add package dmprj/0.2.11/dmprj-0.2.11-py3-none-any.whl

---
+++ packages/dmprj/dmprj-0.2.11-py3-none-any.whl/dmprj/concept/behavioral/base.py
@@ -0,0 +1,45 @@
+# -*- python -*-
+#
+# Copyright 2021, 2022, 2023 Xingeng Chen
+# Copyright 2018, 2019, 2020, 2021 Liang Chen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# dmprj.concept.behavioral.base
+
+
+from dmprj.base.messages import MSG_NOT_IMPLEMENTED
+
+
+class Action(object):
+    '''
+    '''
+
+    def do(self):
+        '''
+        NOTE: template method
+        '''
+        self.prologue()
+        self.main_play()
+        self.epilogue()
+        return None
+
+    def prologue(self):
+        return NotImplementedError(MSG_NOT_IMPLEMENTED)
+
+    def main_play(self):
+        return NotImplementedError(MSG_NOT_IMPLEMENTED)
+
+    def epilogue(self):
+        return NotImplementedError(MSG_NOT_IMPLEMENTED)
