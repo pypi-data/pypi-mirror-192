bff56f1527bcddeb94d20dc3667557452efb793c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package bioflex/1.1.1/bioflex-1.1.1.tar.gz

---
+++ packages/bioflex/bioflex-1.1.1.tar.gz/bioflex-1.1.1/bioflex/_study.py
@@ -0,0 +1,37 @@
+from typing import List
+from ._database import _Database
+from ._base_object import _BaseObject
+
+
+class _Study(_BaseObject):
+	database: _Database
+	id: int
+	hash_id: str
+	title: str
+	authors: List[str]
+	reference: str
+
+	def __init__(self,
+			database: _Database,
+			response: dict,
+		):
+		super().__init__(database)
+
+		self.database = database
+		self.id = response['id']
+		self.title = response['title']
+		self.authors = response['authors']
+		self.reference = response['reference']
+
+
+	def __repr__(self):
+		return 'Study(id="{}",title="{}",reference="{}")'.format(
+			self.id, self.title, self.reference
+		)
+
+
+	def _post_params(self, params: dict):
+		return {
+			'study_idx': self.id,
+			**params,
+		}
---
+++ packages/bioflex/bioflex-1.1.1.tar.gz/bioflex-1.1.1/bioflex/constants.py
@@ -0,0 +1,11 @@
+NUMBER = 0
+CATEGORY = 1
+METADATA_TYPES = ['Numeric', 'Category']
+
+UNIT_RAW = 'raw'
+UNIT_LOGNORM = 'lognorm'
+
+EXPORT_H5AD = 'h5ad'
+EXPORT_SEURAT = 'seurat'
+
+CHUNK_SIZE = 1024 * 1024 # 1MB
