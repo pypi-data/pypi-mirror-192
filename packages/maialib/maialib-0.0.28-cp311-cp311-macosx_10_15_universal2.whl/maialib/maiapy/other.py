a86e9960a4565ad0609bc6c52b53188e140f308f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:57 +0000

    Add package maialib/0.0.28/maialib-0.0.28-cp311-cp311-macosx_10_15_universal2.whl

---
+++ packages/maialib/maialib-0.0.28-cp311-cp311-macosx_10_15_universal2.whl/maialib/maiapy/other.py
@@ -0,0 +1,45 @@
+from importlib import resources
+
+__all__ = ["getSampleScorePath"]
+
+
+def getSampleScorePath(sampleName: str) -> str:
+    """Get a maialib internal XML sample file
+
+    Args:
+       sampleName (str):  Maialib pre-defined XML sample name
+           - "Bach-Solo"
+           - "Beethoven-Orchestra"
+
+    Kwargs:
+       None
+
+    Returns:
+       A full file path (str) to the XML maialib internal sample
+
+    Raises:
+       RuntimeError
+
+    Examples of use:
+
+    >>> import maialib as ml
+    >>> filePath = ml.getSampleScorePath("Beethoven-Orchestra")
+    >>> score = ml.Score(filePath)
+    >>> score.info()
+    """
+    # ===== INPUT VALIDATION ===== #
+    validInputs = ["Bach-Solo", "Beethoven-Orchestra"]
+
+    if sampleName not in validInputs:
+        raise RuntimeError(
+            f"Invalid sample name: {sampleName}.\nThe valid keywords are: {validInputs}")
+
+    # Get the actual XML file name for the given 'alias'
+    xmlFileName = {
+        "Bach-Solo": "cello_suite_1_violin.xml",
+        "Beethoven-Orchestra": "Symphony_5th_1Mov.xml"
+    }[sampleName]
+
+    with resources.path('maialib', 'xml-scores-examples') as data_path:
+        default_config_path = data_path / xmlFileName
+        return str(default_config_path)
