ae1cd5374b35a8572bf01ea7e576654c1c72813c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:39 +0000

    Add package systemJSON/0.1/systemJSON-0.1.tar.gz

---
+++ packages/systemJSON/systemJSON-0.1.tar.gz/systemJSON-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='systemJSON',
+	version='0.1',
+	author='DanXvo',
+	author_email='coldreality@mail.ru',
+	description='is lib for json',
+	packages=['systemJSON'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
---
+++ packages/systemJSON/systemJSON-0.1.tar.gz/systemJSON-0.1/systemJSON/__init__.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+class js:
+    def write(data, filename):
+        data = json.dumps(data)
+        data = json.loads(str(data))
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4)
+
+    def read(filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            return json.load(file)
