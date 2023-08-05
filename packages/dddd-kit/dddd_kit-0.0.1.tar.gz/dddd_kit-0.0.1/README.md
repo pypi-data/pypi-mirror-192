f0468928ebfaa3686a11b52347194b5461ccac5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:28 +0000

    Add package dddd-kit/0.0.1/dddd_kit-0.0.1.tar.gz

---
+++ packages/dddd-kit/dddd_kit-0.0.1.tar.gz/dddd_kit-0.0.1/README.md
@@ -0,0 +1,2 @@
+# python-ddd-infra
+This repository contaions a set of base component and implementation for DDD infrastructure
---
+++ packages/dddd-kit/dddd_kit-0.0.1.tar.gz/dddd_kit-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="dddd_kit",
+    version="0.0.1",
+    author="duxiaoyuan",
+    author_email="duxiaoyuan@hiorka.com",
+    description="A kit for django ddd",
+    long_description="A kit for django ddd",
+    long_description_content_type="text/markdown",
+    url="https://github.com/XiaoyuanDu/dddd-kit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
