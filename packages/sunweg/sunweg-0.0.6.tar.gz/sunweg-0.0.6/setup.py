6aabaaae0954fb4f77752d90020275073a5eb2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:13 +0000

    Add package sunweg/0.0.6/sunweg-0.0.6.tar.gz

---
+++ packages/sunweg/sunweg-0.0.6.tar.gz/sunweg-0.0.6/setup.py
@@ -0,0 +1,27 @@
+#!/bin/python
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requires = [
+    "requests",
+]
+
+setuptools.setup(
+    name='sunweg',
+    version='0.0.6',
+    author="rokam",
+    author_email="lucas@mindello.com.br",
+    description="A library to retrieve data from sunweg.net",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rokam/sunweg",
+    install_requires=requires,
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
