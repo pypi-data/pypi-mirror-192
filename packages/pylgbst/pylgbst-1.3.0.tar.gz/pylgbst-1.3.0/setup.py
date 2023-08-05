7bd19e8f296978f389e7567fc08af267d109be3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:47 +0000

    Add package pylgbst/1.3.0/pylgbst-1.3.0.tar.gz

---
+++ packages/pylgbst/pylgbst-1.3.0.tar.gz/pylgbst-1.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+
+version = "1.3.0"
+tag = os.getenv("TRAVIS_TAG")
+assert not tag or tag == version, "Travis tag has to match version: %r!=%r" % (tag, version)
+
+setup(
+    name="pylgbst",
+    version=version,
+
+    author="Andrey Pokhilko",
+    author_email="andrei.pokhilko@gmail.com",
+    license="MIT",
+    description="Python library to interact with LEGO PoweredUp devices (Lego BOOST etc.)",
+    url='https://github.com/undera/pylgbst',
+    keywords=['LEGO', 'ROBOTICS', 'BLUETOOTH'],
+
+    packages=["pylgbst", "pylgbst.comms"],
+    requires=[],
+    extras_require={
+        # Note that dbus and gi are normally system packages
+        "gatt": ["gatt", "dbus", "gi"],
+        "gattlib": ["gattlib"],
+        "pygatt": ["pygatt", "pexpect"],
+        "bluepy": ["bluepy"],
+        "bleak": ["bleak"],
+    },
+)
