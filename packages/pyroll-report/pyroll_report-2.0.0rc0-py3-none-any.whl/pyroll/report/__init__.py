8adfc73ffdec3904a1917d24050763076ac72b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:49 +0000

    Add package pyroll-report/2.0.0rc0/pyroll_report-2.0.0rc0-py3-none-any.whl

---
+++ packages/pyroll-report/pyroll_report-2.0.0rc0-py3-none-any.whl/pyroll/report/__init__.py
@@ -0,0 +1,15 @@
+from .report import report
+from .pluggy import plugin_manager, hookimpl, hookspec
+
+from . import hookspecs
+
+plugin_manager.add_hookspecs(hookspecs)
+
+from . import unit_display
+
+import importlib.util
+
+CLI_INSTALLED = bool(importlib.util.find_spec("pyroll.cli"))
+
+if CLI_INSTALLED:
+    from . import cli
