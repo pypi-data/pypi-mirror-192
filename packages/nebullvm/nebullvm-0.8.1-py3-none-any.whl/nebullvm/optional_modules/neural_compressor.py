b046efb1ce68e66a2c71ae79d36f23bbac1165ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:29 +0000

    Add package nebullvm/0.8.1/nebullvm-0.8.1-py3-none-any.whl

---
+++ packages/nebullvm/nebullvm-0.8.1-py3-none-any.whl/nebullvm/optional_modules/neural_compressor.py
@@ -0,0 +1,17 @@
+try:
+    import neural_compressor  # noqa F401
+    from neural_compressor.adaptor.pytorch import (
+        _cfg_to_qconfig as cfg_to_qconfig,
+        _cfgs_to_fx_cfgs as cfgs_to_fx_cfgs,
+    )
+    from neural_compressor.experimental import (
+        MixedPrecision,
+        Quantization,
+        Pruning,
+    )
+except ImportError:
+    cfg_to_qconfig = cfgs_to_fx_cfgs = None
+    MixedPrecision = Quantization = Pruning = object
+except ValueError:
+    # MacOS
+    MixedPrecision = Quantization = Pruning = object
