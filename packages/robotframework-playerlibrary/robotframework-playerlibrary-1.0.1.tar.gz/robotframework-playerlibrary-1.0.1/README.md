b97d82e8415ac4462d500f3cb2333e6baf923dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:26 +0000

    Add package robotframework-playerlibrary/1.0.1/robotframework-playerlibrary-1.0.1.tar.gz

---
+++ packages/robotframework-playerlibrary/robotframework-playerlibrary-1.0.1.tar.gz/robotframework-playerlibrary-1.0.1/README.md
@@ -0,0 +1,2 @@
+# robotframework-playerlibrary
+Simple GUI/API automation testing library written in Python using Playwright
---
+++ packages/robotframework-playerlibrary/robotframework-playerlibrary-1.0.1.tar.gz/robotframework-playerlibrary-1.0.1/src/PlayerLibrary/__init__.py
@@ -0,0 +1,22 @@
+from robotlibcore import DynamicCore
+from .api_context import APIContext
+from .button_handler import ButtonHandler
+from .checkbox_handler import CheckboxHandler
+from .datepicker_handler import DatePickerHandler
+from .dropdown_handler import DropdownHandler
+from .iframe_handler import IframeHandler
+from .page_handler import PageHandler
+from .popup_handler import PopupHandler
+from .table_handler import TableHandler
+from .textbox_handler import TextboxHandler
+from .ui_context import UIContext
+
+
+class PlayerLibrary(DynamicCore):
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+
+    def __init__(self):
+        libraries = [APIContext(), UIContext(), ButtonHandler(), CheckboxHandler(), DatePickerHandler(),
+                     DropdownHandler(), IframeHandler(), PageHandler(), PopupHandler(), TableHandler(),
+                     TextboxHandler()]
+        DynamicCore.__init__(self, libraries)
