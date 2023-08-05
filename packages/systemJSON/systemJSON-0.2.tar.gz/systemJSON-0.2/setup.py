fc2397da25f9994ba1ae59dac5270edcfa4b4a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:53 +0000

    Add package systemJSON/0.2/systemJSON-0.2.tar.gz

---
+++ packages/systemJSON/systemJSON-0.2.tar.gz/systemJSON-0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='systemJSON',
+	version='0.2',
+	author='DanXvo',
+	author_email='coldreality@mail.ru',
+	description='lib for python json',
+	packages=['systemJSON'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
