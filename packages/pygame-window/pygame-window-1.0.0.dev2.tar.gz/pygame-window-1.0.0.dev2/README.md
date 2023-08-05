f092494e1f24c56b0f93adf7ec7145b0591b193f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:12 +0000

    Add package pygame-window/1.0.0.dev2/pygame-window-1.0.0.dev2.tar.gz

---
+++ packages/pygame-window/pygame-window-1.0.0.dev2.tar.gz/pygame-window-1.0.0.dev2/README.md
@@ -0,0 +1,9 @@
+# Pygame-Window
+
+Pygame-Window is a Python library that provides an API for managing and
+manipulating multiple windows within the Pygame framework, as well as enhancing
+their functionalities.
+
+The library uses Pygame's sub-module `pygame._sdl2` to access the low-level
+SDL2 library that Pygame is built upon, providing fine-grained control over
+window creation and management.
---
+++ packages/pygame-window/pygame-window-1.0.0.dev2.tar.gz/pygame-window-1.0.0.dev2/setup.py
@@ -0,0 +1,77 @@
+from setuptools import setup, find_packages
+
+with open("src/pygwin/VERSION", "r", encoding="UTF-8") as f:
+    version = f.read()
+
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
+
+with open("requirements/prod.txt", "r", encoding="UTF-8") as f:
+    reqs_prod = f.read().splitlines()
+
+with open("requirements/devs.txt", "r", encoding="UTF-8") as f:
+    reqs_devs = f.read().splitlines()
+
+with open("requirements/docs.txt", "r", encoding="UTF-8") as f:
+    reqs_docs = f.read().splitlines()
+
+reqs_all = reqs_devs + reqs_docs
+
+metadata = {
+    "name": "pygame-window",
+    "version": version,
+    "author": "Zenthm",
+    "author_email": "zenthm.dev@gmail.com",
+    "description": "Enhanced window management and functionalities for Pygame",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://pygame-window.readthedocs.io/en/latest/",
+    "download_url": "https://pypi.org/project/pygame-window/",
+    "project_urls": {
+        "Bug Tracker": "https://github.com/zenthm/pygame-window/issues",
+        "Documentation": "https://pygame-window.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/zenthm/pygame-window",
+    },
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Games/Entertainment",
+        "Topic :: Software Development :: Libraries :: pygame",
+    ],
+    "license": "MIT",
+    "packages": find_packages(where="src"),
+    "package_dir": {"": "src"},
+    "include_package_data": True,
+    "install_requires": reqs_prod,
+    "extras_require": {
+        "all": reqs_all,
+        "devs": reqs_devs,
+        "docs": reqs_docs,
+    },
+    "python_requires": ">=3.7,<3.11",
+    "keywords": [
+        "python",
+        "pygame",
+        "window",
+        "pygame-library",
+        "window-management",
+        "multiple-windows",
+        "pygame-window",
+        "pygame-sdl2",
+    ],
+    "zip_safe": False,
+}
+
+setup(**metadata)
