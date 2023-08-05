0a43f9cbfb1c7e901c0e3a78dc9be3d1681c504c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:16 +0000

    Add package motorized/0.6.0/motorized-0.6.0-py3-none-any.whl

---
+++ packages/motorized/motorized-0.6.0-py3-none-any.whl/motorized/client.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from contextlib import asynccontextmanager
+
+
+class Connection:
+    client: AsyncIOMotorClient = None
+    database: AsyncIOMotorDatabase = None
+
+    async def connect(self, *args, **kwargs):
+        self.client = AsyncIOMotorClient(*args, **kwargs)
+        self.database = self.client.get_default_database()
+
+    async def disconnect(self) -> None:
+        self.client = None
+        self.database = None
+
+
+@asynccontextmanager
+async def client(
+    *args,
+    client: Optional[AsyncIOMotorClient] = None,
+    **kwargs
+):
+    if not client:
+        client = AsyncIOMotorClient(*args, **kwargs)
+    old_client = connection.client
+    old_database = connection.database
+    connection.client = client
+    connection.database = client.get_default_database()
+    yield
+    connection.client = old_client
+    connection.database = old_database
+
+
+connection = Connection()
