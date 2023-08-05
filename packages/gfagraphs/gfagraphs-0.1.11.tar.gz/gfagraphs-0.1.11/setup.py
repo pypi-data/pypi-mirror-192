cf28d49430bd2daaa590098bcdfe43c67f87f99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:18 +0000

    Add package gfagraphs/0.1.11/gfagraphs-0.1.11.tar.gz

---
+++ packages/gfagraphs/gfagraphs-0.1.11.tar.gz/gfagraphs-0.1.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='gfagraphs',
+    version='0.1.11',
+    description='Abstraction layer for GFA file format',
+    url='https://github.com/Tharos-ux/gfatypes',
+    author='Tharos',
+    author_email='dubois.siegfried@gmail.com',
+    license='MIT',
+    packages=['gfagraphs'],
+    zip_safe=False,
+    install_requires=['networkx', 'tharos-pytools']
+)
