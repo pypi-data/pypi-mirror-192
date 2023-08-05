74f1cb458abb12b9de335b60d65813009dff3efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:22 +0000

    Add package kimfame/0.0.5/kimfame-0.0.5.tar.gz

---
+++ packages/kimfame/kimfame-0.0.5.tar.gz/kimfame-0.0.5/src/kimfame/__init__.py
@@ -0,0 +1,13 @@
+from kimfame import aaa
+from kimfame import bbb
+from kimfame import ccc
+from kimfame import ddd
+from kimfame import kimfame
+
+__all__ = [
+    "aaa",
+    "bbb",
+    "ccc",
+    "ddd",
+    "kimfame",
+]
