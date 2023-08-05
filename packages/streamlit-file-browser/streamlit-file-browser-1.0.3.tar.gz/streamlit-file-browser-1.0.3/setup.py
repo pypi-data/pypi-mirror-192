a365fc43a0d43f5f213d82349b2f326d7d2b496c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:55 +0000

    Add package streamlit-file-browser/1.0.3/streamlit-file-browser-1.0.3.tar.gz

---
+++ packages/streamlit-file-browser/streamlit-file-browser-1.0.3.tar.gz/streamlit-file-browser-1.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-file-browser",
+    version="1.0.3",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas",
+        "filetype",
+        "streamlit_molstar",
+        "streamlit >= 0.63",
+    ],
+)
