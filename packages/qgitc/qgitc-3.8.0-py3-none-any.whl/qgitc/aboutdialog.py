5f883cdf4246dc56d5cec533bb8c5974c3963916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:38 +0000

    Add package qgitc/3.8.0/qgitc-3.8.0-py3-none-any.whl

---
+++ packages/qgitc/qgitc-3.8.0-py3-none-any.whl/qgitc/aboutdialog.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from PySide6.QtWidgets import QDialog, QApplication
+from PySide6.QtCore import QSize
+from .ui_aboutdialog import Ui_AboutDialog
+from .common import dataDirPath
+from .version import VERSION
+
+
+class AboutDialog(QDialog, Ui_AboutDialog):
+
+    def __init__(self, parent=None):
+        super(AboutDialog, self).__init__(parent)
+
+        self.setupUi(self)
+
+        icon = qApp.windowIcon()
+        self.appIcon.setPixmap(icon.pixmap(64, 64))
+
+        self.tbAbout.setOpenExternalLinks(True)
+        self.tbLicense.setOpenExternalLinks(True)
+
+        self.__initTabs()
+
+        self.resize(QSize(465, 470))
+
+    def __initTabs(self):
+        about = "<center><h3>QGitc " + VERSION + "</h3></center>"
+        about += "<center>"
+        about += self.tr("Git file conflicts and logs viewer")
+        about += "</center>"
+        about += "<center><a href=https://github.com/timxx/qgitc>"
+        about += self.tr("Visit project host")
+        about += "</a></center><br/>"
+        about += "<center>Copyright © 2016-2023 Weitian Leung</center>"
+
+        self.tbAbout.setHtml(about)
+
+        licenseFile = dataDirPath() + "/licenses/Apache-2.0.html"
+        with open(licenseFile) as f:
+            self.tbLicense.setHtml(f.read())
