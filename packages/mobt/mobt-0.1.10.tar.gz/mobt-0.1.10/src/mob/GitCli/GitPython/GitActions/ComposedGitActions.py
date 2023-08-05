91ca13d32b49a1dcadf933442b90edff1bee6e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:38 +0000

    Add package mobt/0.1.10/mobt-0.1.10.tar.gz

---
+++ packages/mobt/mobt-0.1.10.tar.gz/mobt-0.1.10/src/mob/GitCli/GitPython/GitActions/ComposedGitActions.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+
+from mob.GitCli.GitPython import git_logger
+from mob.GitCli.GitPython.GitActions.GitAction import GitAction
+from mob.GitCli.UndoCommands.ComposedUndoCommand import ComposedUndoCommand
+
+_undo_title_logged = False
+
+
+@dataclass()
+class ComposedGitActions(GitAction):
+    __actions: list[GitAction] = field(default_factory=list)
+
+    def __post_init__(self):
+        self.__undo = ComposedUndoCommand()
+        super().__post_init__()
+
+    def _execute(self) -> None:
+        try:
+            for action in self.__actions:
+                self.__undo.add_command(action.execute())
+        except Exception as e:
+            self.undo()
+            raise e
+
+    def _undo(self):
+        global _undo_title_logged
+        if not _undo_title_logged:
+            git_logger().warning("Undoing all Git commands")
+            _undo_title_logged = True
+
+        self.__undo.undo()
+
+    def add_action(self, action: GitAction) -> None:
+        self.__actions.append(action)
