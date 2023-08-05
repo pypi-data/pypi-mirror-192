7cfa0af73a9caa2a65e7e38acb5e035c89d65a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:46 +0000

    Add package muffin-rest/4.2.5/muffin-rest-4.2.5.tar.gz

---
+++ packages/muffin-rest/muffin-rest-4.2.5.tar.gz/muffin-rest-4.2.5/muffin_rest/__init__.py
@@ -0,0 +1,70 @@
+"""REST helpers for Muffin Framework."""
+
+__version__ = "4.2.5"
+__project__ = "muffin-rest"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+# Default query params
+LIMIT_PARAM = "limit"
+OFFSET_PARAM = "offset"
+
+
+from .api import API  # noqa
+from .errors import APIError  # noqa
+from .handler import RESTHandler  # noqa
+
+# Just an alias to support legacy style
+Api = API
+
+
+__all__ = (
+    "API",
+    "Api",
+    "RESTHandler",
+    "APIError",
+    "PWRESTHandler",
+    "PWFilter",
+    "PWFilters",
+    "PWSort",
+    "PWSorting",
+    "SARESTHandler",
+    "SAFilter",
+    "SAFilters",
+    "SASort",
+    "SASorting",
+    "MongoRESTHandler",
+    "MongoFilter",
+    "MongoFilters",
+    "MongoSort",
+    "MongoSorting",
+)
+
+
+# Support Peewee ORM
+try:
+    from .peewee import PWRESTHandler
+    from .peewee.filters import PWFilter, PWFilters
+    from .peewee.sorting import PWSort, PWSorting
+except ImportError:
+    pass
+
+
+# Support SQLAlchemy ORM
+try:
+    from .sqlalchemy import SARESTHandler
+    from .sqlalchemy.filters import SAFilter, SAFilters
+    from .sqlalchemy.sorting import SASort, SASorting
+except ImportError:
+    pass
+
+
+# Support Mongo ORM
+try:
+    from .mongo import MongoRESTHandler
+    from .mongo.filters import MongoFilter, MongoFilters
+    from .mongo.sorting import MongoSort, MongoSorting
+except ImportError:
+    pass
+
+# pylama:ignore=W0611
