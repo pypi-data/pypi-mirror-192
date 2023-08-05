91fcd117377d3822645f52f09c7b356d61522a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:02 +0000

    Add package pangea-sdk/1.2.1/pangea_sdk-1.2.1-py3-none-any.whl

---
+++ packages/pangea-sdk/pangea_sdk-1.2.1-py3-none-any.whl/pangea/services/base.py
@@ -0,0 +1,38 @@
+# Copyright 2022 Pangea Cyber Corporation
+# Author: Pangea Cyber Corporation
+
+from typing import Optional
+
+from pangea import __version__
+from pangea.config import PangeaConfig
+from pangea.request import PangeaRequest
+
+
+class ServiceBase(object):
+    service_name: str = "base"
+    version: str = "v1"
+
+    def __init__(self, token, config: Optional[PangeaConfig] = None):
+        if not token:
+            raise Exception("No token provided")
+
+        self.config = config if config else PangeaConfig()
+
+        self.request = PangeaRequest(
+            self.config,
+            token,
+            self.version,
+            self.service_name,
+        )
+
+        extra_headers = {}
+
+        self.request.set_extra_headers(extra_headers)
+
+    @property
+    def token(self):
+        return self.request.token
+
+    @token.setter
+    def token(self, value):
+        self.request.token = value
