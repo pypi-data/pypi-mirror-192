832001d49002055b84ce905eada5b6c0c1c02a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:27 +0000

    Add package khoj-assistant/0.3.0a1676434280/khoj-assistant-0.3.0a1676434280.tar.gz

---
+++ packages/khoj-assistant/khoj-assistant-0.3.0a1676434280.tar.gz/khoj-assistant-0.3.0a1676434280/tests/test_image_search.py
@@ -0,0 +1,142 @@
+# Standard Modules
+import logging
+from pathlib import Path
+from PIL import Image
+
+# Internal Packages
+from khoj.utils.state import model
+from khoj.utils.constants import web_directory
+from khoj.search_type import image_search
+from khoj.utils.helpers import resolve_absolute_path
+from khoj.utils.rawconfig import ContentConfig, SearchConfig
+
+
+# Test
+# ----------------------------------------------------------------------------------------------------
+def test_image_search_setup(content_config: ContentConfig, search_config: SearchConfig):
+    # Act
+    # Regenerate image search embeddings during image setup
+    image_search_model = image_search.setup(content_config.image, search_config.image, regenerate=True)
+
+    # Assert
+    assert len(image_search_model.image_names) == 3
+    assert len(image_search_model.image_embeddings) == 3
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_image_metadata(content_config: ContentConfig):
+    "Verify XMP Description and Subjects Extracted from Image"
+    # Arrange
+    expected_metadata_image_name_pairs = [
+        (["Billi Ka Bacha.", "Cat", "Grass"], "kitten_park.jpg"),
+        (["Pasture.", "Horse", "Dog"], "horse_dog.jpg"),
+        (["Guinea Pig Eating Celery.", "Rodent", "Whiskers"], "guineapig_grass.jpg")]
+
+    test_image_paths = [
+        Path(content_config.image.input_directories[0] / image_name[1])
+        for image_name in expected_metadata_image_name_pairs
+    ]
+
+    for expected_metadata, test_image_path in zip(expected_metadata_image_name_pairs, test_image_paths):
+        # Act
+        actual_metadata = image_search.extract_metadata(test_image_path)
+
+        # Assert
+        for expected_snippet in expected_metadata[0]:
+            assert expected_snippet in actual_metadata
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_image_search(content_config: ContentConfig, search_config: SearchConfig):
+    # Arrange
+    output_directory = resolve_absolute_path(web_directory)
+    model.image_search = image_search.setup(content_config.image, search_config.image, regenerate=False)
+    query_expected_image_pairs = [("kitten", "kitten_park.jpg"),
+                                  ("horse and dog in a farm", "horse_dog.jpg"),
+                                  ("A guinea pig eating grass", "guineapig_grass.jpg")]
+
+    # Act
+    for query, expected_image_name in query_expected_image_pairs:
+        hits = image_search.query(
+            query,
+            count = 1,
+            model = model.image_search)
+
+        results = image_search.collate_results(
+            hits,
+            model.image_search.image_names,
+            output_directory=output_directory,
+            image_files_url='/static/images',
+            count=1)
+
+        actual_image_path = output_directory.joinpath(Path(results[0].entry).name)
+        actual_image = Image.open(actual_image_path)
+        expected_image = Image.open(content_config.image.input_directories[0].joinpath(expected_image_name))
+
+        # Assert
+        assert expected_image == actual_image
+
+    # Cleanup
+    # Delete the image files copied to results directory
+    actual_image_path.unlink()
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_image_search_query_truncated(content_config: ContentConfig, search_config: SearchConfig, caplog):
+    # Arrange
+    model.image_search = image_search.setup(content_config.image, search_config.image, regenerate=False)
+    max_words_supported = 10
+    query = " ".join(["hello"]*100)
+    truncated_query = " ".join(["hello"]*max_words_supported)
+
+    # Act
+    try:
+        with caplog.at_level(logging.INFO, logger="khoj.search_type.image_search"):
+            image_search.query(
+                query,
+                count = 1,
+                model = model.image_search)
+    # Assert
+    except RuntimeError as e:
+        if "The size of tensor a (102) must match the size of tensor b (77)" in str(e):
+            assert False, f"Query length exceeds max tokens supported by model\n"
+    assert f"Find Images by Text: {truncated_query}" in caplog.text, "Query not truncated"
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_image_search_by_filepath(content_config: ContentConfig, search_config: SearchConfig, caplog):
+    # Arrange
+    output_directory = resolve_absolute_path(web_directory)
+    model.image_search = image_search.setup(content_config.image, search_config.image, regenerate=False)
+    image_directory = content_config.image.input_directories[0]
+
+    query = f"file:{image_directory.joinpath('kitten_park.jpg')}"
+    expected_image_path = f"{image_directory.joinpath('kitten_park.jpg')}"
+
+    # Act
+    with caplog.at_level(logging.INFO, logger="khoj.search_type.image_search"):
+        hits = image_search.query(
+            query,
+            count = 1,
+            model = model.image_search)
+
+        results = image_search.collate_results(
+            hits,
+            model.image_search.image_names,
+            output_directory=output_directory,
+            image_files_url='/static/images',
+            count=1)
+
+    actual_image_path = output_directory.joinpath(Path(results[0].entry).name)
+    actual_image = Image.open(actual_image_path)
+    expected_image = Image.open(expected_image_path)
+
+    # Assert
+    # Ensure file search triggered instead of query with file path as string
+    assert f"Find Images by Image: {resolve_absolute_path(expected_image_path)}" in caplog.text, "File search not triggered"
+    # Ensure the correct image is returned
+    assert expected_image == actual_image, "Incorrect image returned by file search"
+
+    # Cleanup
+    # Delete the image files copied to results directory
+    actual_image_path.unlink()
---
+++ packages/khoj-assistant/khoj-assistant-0.3.0a1676434280.tar.gz/khoj-assistant-0.3.0a1676434280/tests/test_text_search.py
@@ -0,0 +1,150 @@
+# System Packages
+from copy import deepcopy
+from pathlib import Path
+
+# External Packages
+import pytest
+
+# Internal Packages
+from khoj.utils.state import model
+from khoj.search_type import text_search
+from khoj.utils.rawconfig import ContentConfig, SearchConfig, TextContentConfig
+from khoj.processor.org_mode.org_to_jsonl import OrgToJsonl
+
+
+# Test
+# ----------------------------------------------------------------------------------------------------
+def test_asymmetric_setup_with_missing_file_raises_error(org_config_with_only_new_file: TextContentConfig, search_config: SearchConfig):
+    # Arrange
+    # Ensure file mentioned in org.input-files is missing
+    single_new_file = Path(org_config_with_only_new_file.input_files[0])
+    single_new_file.unlink()
+
+    # Act
+    # Generate notes embeddings during asymmetric setup
+    with pytest.raises(FileNotFoundError):
+        text_search.setup(OrgToJsonl, org_config_with_only_new_file, search_config.asymmetric, regenerate=True)
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_asymmetric_setup_with_empty_file_raises_error(org_config_with_only_new_file: TextContentConfig, search_config: SearchConfig):
+    # Act
+    # Generate notes embeddings during asymmetric setup
+    with pytest.raises(ValueError, match=r'^No valid entries found*'):
+        text_search.setup(OrgToJsonl, org_config_with_only_new_file, search_config.asymmetric, regenerate=True)
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_asymmetric_setup(content_config: ContentConfig, search_config: SearchConfig):
+    # Act
+    # Regenerate notes embeddings during asymmetric setup
+    notes_model = text_search.setup(OrgToJsonl, content_config.org, search_config.asymmetric, regenerate=True)
+
+    # Assert
+    assert len(notes_model.entries) == 10
+    assert len(notes_model.corpus_embeddings) == 10
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_asymmetric_search(content_config: ContentConfig, search_config: SearchConfig):
+    # Arrange
+    model.notes_search = text_search.setup(OrgToJsonl, content_config.org, search_config.asymmetric, regenerate=True)
+    query = "How to git install application?"
+
+    # Act
+    hits, entries = text_search.query(
+        query,
+        model = model.notes_search,
+        rank_results=True)
+
+    results = text_search.collate_results(
+        hits,
+        entries,
+        count=1)
+
+    # Assert
+    # Actual_data should contain "Khoj via Emacs" entry
+    search_result = results[0].entry
+    assert "git clone" in search_result
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_entry_chunking_by_max_tokens(org_config_with_only_new_file: TextContentConfig, search_config: SearchConfig):
+    # Arrange
+    # Insert org-mode entry with size exceeding max token limit to new org file
+    max_tokens = 256
+    new_file_to_index = Path(org_config_with_only_new_file.input_files[0])
+    with open(new_file_to_index, "w") as f:
+        f.write(f"* Entry more than {max_tokens} words\n")
+        for index in range(max_tokens+1):
+            f.write(f"{index} ")
+
+    # Act
+    # reload embeddings, entries, notes model after adding new org-mode file
+    initial_notes_model = text_search.setup(OrgToJsonl, org_config_with_only_new_file, search_config.asymmetric, regenerate=False)
+
+    # Assert
+    # verify newly added org-mode entry is split by max tokens
+    assert len(initial_notes_model.entries) == 2
+    assert len(initial_notes_model.corpus_embeddings) == 2
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_asymmetric_reload(content_config: ContentConfig, search_config: SearchConfig, new_org_file: Path):
+    # Arrange
+    initial_notes_model= text_search.setup(OrgToJsonl, content_config.org, search_config.asymmetric, regenerate=True)
+
+    assert len(initial_notes_model.entries) == 10
+    assert len(initial_notes_model.corpus_embeddings) == 10
+
+    # append org-mode entry to first org input file in config
+    content_config.org.input_files = [f'{new_org_file}']
+    with open(new_org_file, "w") as f:
+        f.write("\n* A Chihuahua doing Tango\n- Saw a super cute video of a chihuahua doing the Tango on Youtube\n")
+
+    # regenerate notes jsonl, model embeddings and model to include entry from new file
+    regenerated_notes_model = text_search.setup(OrgToJsonl, content_config.org, search_config.asymmetric, regenerate=True)
+
+    # Act
+    # reload embeddings, entries, notes model from previously generated notes jsonl and model embeddings files
+    initial_notes_model = text_search.setup(OrgToJsonl, content_config.org, search_config.asymmetric, regenerate=False)
+
+    # Assert
+    assert len(regenerated_notes_model.entries) == 11
+    assert len(regenerated_notes_model.corpus_embeddings) == 11
+
+    # Assert
+    # verify new entry loaded from updated embeddings, entries
+    assert len(initial_notes_model.entries) == 11
+    assert len(initial_notes_model.corpus_embeddings) == 11
+
+    # Cleanup
+    # reset input_files in config to empty list
+    content_config.org.input_files = []
+
+
+# ----------------------------------------------------------------------------------------------------
+def test_incremental_update(content_config: ContentConfig, search_config: SearchConfig, new_org_file: Path):
+    # Arrange
+    initial_notes_model = text_search.setup(OrgToJsonl, content_config.org, search_config.asymmetric, regenerate=True)
+
+    assert len(initial_notes_model.entries) == 10
+    assert len(initial_notes_model.corpus_embeddings) == 10
+
+    # append org-mode entry to first org input file in config
+    with open(new_org_file, "w") as f:
+        f.write("\n* A Chihuahua doing Tango\n- Saw a super cute video of a chihuahua doing the Tango on Youtube\n")
+
+    # Act
+    # update embeddings, entries with the newly added note
+    content_config.org.input_files = [f'{new_org_file}']
+    initial_notes_model = text_search.setup(OrgToJsonl, content_config.org, search_config.asymmetric, regenerate=False)
+
+    # Assert
+    # verify new entry added in updated embeddings, entries
+    assert len(initial_notes_model.entries) == 11
+    assert len(initial_notes_model.corpus_embeddings) == 11
+
+    # Cleanup
+    # reset input_files in config to empty list
+    content_config.org.input_files = []
