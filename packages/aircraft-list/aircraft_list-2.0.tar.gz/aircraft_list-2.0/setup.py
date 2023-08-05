298df5aa4c916443adeb32dc4c186703fbc20f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:42 +0000

    Add package aircraft-list/2.0/aircraft_list-2.0.tar.gz

---
+++ packages/aircraft-list/aircraft_list-2.0.tar.gz/aircraft_list-2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aircraft_list',
+    version='2.0',
+    description='This package contains a list of the Aircraft models and manufacturer as per DOC 8643 ICAO',
+    author='Giorgio Scarso',
+    author_email='scarso.giorgio@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas'],
+    url='https://github.com/George88000/aircraft_models',
+    package_data={'aircraft_list': ['aircraft_model_list.csv']},
+)
