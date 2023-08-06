7f1594f4b53a83f3d626ac5c489df36e7847947b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:50 +0000

    Add package ansible-deployer/0.0.43/ansible_deployer-0.0.43-py3-none-any.whl

---
+++ packages/ansible-deployer/ansible_deployer-0.0.43-py3-none-any.whl/ansible_deployer/modules/database/schema.py
@@ -0,0 +1,23 @@
+"""Sqlite database schema dictionaries"""
+
+SCHEMAS = {
+    "sequences": {
+        "sequence_id": "text",
+        "hostname": "text",
+        "user_id": "text",
+        "hooks": "text",
+        "task": "text",
+        "start": "text",
+        "end": "text",
+        "host_locked": "text",
+        "keep_locked_on": "text",
+        "self_setup_on": "text",
+        "conf_dir_on": "text"
+    },
+    "play_item_tasks": {
+        "sequence_id": "text",
+        "task_name": "text",
+        "hostname": "text",
+        "task_details": "text"
+    }
+}
