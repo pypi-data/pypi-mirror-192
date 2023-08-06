d5fbb1fcbc4b4d8fe96be2a2207be6f031a7b233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:11 +0000

    Add package awsomess/0.0.8/awsomess-0.0.8.tar.gz

---
+++ packages/awsomess/awsomess-0.0.8.tar.gz/awsomess-0.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="awsomess",
+    version="0.0.8",
+    description="just to test deployment and nothing else",
+    long_description="here is a long description of this project",
+    author="afonso",
+    author_email="a.schulzalbrecht@gmail.com",
+    # package_dir={"": "src"},  # allows: import awsomess
+    packages=find_packages(),
+)
