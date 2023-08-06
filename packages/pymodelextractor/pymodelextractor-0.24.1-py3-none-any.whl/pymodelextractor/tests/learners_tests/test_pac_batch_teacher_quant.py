450248e92e37719fa44c5da48d0f198302032218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:37 +0000

    Add package pymodelextractor/0.24.1/pymodelextractor-0.24.1-py3-none-any.whl

---
+++ packages/pymodelextractor/pymodelextractor-0.24.1-py3-none-any.whl/pymodelextractor/tests/learners_tests/test_pac_batch_teacher_quant.py
@@ -0,0 +1,86 @@
+import unittest
+
+from pythautomata.automata_definitions.weighted_tomitas_grammars import WeightedTomitasGrammars
+
+from pymodelextractor.learners.observation_tree_learners.pdfa_quantization_n_ary_tree_learner\
+     import PDFAQuantizationNAryTreeLearner
+
+from pymodelextractor.teachers.pac_batch_probabilistic_teacher import PACBatchProbabilisticTeacher
+from pythautomata.model_comparators.wfa_quantization_comparison_strategy import WFAQuantizationComparator
+from pythautomata.model_comparators.wfa_partition_comparison_strategy import WFAPartitionComparator
+from pythautomata.utilities.probability_partitioner import QuantizationProbabilityPartitioner
+
+class TestPACBatchTeacherQuant(unittest.TestCase):
+
+    def setUp(self):
+        self.partitions = 10
+        self.comparator = WFAQuantizationComparator(self.partitions)
+        self.probability_partitioner = QuantizationProbabilityPartitioner(self.partitions)
+        self.comparator = WFAPartitionComparator(self.probability_partitioner)
+        self.learner = PDFAQuantizationNAryTreeLearner(self.probability_partitioner)
+
+    def test_tomitas_1(self):
+        model = WeightedTomitasGrammars.get_automaton_1()
+        teacher = PACBatchProbabilisticTeacher(model, 0.05, 0.01, comparator = self.comparator,
+                                               max_seq_length=20)
+        result = self.learner.learn(teacher, verbose = True)        
+        extracted_model = result.model     
+        self.assertTrue(self.comparator.are_equivalent(model, extracted_model))
+    
+    def test_tomitas_2(self):
+        model = WeightedTomitasGrammars.get_automaton_2()
+        
+        teacher = PACBatchProbabilisticTeacher(model, 0.05, 0.01, comparator = self.comparator,
+                                               max_seq_length=20)
+        result = self.learner.learn(teacher, verbose = True)        
+        extracted_model = result.model     
+        self.assertTrue(self.comparator.are_equivalent(model, extracted_model))
+    
+
+    def test_tomitas_3(self):
+        model = WeightedTomitasGrammars.get_automaton_3()
+        
+        teacher = PACBatchProbabilisticTeacher(model, 0.05, 0.01, comparator = self.comparator,
+                                               max_seq_length=20)
+        result = self.learner.learn(teacher, verbose = True)        
+        extracted_model = result.model     
+        self.assertTrue(self.comparator.are_equivalent(model, extracted_model))
+
+    def test_tomitas_4(self):
+        model = WeightedTomitasGrammars.get_automaton_4()
+        
+        teacher = PACBatchProbabilisticTeacher(model, 0.05, 0.01, comparator = self.comparator,
+                                               max_seq_length=20)
+        result = self.learner.learn(teacher, verbose = True)        
+        extracted_model = result.model     
+        self.assertTrue(self.comparator.are_equivalent(model, extracted_model))
+    
+    def test_tomitas_5(self):
+        model = WeightedTomitasGrammars.get_automaton_5()
+        
+        teacher = PACBatchProbabilisticTeacher(model, 0.05, 0.01, comparator = self.comparator,
+                                               max_seq_length=20)
+        result = self.learner.learn(teacher, verbose = True)        
+        extracted_model = result.model     
+        self.assertTrue(self.comparator.are_equivalent(model, extracted_model))
+    
+
+    def test_tomitas_6(self):
+        model = WeightedTomitasGrammars.get_automaton_6()
+        
+        teacher = PACBatchProbabilisticTeacher(model, 0.05, 0.01, comparator = self.comparator,
+                                               max_seq_length=20)
+        result = self.learner.learn(teacher, verbose = True)        
+        extracted_model = result.model     
+        self.assertTrue(self.comparator.are_equivalent(model, extracted_model))
+
+    def test_tomitas_7(self):
+        model = WeightedTomitasGrammars.get_automaton_7()
+        
+        teacher = PACBatchProbabilisticTeacher(model, 0.05, 0.01, comparator = self.comparator,
+                                               max_seq_length=20)
+        result = self.learner.learn(teacher, verbose = True)        
+        extracted_model = result.model     
+        self.assertTrue(self.comparator.are_equivalent(model, extracted_model))
+
+
