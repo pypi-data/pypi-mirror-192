1b9706636e4df70f7002cc2c5ea66a259ba7c775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:52 +0000

    Add package laebelmaker/0.4.0/laebelmaker-0.4.0-py3-none-any.whl

---
+++ packages/laebelmaker/laebelmaker-0.4.0-py3-none-any.whl/laebelmaker/cli.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+"""
+Generates Traefik labels for use in e.g. Docker containers.
+"""
+
+from typing import List, Tuple, Callable
+from laebelmaker.label import (
+    gen_label_set_from_user,
+    gen_label_set_from_compose,
+    gen_label_set_from_container,
+)
+from laebelmaker.errors import NoInformationException
+from laebelmaker.utils.formatter import (  # pylint: disable=unused-import
+    formatter_docker,
+    formatter_none,
+    formatter_yaml,
+)
+
+import argparse
+
+__author__ = "Ivan Bratović"
+__copyright__ = "Copyright 2023, Ivan Bratović"
+__license__ = "MIT"
+
+__version__ = "0.4.0"
+
+FORMATS: List[str] = [
+    symbol.split("_")[-1] for symbol in globals() if symbol.startswith("format")
+]
+
+
+def has_yaml_extension(path: str) -> bool:
+    basename = path.strip().split("/")[-1]
+    return basename.lower().endswith(".yaml") or basename.lower().endswith(".yml")
+
+
+def main() -> None:
+    args: argparse.Namespace
+    parser: argparse.ArgumentParser
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Generate Traefik labels")
+    parser.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="use interactive mode",
+    )
+    parser.add_argument(
+        "-c",
+        "--container",
+        metavar="NAME",
+        help="generate labels for a given container on the system",
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        metavar="FORMAT",
+        help=f"set output format, one of: [{', '.join(FORMATS)}]",
+        choices=FORMATS,
+        default="none",
+    )
+    parser.add_argument(
+        "files",
+        metavar="FILE",
+        nargs="*",
+        help="Compose file to generate labels for",
+    )
+
+    args, _ = parser.parse_known_args()
+
+    formatter: Callable[[List[str]], str] = globals()[f"formatter_{args.format}"]
+    labels: List[Tuple[str, List[str]]] = []
+
+    if args.interactive:
+        try:
+            labels = [gen_label_set_from_user("")]
+        except NoInformationException as e:
+            print(e)
+    elif container := args.container:
+        try:
+            labels = [gen_label_set_from_container(container)]
+        except NoInformationException:
+            print(f"Invalid container identifier given: {container!r}.")
+        except ModuleNotFoundError:
+            print(f"Please install the docker module to use this feature.")
+
+    elif args.files:
+        for arg in args.files:
+            try:
+                labels.append(gen_label_set_from_compose(arg))
+            except FileNotFoundError:
+                print(f"Unknown file path: {arg!r}")
+            except NoInformationException as e:
+                print(e)
+    else:
+        parser.print_help()
+        return
+
+    if labels:
+        for title, label_list in labels:
+            print(f"--START GENERATED LABELS FOR {title!r}--")
+            print(formatter(label_list), end="")
+            print(f"--END GENERATED LABELS FOR {title!r}--")
+    else:
+        print("Failed to produce output.")
+        print("Try running: laebelmaker --help")
+
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/laebelmaker/laebelmaker-0.4.0-py3-none-any.whl/laebelmaker/utils/input.py
@@ -0,0 +1,59 @@
+"""Module containing useful functions to get user input"""
+
+from typing import Any, Optional
+import readline
+
+__author__ = "Ivan Bratović"
+__copyright__ = "Copyright 2023, Ivan Bratović"
+__license__ = "MIT"
+
+
+def input_prefilled(prompt: str, text: str = "") -> str:
+    def hook() -> None:
+        readline.insert_text(text)
+        readline.redisplay()
+
+    readline.set_pre_input_hook(hook)
+    result = input(prompt)
+    readline.set_pre_input_hook()
+    return result
+
+
+def input_item(name: str, typ: type, item_orig: Optional[Any] = None) -> Any:
+    if typ == int:
+        hint_text = " (integer)"
+    elif typ == bool:
+        hint_text = " (yes/No)"
+    else:
+        hint_text = ""
+    new_value: str = input_prefilled(
+        f"Enter value for {name.replace('_', ' ')!r}{hint_text}: ",
+        str(item_orig) if item_orig else "",
+    )
+    if typ == bool:
+        if new_value.lower() not in ("true", "yes", "1", "y"):
+            new_value = ""
+    return typ(new_value if new_value else False)
+
+
+def query_selection(options: list[Any], item_name: str, default_index: int = 0) -> Any:
+    if not options:
+        raise ValueError("Options list must not be empty for query_selection.")
+    if len(options) == 1:
+        return options[0]
+    print(f"Found multiple {item_name}s.")
+    for i, service in enumerate(options):
+        print(f" {i+1}. {service}")
+    answer: str = input(
+        f"{item_name.capitalize()} number to use (default {default_index + 1}): "
+    )
+    selection: int = default_index
+    if answer:
+        selection = int(answer) - 1
+    assert selection in range(len(options)), "Selected index out of range"
+    return options[selection]
+
+
+def query_change(item_orig: Any, item_name: str) -> Any:
+    typ: type = type(item_orig)
+    return input_item(item_name, typ, item_orig)
