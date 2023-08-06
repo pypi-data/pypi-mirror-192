3dc18bfe98dc3221eb1b675651d94179ed19dcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:15 +0000

    Add package sherpa-onnx/1.1/sherpa_onnx-1.1-cp38-cp38-win_amd64.whl

---
+++ packages/sherpa-onnx/sherpa_onnx-1.1-cp38-cp38-win_amd64.whl/sherpa_onnx/__init__.py
@@ -0,0 +1,9 @@
+from _sherpa_onnx import (
+    FeatureExtractorConfig,
+    OnlineRecognizerConfig,
+    OnlineStream,
+    OnlineTransducerModelConfig,
+)
+
+from .online_recognizer import OnlineRecognizer
+__version__ = '1.1'
