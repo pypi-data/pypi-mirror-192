21a5c863c2e5ccd65a1590d6ef42b7b64046cf78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:20 +0000

    Add package manuscriptify/1.0.5/manuscriptify-1.0.5.tar.gz

---
+++ packages/manuscriptify/manuscriptify-1.0.5.tar.gz/manuscriptify-1.0.5/manuscriptify/constants.py
@@ -0,0 +1,24 @@
+ARTICLES = ['A', 'An', 'The']
+
+CONJUNCTIONS = ['For', 'And',
+                'Nor', 'But',
+                'Or', 'Yet', 'So']
+
+PREPOSITIONS = ['At', 'By', 'Down',
+                'From', 'In', 'Into',
+                'Near', 'Of', 'Off',
+                'On', 'To', 'Upon',
+                'With']
+
+SPECIAL_PARTS = ['FOREWORD',
+                 'PREFACE',
+                 'ACKNOWLEDGEMENTS',
+                 'PROLOGUE',
+                 'EPILOGUE',
+                 'PROLOG',
+                 'EPILOG']
+WEBSITE_LINK = {
+    'link': {
+        'url': 'https://manuscriptify.com'
+    }
+}
