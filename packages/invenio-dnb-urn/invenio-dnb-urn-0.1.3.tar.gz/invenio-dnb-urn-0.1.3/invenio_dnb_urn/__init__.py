3df62bc3996a0c9c1c78cd97d8efc43b87cd9fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:24 +0000

    Add package invenio-dnb-urn/0.1.3/invenio-dnb-urn-0.1.3.tar.gz

---
+++ packages/invenio-dnb-urn/invenio-dnb-urn-0.1.3.tar.gz/invenio-dnb-urn-0.1.3/invenio_dnb_urn/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022, 2023 University of Münster.
+#
+# Invenio-Dnb-Urn is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio Serializer to create xepicur serialization for DNB harvesting."""
+
+from .serialize import InvenioSerializerXMetaDissPlus
+from .provider import DnbUrnProvider
+
+__version__ = '0.1.3'
+
+__all__ = (
+    '__version__',
+    'InvenioSerializerXMetaDissPlus',
+    'DnbUrnProvider',
+)
+
