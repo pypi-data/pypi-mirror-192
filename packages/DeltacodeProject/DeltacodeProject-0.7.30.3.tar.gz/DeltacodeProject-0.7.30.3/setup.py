1d6b572aa8689423d0edb040d57974fdef1b2a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:13 +0000

    Add package DeltacodeProject/0.7.30.3/DeltacodeProject-0.7.30.3.tar.gz

---
+++ packages/DeltacodeProject/DeltacodeProject-0.7.30.3.tar.gz/DeltacodeProject-0.7.30.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='DeltacodeProject',
+    version='0.7.30.3',
+    author='daisseur',
+    author_email='daisseur@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/daisseur/Deltacode_project',
+    description="Encoder et décoder du texte à l'aide de pluisieurs types d'encodages",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    license=open('LICENSE', 'r').read(),
+    keywords=['python', 'deltacode', 'code', 'coding', 'encoding', 'DayEncoding', 'ROT', 'Cesar'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
