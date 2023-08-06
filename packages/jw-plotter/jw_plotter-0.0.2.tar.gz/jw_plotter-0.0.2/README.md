b07adab54fb9b4a2e546b3e098f47c380c8da0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:38 +0000

    Add package jw-plotter/0.0.2/jw_plotter-0.0.2.tar.gz

---
+++ packages/jw-plotter/jw_plotter-0.0.2.tar.gz/jw_plotter-0.0.2/README.md
@@ -0,0 +1,7 @@
+# JW Plotter
+
+PythonMegaLiveInstantActionPlotterDeluxe
+
+PREALPHA EARLY ACCESS
+
+![This is an example plot](doc/example_plot.png)
---
+++ packages/jw-plotter/jw_plotter-0.0.2.tar.gz/jw_plotter-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jw_plotter",
+    version="0.0.2",
+    author="Jakob Weimar",
+    author_email="jakob.weimar@gmx.de",
+    description="A small python liveplotter using PyQtGraph",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JakobWe/JWPlotter",
+    packages=['jw_plotter'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pyqtgraph>=0.10.0",
+    ],
+    python_requires='>=3.6',
+)
