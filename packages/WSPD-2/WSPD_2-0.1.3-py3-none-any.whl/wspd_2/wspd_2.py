40d2b1f0232474934e3f9722aaaacfdedbf8cbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:51 +0000

    Add package WSPD-2/0.1.3/WSPD_2-0.1.3-py3-none-any.whl

---
+++ packages/WSPD-2/WSPD_2-0.1.3-py3-none-any.whl/wspd_2/wspd_2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 10:32:52 2023
+
+@author: Sören
+"""
+
+class WSPD_2:
+    def __init__(self):
+        pass
+    
+    def run():
+        print('WSPD_2 is running')
+        
