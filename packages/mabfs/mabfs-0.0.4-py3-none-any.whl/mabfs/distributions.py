3937f4aecbb1f4054701751d3d6e9f482836411e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:38 +0000

    Add package mabfs/0.0.4/mabfs-0.0.4-py3-none-any.whl

---
+++ packages/mabfs/mabfs-0.0.4-py3-none-any.whl/mabfs/distributions.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class BetaDistribution:
+
+    def __init__(self, a: int, b: int):
+
+        r"""
+        :param a: beta shape parameter
+        :param b: beta rate parameter
+        """
+        self.a = a
+        self.b = b
+
+    def sample(self) -> np.float:
+        return np.random.beta(self.a, self.b)
+
+    def update(self, result: int):
+        self.a += result
+        self.b += (1-result)
