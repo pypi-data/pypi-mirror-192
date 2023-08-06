d5b15c1ec76627999f18409df2f266a9fb571791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:49 +0000

    Add package Pykui-test6/1.0/Pykui_test6-1.0.tar.gz

---
+++ packages/Pykui-test6/Pykui_test6-1.0.tar.gz/Pykui_test6-1.0/README.md
@@ -0,0 +1,9 @@
+# PyKui tutorials
+## Beta 1
+### Free for everyone
+
+---
+
+All the info is in directory 'info', you have, credits, tutorials and all versions
+
+If you need help, contact to: 'alt.f4.studios.dev@gmail.com'
---
+++ packages/Pykui-test6/Pykui_test6-1.0.tar.gz/Pykui_test6-1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = "Pykui_test6",
+    version="1.0",
+    author="Paul Cannon Palacios and Pau Cava de las Heras",
+    author_email="alt.f4.studios.dev@gmail.com",
+    description="Make graphics easier and faster!",
+    long_description="PyKui is a module that is based on pygame and it makes graphics easier and faster!",
+    license="Free for everyone"
+)
