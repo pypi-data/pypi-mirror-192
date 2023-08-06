5067951823c3d127750525abae967a6bd1061941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:52 +0000

    Add package lino-noi/23.2.2/lino-noi-23.2.2.tar.gz

---
+++ packages/lino-noi/lino-noi-23.2.2.tar.gz/lino-noi-23.2.2/lino_noi/lib/users/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2015-2021 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+"""
+Lino Noi extension of :mod:`lino.modlib.users`.
+
+.. autosummary::
+   :toctree:
+
+    fixtures.demo
+    fixtures.demo2
+
+"""
+
+# from lino_xl.lib.online.users import Plugin
+from lino.modlib.users import Plugin
