0dac93747ce5f0466255d6319e5dd3dd81f35498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:09 +0000

    Add package marlin-cli/0.0.6/marlin_cli-0.0.6-py3-none-any.whl

---
+++ packages/marlin-cli/marlin_cli-0.0.6-py3-none-any.whl/marlin-cli/marlin.py
@@ -0,0 +1,18 @@
+from commands.docs import docs
+from commands.init import init
+from commands.install import install
+import click
+
+
+@click.group()
+@click.version_option("0.0.6")
+def cli():
+    pass
+
+
+cli.add_command(init)
+cli.add_command(docs)
+cli.add_command(install)
+
+if __name__ == "__main__":
+    cli()
