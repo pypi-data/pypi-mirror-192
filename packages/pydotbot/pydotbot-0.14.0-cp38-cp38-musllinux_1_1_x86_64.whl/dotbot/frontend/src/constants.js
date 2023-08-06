f258acb5d00d0c6f1b7ab11c9c70e56f86e24ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:22 +0000

    Add package pydotbot/0.14.0/pydotbot-0.14.0-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/pydotbot/pydotbot-0.14.0-cp38-cp38-musllinux_1_1_x86_64.whl/dotbot/frontend/src/constants.js
@@ -0,0 +1,14 @@
+export const ApplicationType = {
+  DotBot: 0,
+  SailBot: 1,
+};
+
+export const ControlModeType = {
+  Manual: 0,
+  Auto: 1,
+};
+
+export const maxWaypoints = 8;
+
+export const lh2_distance_threshold = 0.01;
+export const gps_distance_threshold = 10;  // 10 meters
