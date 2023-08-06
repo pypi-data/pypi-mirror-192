c415e2826a86c39fcbf4e292374f7acd576ad016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:47 +0000

    Add package shapes-recognition/1.0.6/shapes_recognition-1.0.6-py3-none-any.whl

---
+++ packages/shapes-recognition/shapes_recognition-1.0.6-py3-none-any.whl/shapes_recognition/__init__.py
@@ -0,0 +1,5 @@
+from .dist.core import clean_results, read_data
+from .dist.core import check_self_learning_data, check_recognition_data
+from .dist.core import self_learning
+from .dist.core import recognition
+from .dist.core import show_self_learning_results, show_recognition_results
