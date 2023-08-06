837322cbe3532cb83efc342dfe7ce4fc40bfef91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:42 +0000

    Add package lpneuralnet/0.0.0/lpneuralnet-0.0.0-py3-none-any.whl

---
+++ packages/lpneuralnet/lpneuralnet-0.0.0-py3-none-any.whl/lpneuralnet/__init__.py
@@ -0,0 +1,5 @@
+from lpneuralnet.andGate import runAnd
+from lpneuralnet.nandGate import runNand
+from lpneuralnet.norGate import runNor
+from lpneuralnet.notGate import runNot
+from lpneuralnet.orGate import runOr
