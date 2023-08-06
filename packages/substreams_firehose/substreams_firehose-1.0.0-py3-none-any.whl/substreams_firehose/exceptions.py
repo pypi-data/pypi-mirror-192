17f32602d2a899d6d2e9a9b13247ef7fab253e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:42 +0000

    Add package substreams_firehose/1.0.0/substreams_firehose-1.0.0-py3-none-any.whl

---
+++ packages/substreams_firehose/substreams_firehose-1.0.0-py3-none-any.whl/substreams_firehose/exceptions.py
@@ -0,0 +1,25 @@
+"""
+SPDX-License-Identifier: MIT
+
+Holds the custom exceptions defined for use by the application.
+"""
+
+class BlockStreamException(Exception):
+	"""
+	Thrown by a task when failing to process a block.
+
+	The information will be used to start another task for the remaining blocks to be processed.
+
+	Attributes:
+		start: The block stream's starting block.
+		end: The block stream's ending block.
+		failed: The block that failed processing.
+	"""
+	def __init__(self, start: int, end: int, failed: int) -> None:
+		self.start = start
+		self.end = end
+		self.failed = failed
+
+	def __str__(self) -> str:
+		return (f'Block streaming failed for block #{self.failed} in range [{self.start}, {self.end}]'
+				f' ({self.end - self.failed} blocks remaining)')
