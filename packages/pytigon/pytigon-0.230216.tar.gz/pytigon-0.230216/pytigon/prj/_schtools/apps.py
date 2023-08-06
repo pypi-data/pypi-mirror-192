b0bc4dd686409a3f8a65a71c4644d84265ceeadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:13 +0000

    Add package pytigon/0.230216/pytigon-0.230216.tar.gz

---
+++ packages/pytigon/pytigon-0.230216.tar.gz/pytigon-0.230216/pytigon/prj/_schtools/apps.py
@@ -0,0 +1,14 @@
+APPS = [
+    "schcommander",
+    "schsimplescripts",
+    "schtools",
+    "schattachements",
+    "schlog",
+    "schauth",
+    "schtasks",
+    "schworkflow",
+    "_schdata.schelements",
+]
+APPS_EXT = []
+PUBLIC = True
+MAIN_PRJ = False
