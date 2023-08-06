f727fb84af7a2e8cccc419a791c16a52941fec2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:38 +0000

    Add package jaypore-ci/0.2.8/jaypore_ci-0.2.8.tar.gz

---
+++ packages/jaypore-ci/jaypore_ci-0.2.8.tar.gz/jaypore_ci-0.2.8/jaypore_ci/interfaces.py
@@ -0,0 +1,119 @@
+"""
+Defines interfaces for remotes and executors.
+
+Currently only gitea and docker are supported as remote and executor
+respectively.
+"""
+from enum import Enum
+from typing import NamedTuple
+
+
+class TriggerFailed(Exception):
+    "Failure to trigger a job"
+
+
+class RemoteApiFailed(Exception):
+    "Failure while working with a remote"
+
+
+class JobStatus(NamedTuple):
+    is_running: bool
+    exit_code: int
+    logs: str
+    started_at: str
+    finished_at: str
+
+
+class Status(Enum):
+    "Each pipeline can ONLY be in any one of these statuses"
+    PENDING = 10
+    RUNNING = 30
+    FAILED = 40
+    PASSED = 50
+    TIMEOUT = 60
+    SKIPPED = 70
+
+
+class Executor:
+    """
+    An executor is something used to run a job.
+    It could be docker / podman / shell etc.
+
+    It must define `__enter__` and `__exit__` so that it can be used as a context manager.
+
+    """
+
+    def run(self, job: "Job") -> str:
+        "Run a job and return it's ID"
+        raise NotImplementedError()
+
+    def __init__(self):
+        self.pipe_id = None
+        self.pipeline = None
+
+    def set_pipeline(self, pipeline: "Pipeline") -> None:
+        """Set the current pipeline to the given one."""
+        self.pipe_id = id(pipeline)
+        self.pipeline = pipeline
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        On exit the executor must clean up any pending / stuck / zombie jobs that are still there.
+        """
+
+    def get_status(self, run_id: str) -> JobStatus:
+        """
+        Returns the status of a given run.
+        """
+        raise NotImplementedError()
+
+
+class Remote:
+    """
+    Something that allows us to show other people the status of the CI job.
+    It could be gitea / github / gitlab / email system.
+
+    Must define `__enter__` and `__exit__` so that it can be used as a context
+    manager.
+    """
+
+    def __init__(self, *, sha, branch):
+        self.sha = sha
+        self.branch = branch
+
+    def publish(self, report: str, status: str):
+        """
+        Publish this report somewhere.
+        """
+        raise NotImplementedError()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    @classmethod
+    def from_env(cls):
+        """
+        This function should create a Remote instance from the given environment.
+        It can read git information / look at environment variables etc.
+        """
+        raise NotImplementedError()
+
+
+class Reporter:
+    """
+    Something that generates the status of a pipeline.
+
+    It can be used to generate reports in markdown, plaintext, html, pdf etc.
+    """
+
+    def render(self, pipeline: "Pipeline") -> str:
+        """
+        Render a report for the pipeline.
+        """
+        raise NotImplementedError()
---
+++ packages/jaypore-ci/jaypore_ci-0.2.8.tar.gz/jaypore_ci-0.2.8/jaypore_ci/remotes/gitea.py
@@ -0,0 +1,173 @@
+"""
+A gitea remote git host.
+
+This is used to report pipeline status to the remote.
+"""
+import os
+import subprocess
+from pathlib import Path
+from urllib.parse import urlparse
+
+import requests
+
+from jaypore_ci.interfaces import Remote, RemoteApiFailed
+from jaypore_ci.logging import logger
+
+
+class Gitea(Remote):  # pylint: disable=too-many-instance-attributes
+    """
+    The remote implementation for gitea.
+    """
+
+    @classmethod
+    def from_env(cls):
+        """
+        Creates a remote instance from the environment.
+        It will:
+
+            - Find the remote location using `git remote`.
+            - Find the current branch
+            - Create a new pull request for that branch
+            - Allow posting updates using the gitea token provided
+        """
+        remote = (
+            subprocess.check_output(
+                "git remote -v | grep push | awk '{print $2}'", shell=True
+            )
+            .decode()
+            .strip()
+        )
+        assert "https://" in remote, "Only https remotes supported"
+        assert ".git" in remote
+        remote = urlparse(remote)
+        branch = (
+            subprocess.check_output(
+                r"git branch | grep \* | awk '{print $2}'", shell=True
+            )
+            .decode()
+            .strip()
+        )
+        os.environ["JAYPORE_COMMIT_BRANCH"] = branch
+        sha = subprocess.check_output("git rev-parse HEAD", shell=True).decode().strip()
+        os.environ["JAYPORE_COMMIT_SHA"] = sha
+        owner = Path(remote.path).parts[1]
+        repo = Path(remote.path).parts[2].replace(".git", "")
+        token = os.environ["JAYPORE_GITEA_TOKEN"]
+        return cls(
+            root=f"{remote.scheme}://{remote.netloc}",
+            owner=owner,
+            repo=repo,
+            branch=branch,
+            token=token,
+            sha=sha,
+        )
+
+    def __init__(
+        self, *, root, owner, repo, token, **kwargs
+    ):  # pylint: disable=too-many-arguments
+        super().__init__(**kwargs)
+        # --- customer
+        self.root = root
+        self.api = f"{root}/api/v1"
+        self.owner = owner
+        self.repo = repo
+        self.token = token
+        self.timeout = 10
+        self.base_branch = "main"
+
+    def logging(self):
+        """
+        Return's a logging instance with information about gitea bound to it.
+        """
+        return logger.bind(
+            root=self.root, owner=self.owner, repo=self.repo, branch=self.branch
+        )
+
+    def get_pr_id(self):
+        """
+        Returns the pull request ID for the current branch.
+        """
+        r = requests.post(
+            f"{self.api}/repos/{self.owner}/{self.repo}/pulls",
+            params={"access_token": self.token},
+            timeout=self.timeout,
+            json={
+                "base": self.base_branch,
+                "body": "Branch auto created by JayporeCI",
+                "head": self.branch,
+                "title": self.branch,
+            },
+        )
+        self.logging().debug("Get PR Id", status_code=r.status_code)
+        if r.status_code == 409:
+            return r.text.split("issue_id:")[1].split(",")[0].strip()
+        if r.status_code == 201:
+            return self.get_pr_id()
+        if r.status_code == 404 and r.json()["message"] == "IsBranchExist":
+            self.base_branch = "develop"
+            return self.get_pr_id()
+        self.logging().debug()(
+            "Failed gitea api",
+            api=self.api,
+            owner=self.owner,
+            repo=self.repo,
+            token=self.token,
+            branch=self.branch,
+            status=r.status_code,
+            response=r.text,
+        )
+        raise RemoteApiFailed(r)
+
+    def publish(self, report: str, status: str):
+        """
+        Will publish the report to the remote.
+
+        :param report: Report to write to remote.
+        :param status: One of ["pending", "success", "error", "failure",
+            "warning"] This is the dot next to each commit in gitea.
+        """
+        assert status in ("pending", "success", "error", "failure", "warning")
+        issue_id = self.get_pr_id()
+        # Get existing PR body
+        r = requests.get(
+            f"{self.api}/repos/{self.owner}/{self.repo}/pulls/{issue_id}",
+            timeout=self.timeout,
+            params={"access_token": self.token},
+        )
+        self.logging().debug("Get existing body", status_code=r.status_code)
+        assert r.status_code == 200
+        body = r.json()["body"]
+        body = (line for line in body.split("\n"))
+        prefix = []
+        for line in body:
+            if "```jayporeci" in line:
+                prefix = prefix[:-1]
+                break
+            prefix.append(line)
+        while prefix and prefix[-1].strip() == "":
+            prefix = prefix[:-1]
+        prefix.append("")
+        # Post new body with report
+        report = "\n".join(prefix) + "\n" + report
+        r = requests.patch(
+            f"{self.api}/repos/{self.owner}/{self.repo}/pulls/{issue_id}",
+            data={"body": report},
+            timeout=self.timeout,
+            params={"access_token": self.token},
+        )
+        self.logging().debug("Published new report", status_code=r.status_code)
+        # Set commit status
+        r = requests.post(
+            f"{self.api}/repos/{self.owner}/{self.repo}/statuses/{self.sha}",
+            json={
+                "context": "JayporeCi",
+                "description": f"Pipeline status is: {status}",
+                "state": status,
+                "target_url": f"{self.root}/{self.owner}/{self.repo}/pulls/{issue_id}",
+            },
+            timeout=self.timeout,
+            params={"access_token": self.token},
+        )
+        self.logging().debug(
+            "Published new status", status=status, status_code=r.status_code
+        )
