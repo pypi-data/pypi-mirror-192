0712ad7fe47017a46b278c3d02887bb78ebcb9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:44 +0000

    Add package grads/0.0.1/grads-0.0.1.tar.gz

---
+++ packages/grads/grads-0.0.1.tar.gz/grads-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='grads',
+    version='0.0.1',
+    description='Grads',
+    long_description='',
+    url='https://github.com/MaximilienLC/gran',
+    author='Maximilien Le Clei',
+    author_email='maximilienleclei@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
