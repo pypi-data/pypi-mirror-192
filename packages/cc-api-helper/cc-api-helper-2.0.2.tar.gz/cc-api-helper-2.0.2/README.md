51a30b2108bf8500b76b49eb1aa89fa446234a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:22 +0000

    Add package cc-api-helper/2.0.2/cc-api-helper-2.0.2.tar.gz

---
+++ packages/cc-api-helper/cc-api-helper-2.0.2.tar.gz/cc-api-helper-2.0.2/README.md
@@ -0,0 +1,17 @@
+# cc-api-helper
+
+
+
+## Requirement
+
+Python >=3.7
+
+## Installation
+
+```bash
+pip install cc-api-helper
+```
+
+## License
+
+MIT
---
+++ packages/cc-api-helper/cc-api-helper-2.0.2.tar.gz/cc-api-helper-2.0.2/api_helper/__init__.py
@@ -0,0 +1,10 @@
+from .utils import *
+# from .importers import *
+from .captcha import *
+from .client import *
+
+__all__ = [
+    # 'import_win_lose', 'import_ticket',
+    'get_uri',
+    'get_base_url', 'captcha_solver', 'login_required', 'BaseClient'
+]
