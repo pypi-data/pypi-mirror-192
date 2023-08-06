bc4e9bc2a4957ee49bdd2cade03823055e2af4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:06 +0000

    Add package vcs-p-pack/0.0.2/vcs_p_pack-0.0.2.tar.gz

---
+++ packages/vcs-p-pack/vcs_p_pack-0.0.2.tar.gz/vcs_p_pack-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vcs_p_pack',
+    version='0.0.2',
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': [
+            'parse_work=package.vcs_p:parse_work'
+        ]
+    },
+    package_dir={"": "package"},
+    packages=find_packages(where="package"),
+)
