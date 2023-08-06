d50e48c801f721cd61f8a257a108f3c4b7f906b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:15 +0000

    Add package gstorm/0.10.0/gstorm-0.10.0-py3.11.egg

---
+++ packages/gstorm/gstorm-0.10.0-py3.11.egg/gstorm/__init__.py
@@ -0,0 +1,16 @@
+# from .gstorm import mainClass
+from .__version__ import __version__
+from . import cli
+from . import helpers
+from .BaseGraphQLType import BaseGraphQLType
+from .GraphQLType import GraphQLType
+from .QueryBuilder import QueryBuilder
+from .MutationBuilder import MutationBuilder
+from ._mutation_runners import (
+    create,
+    save_multi_create,
+    update
+)
+
+# * Package name:
+name = 'gstorm'
