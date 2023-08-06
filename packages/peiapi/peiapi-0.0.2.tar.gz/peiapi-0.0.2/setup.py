2d0c34f681e40e77b5d65a5eb325f5ec86ef5864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:29 +0000

    Add package peiapi/0.0.2/peiapi-0.0.2.tar.gz

---
+++ packages/peiapi/peiapi-0.0.2.tar.gz/peiapi-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+# 若Discription.md中有中文 須加上 encoding="utf-8"
+#with open("Discription.md", "r",encoding="utf-8") as f:
+#    long_description = f.read()
+    
+setup(
+    name = "peiapi",
+    version = "0.0.2",
+    author = "pei-jan",
+    author_email="orangepower856@hotmail.com",
+    description="call trade function",
+    packages=find_packages(),
+    #license=
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    #url="https://github.com/seanbbear/VerdictCut",                                         packages=setuptools.find_packages(),     
+    #classifiers=[
+    #    "Programming Language :: Python :: 3",
+    #    "License :: OSI Approved :: MIT License",
+    #    "Operating System :: OS Independent",
+    #],
+    #python_requires='>=3.6'
+    )
