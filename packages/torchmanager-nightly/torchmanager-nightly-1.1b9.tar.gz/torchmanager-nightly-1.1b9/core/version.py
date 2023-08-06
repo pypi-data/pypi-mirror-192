3d53e322a98cef884a1cc4d1d53d37cdde50a1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:52 +0000

    Add package torchmanager-nightly/1.1b9/torchmanager-nightly-1.1b9.tar.gz

---
+++ packages/torchmanager-nightly/torchmanager-nightly-1.1b9.tar.gz/torchmanager-nightly-1.1b9/core/version.py
@@ -0,0 +1,24 @@
+from .view import warnings
+
+CURRENT: str = "v1.1b9"
+DESCRIPTION: str = "PyTorch Training Manager v1.1 (Beta 9)"
+
+class VersionError(SystemError):
+    def __init__(self, method_name: str, maximum_supported_version: str) -> None:
+        super().__init__(f"`{method_name}` has been deprecated and removed from version {maximum_supported_version}.")
+
+def deprecated(target_version: str, removing_version: str):
+    '''
+    Deprecated decorator function
+
+    - Parameters:
+        - target_version: A `str` of version for the deprecation
+        - remove_version: A `str` of version for removing
+    '''
+    # define wrapping function
+    def wrapping_fn(fn):
+        if CURRENT >= removing_version: raise VersionError(fn.__name__, removing_version)
+        elif CURRENT >= target_version: warnings.warn(f"{fn.__name__} has been deprecated from {target_version} and will be removed from {removing_version}", DeprecationWarning)
+        else: warnings.warn(f"{fn} will be deprecated from {target_version} and removed from {removing_version}", PendingDeprecationWarning)
+        return fn
+    return wrapping_fn
