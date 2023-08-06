f77e84c7a79f74b03df6eb47b0065297e7bfddeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:26 +0000

    Add package uiuc-490-sp23/0.1.1/uiuc_490_sp23-0.1.1-py3-none-any.whl

---
+++ packages/uiuc-490-sp23/uiuc_490_sp23-0.1.1-py3-none-any.whl/uiuc_490_sp23/__main__.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print("The parent module is not intended to be ran directly.")
+    print("Please run:\n\tpython3 -m uiuc-490-sp23.assignment<n>\nwhere <n> is the assignment number.")
+    print("Current assignments available are:")
+    print("    assignment1")
