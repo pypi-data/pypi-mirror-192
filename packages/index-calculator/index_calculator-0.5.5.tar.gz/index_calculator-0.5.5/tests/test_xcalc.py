3ec9b736fdf0cd8c215cccae44218d77325531b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:57 +0000

    Add package index-calculator/0.5.5/index_calculator-0.5.5.tar.gz

---
+++ packages/index-calculator/index_calculator-0.5.5.tar.gz/index_calculator-0.5.5/tests/test_xcalc.py
@@ -0,0 +1,59 @@
+import pytest  # noqa
+from pyhomogenize import open_xrdataset
+
+import index_calculator as xcalc
+from index_calculator import test_netcdf
+
+
+def test_processing():
+    tas_ds = open_xrdataset(test_netcdf["tas"])
+    preproc = xcalc.preprocessing(tas_ds, freq="week")
+    proc = xcalc.processing("TG", preproc_obj=preproc)
+    postproc = xcalc.postprocessing(
+        project="CORDEX",
+        proc_obj=proc,
+        institution="test institution",
+        institution_id="TEST",
+        contact="test@test.de",
+    )
+    xcalc.outputwriter(postproc_obj=postproc)
+
+
+def test_index_calculator():
+    tas_ds = open_xrdataset(test_netcdf["tas"])
+    xcalc.index_calculator(
+        ds=tas_ds,
+        freq="week",
+        index="TG",
+        project="CORDEX",
+        institution="test institution",
+        institution_id="TEST",
+        contact="test@test.de",
+        write=True,
+    )
+
+
+def test_thresh_index_calculator():
+    pr_ds = open_xrdataset(test_netcdf["pr"])
+    xcalc.index_calculator(
+        ds=pr_ds,
+        freq="week",
+        index="RX3day",
+        project="CORDEX",
+        institution_id="TEST",
+    )
+    xcalc.index_calculator(
+        ds=pr_ds,
+        freq="week",
+        index="RXYYday",
+        thresh=3,
+        project="CORDEX",
+        institution_id="TEST",
+    )
+    xcalc.index_calculator(
+        ds=pr_ds,
+        freq="week",
+        index="RXYYday",
+        project="CORDEX",
+        institution_id="TEST",
+    )
