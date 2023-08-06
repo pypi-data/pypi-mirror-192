a251d93fb967fda9ae22a61378852c61bd148ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:16 +0000

    Add package kretanje-cijena/0.0.2/kretanje_cijena-0.0.2-py3-none-any.whl

---
+++ packages/kretanje-cijena/kretanje_cijena-0.0.2-py3-none-any.whl/kretanje_cijena/gunzip_importer.py
@@ -0,0 +1,19 @@
+from gzip import decompress as _decompress
+from json import loads as _loads
+from typing import Any as _Any
+from typing import Dict as _Dict
+from urllib.parse import urljoin as _urljoin
+
+from brds import Importer as _Importer
+from requests import get as _get
+
+
+class GunzipImporter(_Importer):
+    def __init__(self: "GunzipImporter", domain: str) -> None:
+        self.domain = domain
+
+    def get(self: "GunzipImporter", url: str) -> _Dict[str, _Any]:
+        response = _get(_urljoin(self.domain, url), verify=False)
+        data = _decompress(response.content)
+        ret: _Dict[str, _Any] = _loads(data)
+        return ret
