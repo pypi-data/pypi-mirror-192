14385409761697f6c2ce0bf686aa65d66b0d5e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:38 +0000

    Add package ValiotWorker/5.0.1/ValiotWorker-5.0.1.tar.gz

---
+++ packages/ValiotWorker/ValiotWorker-5.0.1.tar.gz/ValiotWorker-5.0.1/ValiotWorker/__main__.py
@@ -0,0 +1,63 @@
+import os
+from pprint import pprint
+from pygqlc import GraphQLClient
+from .worker import ValiotWorker, QueueType, PollingMode, JobConfigMode
+# ! Create main helpers:
+gql = GraphQLClient()
+vw = ValiotWorker()
+# * Initialize helpers:
+gql.addEnvironment(
+    'dev',
+    url=os.environ.get('API'),
+    wss=os.environ.get('WSS'),
+    headers={'Authorization': os.environ.get('TOKEN')},
+    default=True)
+vw.setClient(gql)
+vw.setWorker(os.environ.get('WORKER'))
+vw.setPollingMode(PollingMode.SUBSCRIPTION)
+vw.setJobConfigMode(JobConfigMode.SYNC)
+
+
+@vw.job(
+    name='TEST_JOB',
+    alias='test job 1',
+    description='',
+    schedule='* * * * *',
+    enabled=True,
+    queueType=QueueType.FREQUENCY,
+)
+def test_job(job_id, update_job, **_):
+    print("Hi, I'm test job #1")
+    update_job({
+        'id': job_id,
+        'status': 'FINISHED',
+        'output': '{}',
+        'progress': 100
+    })
+
+
+@vw.job(
+    name='TEST_JOB_2',
+    alias='test job 2',
+    description='',
+    schedule='*/3 * * * *',
+    enabled=True,
+    queueType=QueueType.FREQUENCY,
+)
+def test_job_2(job_id, update_job, **_):
+    print("Hi, I'm test job #2")
+    update_job({
+        'id': job_id,
+        'status': 'FINISHED',
+        'output': '{}',
+        'progress': 100
+    })
+
+
+def main():
+    print('main for valiot worker')
+    vw.run(interval=5.0)
+
+
+if __name__ == "__main__":
+    main()
