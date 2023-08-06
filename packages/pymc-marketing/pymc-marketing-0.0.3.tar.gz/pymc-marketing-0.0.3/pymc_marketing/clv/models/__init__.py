951c66f4f5d44c78dd295e12f3b4ef802ecf344d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:55 +0000

    Add package pymc-marketing/0.0.3/pymc-marketing-0.0.3.tar.gz

---
+++ packages/pymc-marketing/pymc-marketing-0.0.3.tar.gz/pymc-marketing-0.0.3/pymc_marketing/clv/models/__init__.py
@@ -0,0 +1,13 @@
+from pymc_marketing.clv.models.basic import CLVModel
+from pymc_marketing.clv.models.beta_geo import BetaGeoModel
+from pymc_marketing.clv.models.gamma_gamma import (
+    GammaGammaModel,
+    GammaGammaModelIndividual,
+)
+
+__all__ = (
+    "CLVModel",
+    "GammaGammaModel",
+    "GammaGammaModelIndividual",
+    "BetaGeoModel",
+)
