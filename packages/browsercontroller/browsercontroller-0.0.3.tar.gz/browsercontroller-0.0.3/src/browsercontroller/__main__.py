7688aca3e9428d7d83ea8aa3254b70daf17a6e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:55 +0000

    Add package browsercontroller/0.0.3/browsercontroller-0.0.3.tar.gz

---
+++ packages/browsercontroller/browsercontroller-0.0.3.tar.gz/browsercontroller-0.0.3/src/browsercontroller/__main__.py
@@ -0,0 +1,5 @@
+"""Entry point for the project."""
+
+from browsercontroller.get_controller import get_ubuntu_apt_firefox_controller
+
+get_ubuntu_apt_firefox_controller("https://www.startpagina.nl")
