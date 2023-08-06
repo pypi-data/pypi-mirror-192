24ba430dbe13c402eaccdd6d09a36951b9207cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:37 +0000

    Add package pycodegrade/0.0.3/pycodegrade-0.0.3.tar.gz

---
+++ packages/pycodegrade/pycodegrade-0.0.3.tar.gz/pycodegrade-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycodegrade',
+    version='0.0.3',
+    description='PYPI pycodegrade for Python',
+    author='teddylee777',
+    author_email='teddylee777@gmail.com',
+    url='https://github.com/teddylee777/pycodegrade',
+    install_requires=['pyairtable', 'pandas', 'scikit-learn', 'matplotlib', 'seaborn'],
+    packages=find_packages(exclude=[]),
+    keywords=['pycodegrade', 'codegrade', 'python'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
