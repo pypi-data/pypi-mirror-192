837ff097ebcf2441cab1c91309dc5d663d7efa39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:25 +0000

    Add package etsy-searcher/1.0.2/etsy-searcher-1.0.2.tar.gz

---
+++ packages/etsy-searcher/etsy-searcher-1.0.2.tar.gz/etsy-searcher-1.0.2/etsy_searcher/models/Result.py
@@ -0,0 +1,29 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+BADGES = {
+    "pop": "Popular Now",
+    "frs": "Free Shipping",
+    "sts": "Star Seller",
+    "pro": "Promotion",
+    "bes": "Best Seller",
+    "cns": "Low Stock"
+}
+
+class Listing(BaseModel):
+    listing_id: int
+    badges: Optional[List[str]]
+
+    def __init__(__pydantic_self__, **data) -> None:
+        badges = data.get("badges")
+        if badges:
+            data["badges"] = [BADGES.get(badge) for badge in badges]
+        super().__init__(**data)
+    
+class Result(BaseModel):
+    organic_listings: List[Listing]
+    ad_listings: List[Listing]
+    
+    @property
+    def all_listings(self):
+        return self.organic_listings + self.ad_listings
---
+++ packages/etsy-searcher/etsy-searcher-1.0.2.tar.gz/etsy-searcher-1.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(
+    name="etsy-searcher",
+    version="1.0.2",
+    author="Esat Yılmaz",
+    author_email="esatyilmaz3500@gmail.com",
+    description="Etsy Search API",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        "pydantic", "requests"
+    ]
+)
