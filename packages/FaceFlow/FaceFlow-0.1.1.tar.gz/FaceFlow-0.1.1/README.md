b4531328f6d38d3e18ee0b6cf86436e25a95d0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:58 +0000

    Add package FaceFlow/0.1.1/FaceFlow-0.1.1.tar.gz

---
+++ packages/FaceFlow/FaceFlow-0.1.1.tar.gz/FaceFlow-0.1.1/README.md
@@ -0,0 +1,9 @@
+# FaceFlow
+
+Recognizes faces and trains models, brings in the pictures and provides identification predictions and face classification. It also performs semi-supervised learning.
+
+## Feature
+
+-   Recognize faces.
+-   Train the model through semi-supervised learning with labeled or unlabeled pictures.
+-   It provides identification predictions and face classification for pictures
---
+++ packages/FaceFlow/FaceFlow-0.1.1.tar.gz/FaceFlow-0.1.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+requirements = [
+    'numpy',
+    'scikit-learn',
+    'dlib',
+    'face_recognition',
+    'pickle',
+]
+
+setuptools.setup(
+    name='FaceFlow',
+    version='0.1.1',
+    author='Jaewook Lee',
+    author_email='me@jwlee.xyz',
+    description='Face Classification package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/asheswook/FaceFlow',
+    project_urls={
+        'Bug Tracker': 'https://github.com/asheswook/FaceFlow/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'FaceFlow'},
+    package=setuptools.find_packages(where='FaceFlow'),
+    python_requires='>=3.7',
+    install_requires=requirements,
+)
