bfd05ba310c47a8f55dee144811cc2f57bbac62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:53 +0000

    Add package chorder-py/1.0.1/chorder-py-1.0.1.tar.gz

---
+++ packages/chorder-py/chorder-py-1.0.1.tar.gz/chorder-py-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chorder-py",
+    version="1.0.1",
+    author="TuneFlow",
+    author_email="contact@info.tuneflow.com",
+    description="A modified version of chorder with drums fix",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuneflow/chorder-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
