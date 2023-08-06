0ac76258455c2f99cd6fa2672cf96eaffcefede7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:19 +0000

    Add package lab1-test/1.0.99/lab1_test-1.0.99-py3-none-any.whl

---
+++ packages/lab1-test/lab1_test-1.0.99-py3-none-any.whl/canisius_cyb600_example/main.py
@@ -0,0 +1,24 @@
+import http.server
+import socketserver
+import datetime
+
+def start_server():
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            now = datetime.datetime.now()
+            self.wfile.write(bytes(f'The current date and time is {now}', 'utf-8'))
+
+    PORT = 8080
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"serving at port {PORT}")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+    print("Server stopped.")
+
