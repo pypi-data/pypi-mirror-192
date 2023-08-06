8f852dca982c0434e53cff51de1836c241314330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:26 +0000

    Add package miditoolkit-light/0.1.17/miditoolkit_light-0.1.17.tar.gz

---
+++ packages/miditoolkit-light/miditoolkit_light-0.1.17.tar.gz/miditoolkit_light-0.1.17/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='miditoolkit_light',
+    version='0.1.17',
+    description='',
+    author='TuneFlow',
+    author_email='zaoyinji@gmail.com',
+    url='https://github.com/tuneflow/miditoolkit',
+    packages=find_packages(),
+    package_data={'': ['examples_data/*']},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Sound/Audio :: MIDI",
+    ],
+    keywords='music midi mir',
+
+    license='MIT',
+    install_requires=[
+        'numpy >= 1.7.0',
+        'mido >= 1.1.16',
+    ]
+)
+
+
+"""
+python setup.py sdist
+twine upload dist/*
+"""
