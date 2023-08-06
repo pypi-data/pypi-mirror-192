f055c7f75cd56289bbd5bd73b9e8ecf88b4f8c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:54 +0000

    Add package freqprob/0.1.0/freqprob-0.1.0.tar.gz

---
+++ packages/freqprob/freqprob-0.1.0.tar.gz/freqprob-0.1.0/freqprob/__init__.py
@@ -0,0 +1,35 @@
+"""
+freqprob __init__.py
+"""
+
+# Version of the ngesh package
+__version__ = "0.1.0"  # Remember to sync with setup.py
+__author__ = "Tiago Tresoldi"
+__email__ = "tiago.tresoldi@lingfil.uu.se"
+
+
+# Import from local modules
+from .freqprob import (
+    ELE,
+    MLE,
+    CertaintyDegree,
+    Laplace,
+    Lidstone,
+    Random,
+    SimpleGoodTuring,
+    Uniform,
+    WittenBell,
+)
+
+# Build the namespace
+__all__ = [
+    "Uniform",
+    "Random",
+    "MLE",
+    "Lidstone",
+    "Laplace",
+    "ELE",
+    "WittenBell",
+    "CertaintyDegree",
+    "SimpleGoodTuring",
+]
