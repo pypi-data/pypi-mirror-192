a085212d14d67777ffb1c18169547eb5d73752c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:43 +0000

    Add package defectDetector/0.1.0/defectDetector-0.1.0.tar.gz

---
+++ packages/defectDetector/defectDetector-0.1.0.tar.gz/defectDetector-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='defectDetector',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Opencv library for morphological transformation and contours',
+    author='Maxelerator',
+    license='Maxelerator Foundation',
+)
