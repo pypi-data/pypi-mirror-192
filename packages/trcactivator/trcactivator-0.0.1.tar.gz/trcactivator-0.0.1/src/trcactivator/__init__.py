8f737b3f6144b543310105fee65211407213aef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:21 +0000

    Add package trcactivator/0.0.1/trcactivator-0.0.1.tar.gz

---
+++ packages/trcactivator/trcactivator-0.0.1.tar.gz/trcactivator-0.0.1/src/trcactivator/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+#
+#     d88P     d88P          888b    888          888
+#      d88P   d88P           8888b   888          888
+#       d88P d88P            88888b  888          888
+#        d88888P    888888   888Y88b 888  .d88b.  888888
+#        d88888P    888888   888 Y88b888 d8P  Y8b 888
+#       d88P d88P            888  Y88888 88888888 888
+#      d88P   d88P           888   Y8888 Y8b.     Y88b.
+#     d88P     d88P          88.8    Y888  "Y8888   "Y888
+#
+#                      © Copyright 2023
+#                    https://x-net.pp.ua
+#                 https://github.com/Conradk10
+#
+#                 Licensed under the GNU GPLv3
+#          https://www.gnu.org/licenses/agpl-3.0.html
+#
+
+"""This package is designed to automate the activation of multi-checks from the @tonRocketBot bot
+from multiple Telegram accounts. The package uses the Telethon library to interact with the Telegram API."""
+
+from .utils import (
+    activate_multicheque,
+    parse_url,
+)
