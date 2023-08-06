ad1bd4a3d350f7b7e7195a87add3db9ce77d29e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:36 +0000

    Add package altair-express/0.1.25/altair_express-0.1.25-py2.py3-none-any.whl

---
+++ packages/altair-express/altair_express-0.1.25-py2.py3-none-any.whl/altair_express/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for Altair Express."""
+
+__author__ = """Dylan Wootton"""
+__email__ = 'dwootton@mit.edu'
+__version__ = '0.1.25'
+
+from .jointplot import *
+from .pairplot import *
+from .distributional import *
+from .relational import *
+from .data_table import *
+from .interactions import *
