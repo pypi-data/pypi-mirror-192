9496f193e2f99483ea36b0e6824539f32d9b2bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:00 +0000

    Add package hapdoc/0.5/hapdoc-0.5.tar.gz

---
+++ packages/hapdoc/hapdoc-0.5.tar.gz/hapdoc-0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open('./readme.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hapdoc',
+    description='autodoc tool for everything',
+    long_description=long_description,
+    author='Ethosa',
+    author_email='social.ethosa@gmail.com',
+    maintainer='HapticX',
+    maintainer_email='hapticx.company@gmail.com',
+    url='https://github.com/HapticX/hapdoc',
+    version='0.5',
+    packages=find_packages(),
+    py_modules=['hapdoc'],
+    install_requires=['click', 'colorama', 'fastapi', 'uvicorn'],
+    entry_points={
+        'console_scripts': [
+            'hapdoc = hapdoc.hapdoc:hapdoc'
+        ]
+    },
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: FastAPI',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation'
+    ]
+)
