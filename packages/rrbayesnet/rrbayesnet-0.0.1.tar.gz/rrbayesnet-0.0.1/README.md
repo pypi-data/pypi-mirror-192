c5d628f6a07d3a9694a1ba62c6f14ff8c4e3cac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:24 +0000

    Add package rrbayesnet/0.0.1/rrbayesnet-0.0.1.tar.gz

---
+++ packages/rrbayesnet/rrbayesnet-0.0.1.tar.gz/rrbayesnet-0.0.1/README.md
@@ -0,0 +1,10 @@
+# lab-2-ia
+
+## Bayesian Network Python Implementation
+
+### Author: Roberto Rios
+
+### Version: Alpha 0.0.1 (not even functional)
+
+first upload to pip just, still finishing it
+Dont use untill this description includes examples
---
+++ packages/rrbayesnet/rrbayesnet-0.0.1.tar.gz/rrbayesnet-0.0.1/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="rrbayesnet",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Python bayesian network implementation.",
+    url="https://github.com/robertriosm/lab-2-ia",
+    author="Roberto Rios",
+    author_email="rio20979@uvg.edu.gt",
+    license="MIT"
+)
