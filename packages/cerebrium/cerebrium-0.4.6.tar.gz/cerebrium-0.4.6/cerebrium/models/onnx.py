8e09ed24872b23952e5b2045066f35fbe99e7b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:14 +0000

    Add package cerebrium/0.4.6/cerebrium-0.4.6.tar.gz

---
+++ packages/cerebrium/cerebrium-0.4.6.tar.gz/cerebrium-0.4.6/cerebrium/models/onnx.py
@@ -0,0 +1,11 @@
+from cerebrium.models.base import BaseModel
+
+
+class OnnxModel(BaseModel):
+    def __init__(self, model):
+        super().__init__(model)
+        self.output_names = [output.name for output in self.model.get_outputs()]
+
+    def predict(self, onnx_input: dict) -> list:
+        res = self.model.run(self.output_names, onnx_input)
+        return res[0].tolist()
