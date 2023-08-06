397eb8406762fa39ef80c3b247bd8dcc42f9a925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:31 +0000

    Add package marlin-cli/0.0.11/marlin_cli-0.0.11.tar.gz

---
+++ packages/marlin-cli/marlin_cli-0.0.11.tar.gz/marlin_cli-0.0.11/marlin_cli/marlin.py
@@ -0,0 +1,18 @@
+from marlin_cli.commands.docs import docs
+from marlin_cli.commands.init import init
+from marlin_cli.commands.install import install
+import click
+
+
+@click.group()
+@click.version_option("0.0.11")
+def cli():
+    pass
+
+
+cli.add_command(init)
+cli.add_command(docs)
+cli.add_command(install)
+
+if __name__ == "__main__":
+    cli()
