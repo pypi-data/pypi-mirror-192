4f5533c9f955f5d6cd61bae104d64904382533b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:52 +0000

    Add package chrisbase/0.1.5/chrisbase-0.1.5.tar.gz

---
+++ packages/chrisbase/chrisbase-0.1.5.tar.gz/chrisbase-0.1.5/src/chrisbase/time.py
@@ -0,0 +1,23 @@
+from datetime import *
+
+
+def elasped_sec(x, *args, **kwargs):
+    t1 = datetime.now()
+    return x(*args, **kwargs), datetime.now() - t1
+
+
+def now(fmt='[%m.%d %H:%M:%S]', prefix=None):
+    if prefix:
+        return f"{prefix} {datetime.now().strftime(fmt)}"
+    else:
+        return datetime.now().strftime(fmt)
+
+
+def from_timestamp(stamp, fmt='%Y/%m/%d %H:%M:%S'):
+    return datetime.fromtimestamp(stamp, tz=timezone.utc).astimezone().strftime(fmt)
+
+
+def str_delta(x: timedelta):
+    mm, ss = divmod(x.total_seconds(), 60)
+    hh, mm = divmod(mm, 60)
+    return f"{hh:02.0f}:{mm:02.0f}:{ss:06.3f}"
