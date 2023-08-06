2b6e610da8007edddb588729c15a828a7426c134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:16 +0000

    Add package nonebot-plugin-resolver/1.0.2/nonebot_plugin_resolver-1.0.2.tar.gz

---
+++ packages/nonebot-plugin-resolver/nonebot_plugin_resolver-1.0.2.tar.gz/nonebot_plugin_resolver-1.0.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot-plugin-resolver']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.7,<4.0',
+ 'beautifulsoup4>=4.11,<5.0',
+ 'httpx>=0.23,<0.24',
+ 'lxml>=4.9,<5.0',
+ 'nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0',
+ 'nonebot2>=2.0.0-beta.5,<3.0.0',
+ 'tweepy>=4.12,<5.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-resolver',
+    'version': '1.0.2',
+    'description': 'NoneBot2的解析视频链接/小程序插件，发送 tiktok、bilibili、twitter等可以让QQ发送响应的图片、视频',
+    'long_description': '<div align="center">\n  <a href="https://v2.nonebot.dev/store"><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/nbp_logo.png" width="180" height="180" alt="NoneBotPluginLogo"></a>\n  <br>\n  <p><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/NoneBotPlugin.svg" width="240" alt="NoneBotPluginText"></p>\n</div>\n\n<div align="center">\n\n# nonebot-plugin-resolver\n\n_✨ NoneBot2 解析视频链接/小程序插件 ✨_\n\n\n<a href="./LICENSE">\n    <img src="https://img.shields.io/github/license/owner/nonebot-plugin-example.svg" alt="license">\n</a>\n<a href="https://pypi.python.org/pypi/nonebot-plugin-example">\n    <img src="https://img.shields.io/pypi/v/nonebot-plugin-example.svg" alt="pypi">\n</a>\n<img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="python">\n\n</div>\n\n## 📖 介绍\n\n这是一个 nonebot2 插件，解析视频链接/小程序插件，发送 tiktok、bilibili、twitter等可以让QQ发送响应的图片、视频\n\n## 💿 安装\n\n<details>\n<summary>使用 nb-cli 安装</summary>\n在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装\n\n    nb plugin install nonebot-plugin-resolver\n\n</details>\n\n<details>\n<summary>使用包管理器安装</summary>\n在 nonebot2 项目的插件目录下, 打开命令行, 根据你使用的包管理器, 输入相应的安装命令\n\n<details>\n<summary>pip</summary>\n\n    pip install nonebot-plugin-resolver\n</details>\n<details>\n<summary>pdm</summary>\n\n    pdm add nonebot-plugin-resolver\n</details>\n<details>\n<summary>poetry</summary>\n\n    poetry add nonebot-plugin-resolver\n</details>\n<details>\n<summary>conda</summary>\n\n    conda install nonebot-plugin-resolver\n</details>\n\n打开 nonebot2 项目根目录下的 `pyproject.toml` 文件, 在 `[tool.nonebot]` 部分追加写入\n\n    plugins = ["nonebot-plugin-resolver"]\n\n</details>\n\n## ⚙️ 配置\n\n在 nonebot2 项目的`.env`文件中添加下表中的必填配置\n\n```\n# twitter的token和本地代理\nbearer_token = ""\nresolver_proxy = "http://127.0.0.1:7890"\n```\n\n## 🎉 使用 & 效果图\n![help](./img/example.png)\n![help](./img/example2.png)\n![help](./img/example3.png)\n![help](./img/example4.png)\n![help](./img/example5.png)\n\n',
+    'author': 'zhiyu1998',
+    'author_email': 'renzhiyu0416@qq.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/zhiyu1998/nonebot_plugin_resolver',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
