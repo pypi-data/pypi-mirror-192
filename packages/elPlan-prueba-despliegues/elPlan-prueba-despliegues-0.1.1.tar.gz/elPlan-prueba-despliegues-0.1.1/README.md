6c323184d8c72223ad56227b57779dc78df7fa7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:10 +0000

    Add package elPlan-prueba-despliegues/0.1.1/elPlan-prueba-despliegues-0.1.1.tar.gz

---
+++ packages/elPlan-prueba-despliegues/elPlan-prueba-despliegues-0.1.1.tar.gz/elPlan-prueba-despliegues-0.1.1/README.md
@@ -0,0 +1,13 @@
+# elPlan-prueba-despliegues
+Repository for testing deploys in AWS with GitHub Actions.
+
+# What is this app?
+This app is a sample welcome to show students a simple Hello World for CI/CD
+
+# How to use?
+For a simple use execute 'execute.sh' changing params your-name and your-surname for yours.
+
+The app has been deployed in Pypi. So you can also install it as a package with 'pip install elPlan-prueba-despliegues==0.1.0' and use it with
+'welcome --name your-name --surname your-surname'
+
+
---
+++ packages/elPlan-prueba-despliegues/elPlan-prueba-despliegues-0.1.1.tar.gz/elPlan-prueba-despliegues-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as f:
+    readme = f.read()
+
+requirements = list()
+with open(os.path.join(HERE, 'requirements.txt')) as f:
+    for line in f:
+        line = line.strip()
+        if not line.startswith('#'):
+            requirements.append(line)
+
+
+setup(
+    name='elPlan-prueba-despliegues',
+    version='0.1.1',
+    description='testing welcome student app',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    test_suite="tests",
+    entry_points={
+        'console_scripts': ['welcome = src.app:main']
+    }
+)
