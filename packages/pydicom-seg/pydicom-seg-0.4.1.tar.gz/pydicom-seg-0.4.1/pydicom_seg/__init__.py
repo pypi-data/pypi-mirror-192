f201d201fd506c22551a3fa282b95970b8c0cc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:18 +0000

    Add package pydicom-seg/0.4.1/pydicom-seg-0.4.1.tar.gz

---
+++ packages/pydicom-seg/pydicom-seg-0.4.1.tar.gz/pydicom-seg-0.4.1/pydicom_seg/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.4.1"
+
+from pydicom_seg import template
+from pydicom_seg.reader import MultiClassReader, SegmentReader
+from pydicom_seg.writer import MultiClassWriter
+
+__all__ = [
+    "MultiClassReader",
+    "MultiClassWriter",
+    "SegmentReader",
+    "template",
+]
