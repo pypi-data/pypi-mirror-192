799f4693536b7bda65fb22f5962d0e9247e74857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package git-sim/0.2.4/git-sim-0.2.4.tar.gz

---
+++ packages/git-sim/git-sim-0.2.4.tar.gz/git-sim-0.2.4/git_sim/log.py
@@ -0,0 +1,39 @@
+import typer
+
+from git_sim.animations import handle_animations
+from git_sim.git_sim_base_command import GitSimBaseCommand
+from git_sim.settings import settings
+
+
+class Log(GitSimBaseCommand):
+    def __init__(self, commits: int):
+        super().__init__()
+        self.numCommits = commits + 1
+        self.defaultNumCommits = commits + 1
+        try:
+            self.selected_branches.append(self.repo.active_branch.name)
+        except TypeError:
+            pass
+
+    def construct(self):
+        if not settings.stdout:
+            print(f"{settings.INFO_STRING} {type(self).__name__.lower()}")
+        self.show_intro()
+        self.get_commits()
+        self.parse_commits(self.commits[0])
+        self.recenter_frame()
+        self.scale_frame()
+        self.fadeout()
+        self.show_outro()
+
+
+def log(
+    commits: int = typer.Option(
+        default=settings.commits,
+        help="The number of commits to display in the simulated log output",
+        min=1,
+        max=12,
+    ),
+):
+    scene = Log(commits=commits)
+    handle_animations(scene=scene)
