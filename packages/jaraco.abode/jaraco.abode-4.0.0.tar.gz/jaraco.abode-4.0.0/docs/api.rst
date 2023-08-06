96355cf4a322a0c908cab129aa02db2de8a3b845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:14 +0000

    Add package jaraco.abode/4.0.0/jaraco.abode-4.0.0.tar.gz

---
+++ packages/jaraco.abode/jaraco.abode-4.0.0.tar.gz/jaraco.abode-4.0.0/docs/api.rst
@@ -0,0 +1,42 @@
+API
+***
+
+.. automodule:: jaraco.abode
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: jaraco.abode.automation
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: jaraco.abode.cli
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: jaraco.abode.client
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: jaraco.abode.event_controller
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: jaraco.abode.exceptions
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: jaraco.abode.settings
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: jaraco.abode.socketio
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ packages/jaraco.abode/jaraco.abode-4.0.0.tar.gz/jaraco.abode-4.0.0/tests/mock/automation.py
@@ -0,0 +1,36 @@
+"""Mock Abode Automation."""
+
+
+def get_response_ok(name, enabled, aid):
+    """Return automation json."""
+    return dict(
+        name=name,
+        enabled=str(enabled),
+        version=2,
+        id=aid,
+        subType='',
+        actions=[
+            dict(
+                directive=dict(
+                    trait='panel.trains.panelMode',
+                    name='panel.directives.arm',
+                    state=dict(panelMode='AWAY'),
+                )
+            )
+        ],
+        conditions={},
+        triggers=dict(
+            operator='OR',
+            expressions=list(
+                dict(
+                    mobileDevices=['89381', '658'],
+                    property=dict(
+                        trait='mobile.traits.location',
+                        name='location',
+                        location='31675',
+                        equalTo='LAST_OUT',
+                    ),
+                )
+            ),
+        ),
+    )
