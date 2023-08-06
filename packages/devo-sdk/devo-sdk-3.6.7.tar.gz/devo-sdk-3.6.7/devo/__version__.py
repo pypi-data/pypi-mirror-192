70a32a101decefc1d43ceb32dac5b7a0ceb31b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:16 +0000

    Add package devo-sdk/3.6.7/devo-sdk-3.6.7.tar.gz

---
+++ packages/devo-sdk/devo-sdk-3.6.7.tar.gz/devo-sdk-3.6.7/devo/__version__.py
@@ -0,0 +1,7 @@
+__description__ = 'Devo Python Library.'
+__url__ = 'http://www.devo.com'
+__version__ = "3.6.7"
+__author__ = 'Devo'
+__author_email__ = 'support@devo.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Devo'
