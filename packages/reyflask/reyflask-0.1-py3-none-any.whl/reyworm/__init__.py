5bb491f47cfe9e2263fcea48abfe98dff95a18bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:35 +0000

    Add package reyflask/0.1/reyflask-0.1-py3-none-any.whl

---
+++ packages/reyflask/reyflask-0.1-py3-none-any.whl/reyworm/__init__.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-19 18:59:26
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Rey's personal worm set
+"""
+
+
+from .rtranslate import translate
