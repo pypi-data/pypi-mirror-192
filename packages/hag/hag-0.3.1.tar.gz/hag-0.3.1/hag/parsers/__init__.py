effedc32afdc3b204945a378c4b33f285600de3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:54 +0000

    Add package hag/0.3.1/hag-0.3.1.tar.gz

---
+++ packages/hag/hag-0.3.1.tar.gz/hag-0.3.1/hag/parsers/__init__.py
@@ -0,0 +1,31 @@
+from .alacritty import Alacritty
+from .lf import Lf
+from .mpv import Mpv
+from .neovim import Neovim
+from .qutebrowser import Qutebrowser
+from .rofi import Rofi
+from .sioyek import Sioyek
+from .sxhkd import Sxhkd
+from .sxiv import Sxiv
+from .termite import Termite
+from .vim import Vim
+from .vimiv import Vimiv
+from .zathura import Zathura
+from .zsh import Zsh
+
+__all__ = [
+    "Alacritty",
+    "Lf",
+    "Mpv",
+    "Neovim",
+    "Qutebrowser",
+    "Rofi",
+    "Sioyek",
+    "Sxhkd",
+    "Sxiv",
+    "Termite",
+    "Vim",
+    "Vimiv",
+    "Zathura",
+    "Zsh",
+]
