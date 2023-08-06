8fa1c4a70714bbb9dbebc13451dbd569802130b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:41 +0000

    Add package crosscompute-views-map/0.1.5/crosscompute_views_map-0.1.5-py3-none-any.whl

---
+++ packages/crosscompute-views-map/crosscompute_views_map-0.1.5-py3-none-any.whl/crosscompute_views_map/constants.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+PACKAGE_FOLDER = Path(__file__).parent
+ASSETS_FOLDER = PACKAGE_FOLDER / 'assets'
+
+
+MAPBOX_CSS_URI = 'https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.css'
+MAPBOX_JS_URI = 'https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.js'
+
+
+TURF_JS_URI = (
+    'https://cdnjs.cloudflare.com/ajax/libs/Turf.js/6.5.0/turf.min.js')
+
+
+DECK_JS_URI = 'https://unpkg.com/deck.gl@latest/dist.min.js'
