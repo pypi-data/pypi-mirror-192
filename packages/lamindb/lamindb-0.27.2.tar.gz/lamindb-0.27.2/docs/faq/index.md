71aabf6ef0c1ffd5c91a7f07b28c534dba069ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:58 +0000

    Add package lamindb/0.27.2/lamindb-0.27.2.tar.gz

---
+++ packages/lamindb/lamindb-0.27.2.tar.gz/lamindb-0.27.2/docs/faq/index.md
@@ -0,0 +1,17 @@
+# FAQ
+
+```{toctree}
+:maxdepth: 1
+
+ingest
+nb
+ingest-same-file-twice
+track-runin
+existing-data
+stream
+storage
+select
+data-validation
+ingest-acid
+lazy-loading
+```
---
+++ packages/lamindb/lamindb-0.27.2.tar.gz/lamindb-0.27.2/noxfile.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import nox
+from lndb.test.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def build(session):
+    login_testuser1(session)
+    login_user_2 = "lndb login testuser2@lamin.ai --password goeoNJKE61ygbz1vhaCVynGERaRrlviPBVQsjkhz"  # noqa
+    session.run(*(login_user_2.split(" ")), external=True)
+    session.install(".[dev,test]")
+    instance_dirs = [
+        d for d in ["./docs/guide/mydata", "./mydata-test-db"] if Path(d).exists()
+    ]
+    for instance_dir in instance_dirs:
+        clean_instance = f"rm -r {instance_dir}"
+        session.run(*clean_instance.split(" "))
+    test_db = "lndb init --storage mydata-test-db"
+    session.run(*test_db.split(" "), external=True)
+    run_pytest(session)
+    build_docs(session)
