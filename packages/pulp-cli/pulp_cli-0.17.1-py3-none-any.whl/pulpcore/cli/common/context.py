f158fbb4bbd151895ad6bec0e7322d76cb67d61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:28 +0000

    Add package pulp-cli/0.17.1/pulp_cli-0.17.1-py3-none-any.whl

---
+++ packages/pulp-cli/pulp_cli-0.17.1-py3-none-any.whl/pulpcore/cli/common/context.py
@@ -0,0 +1,33 @@
+from pulp_glue.common.context import (  # noqa: F401
+    EntityDefinition,
+    EntityFieldDefinition,
+    PluginRequirement,
+    PreprocessedEntityDefinition,
+    PulpACSContext,
+    PulpContentContext,
+    PulpContext,
+    PulpDistributionContext,
+    PulpEntityContext,
+    PulpException,
+    PulpRemoteContext,
+    PulpRepositoryContext,
+    PulpRepositoryVersionContext,
+    registered_repository_contexts,
+)
+
+__all__ = [
+    "EntityDefinition",
+    "EntityFieldDefinition",
+    "PluginRequirement",
+    "PreprocessedEntityDefinition",
+    "PulpACSContext",
+    "PulpContentContext",
+    "PulpContext",
+    "PulpDistributionContext",
+    "PulpEntityContext",
+    "PulpException",
+    "PulpRemoteContext",
+    "PulpRepositoryContext",
+    "PulpRepositoryVersionContext",
+    "registered_repository_contexts",
+]
---
+++ packages/pulp-cli/pulp_cli-0.17.1-py3-none-any.whl/pulpcore/cli/file/__init__.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+import click
+from pulp_glue.common.context import PluginRequirement
+from pulp_glue.common.i18n import get_translation
+
+from pulpcore.cli.common.generic import PulpCLIContext, pass_pulp_context, pulp_group
+from pulpcore.cli.file.acs import acs
+from pulpcore.cli.file.content import content
+from pulpcore.cli.file.distribution import distribution
+from pulpcore.cli.file.publication import publication
+from pulpcore.cli.file.remote import remote
+from pulpcore.cli.file.repository import repository
+
+translation = get_translation(__name__)
+_ = translation.gettext
+
+
+@pulp_group(name="file")
+@pass_pulp_context
+def file_group(pulp_ctx: PulpCLIContext) -> None:
+    pulp_ctx.needs_plugin(PluginRequirement("file", min="1.6.0"))
+
+
+def mount(main: click.Group, **kwargs: Any) -> None:
+    file_group.add_command(repository)
+    file_group.add_command(remote)
+    file_group.add_command(publication)
+    file_group.add_command(distribution)
+    file_group.add_command(content)
+    file_group.add_command(acs)
+    main.add_command(file_group)
