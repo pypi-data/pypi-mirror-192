86b4fd8cb841b6cd505dec8f74af3997746b95bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:57 +0000

    Add package awsomess/0.0.1/awsomess-0.0.1.tar.gz

---
+++ packages/awsomess/awsomess-0.0.1.tar.gz/awsomess-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="awsomess",
+    version="0.0.1",
+    description="just to test deployment and nothing else",
+    author="afonso",
+    author_email="a.schulzalbrecht@gmail.com",
+    # package_dir={"": "src"},  # allows: import awsomess
+)
