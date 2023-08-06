2352949d1137fbc4cfb487848c2933b8cc48c8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:32 +0000

    Add package netunicorn-base/0.2.3/netunicorn-base-0.2.3.tar.gz

---
+++ packages/netunicorn-base/netunicorn-base-0.2.3.tar.gz/netunicorn-base-0.2.3/src/netunicorn/director/base/utils.py
@@ -0,0 +1,14 @@
+import json
+
+import asyncpg
+
+from netunicorn.base.utils import UnicornEncoder
+
+
+async def __init_connection(conn: asyncpg.Connection) -> None:
+    await conn.set_type_codec(
+        "jsonb",
+        encoder=lambda x: json.dumps(x, cls=UnicornEncoder),
+        decoder=json.loads,
+        schema="pg_catalog",
+    )
