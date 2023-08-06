3f6e507d595e9e0b74badb67d5308a4bdfcd221c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:17 +0000

    Add package oc-idmanager/0.2.6/oc_idmanager-0.2.6.tar.gz

---
+++ packages/oc-idmanager/oc_idmanager-0.2.6.tar.gz/oc_idmanager-0.2.6/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['oc_idmanager']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0',
+ 'lxml>=4.9.1,<5.0.0',
+ 'oc-meta==1.2.4',
+ 'requests>=2.28.1,<3.0.0',
+ 'validators>=0.20.0,<0.21.0']
+
+setup_kwargs = {
+    'name': 'oc-idmanager',
+    'version': '0.2.6',
+    'description': 'This library allows validating identifiers with DOI, ISBN, ISSN, ORCID, PMCID, and PMID schemes',
+    'long_description': 'None',
+    'author': 'Silvio Peroni',
+    'author_email': 'essepuntato@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.4,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
