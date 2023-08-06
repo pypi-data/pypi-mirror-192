344e3452369ddfcb0fa72ee32fe22e1479d10af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:37 +0000

    Add package cognite-sdk/5.5.2/cognite_sdk-5.5.2.tar.gz

---
+++ packages/cognite-sdk/cognite_sdk-5.5.2.tar.gz/cognite_sdk-5.5.2/cognite/client/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from cognite.client._cognite_client import CogniteClient
+from cognite.client.config import ClientConfig, global_config
+from cognite.client._version import __version__
