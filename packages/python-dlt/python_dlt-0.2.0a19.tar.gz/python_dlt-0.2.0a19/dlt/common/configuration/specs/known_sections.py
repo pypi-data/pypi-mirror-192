ce1f57e4dda1c0e01c9a47d4f9859a6afc1c0794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:21 +0000

    Add package python-dlt/0.2.0a19/python_dlt-0.2.0a19.tar.gz

---
+++ packages/python-dlt/python_dlt-0.2.0a19.tar.gz/python_dlt-0.2.0a19/dlt/common/configuration/specs/known_sections.py
@@ -0,0 +1,5 @@
+DATA_WRITER = "data_writer"
+"""default section holding BufferedDataWriter settings"""
+
+SOURCES = "sources"
+"""a top section holding source and resource configs often within their own sections named after modules they are in"""
