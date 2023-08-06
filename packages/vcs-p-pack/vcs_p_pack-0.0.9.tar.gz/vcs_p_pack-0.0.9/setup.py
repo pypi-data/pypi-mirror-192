41c3ce9bb0d134f6d6be45d8fb82a325185ba9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:19 +0000

    Add package vcs-p-pack/0.0.9/vcs_p_pack-0.0.9.tar.gz

---
+++ packages/vcs-p-pack/vcs_p_pack-0.0.9.tar.gz/vcs_p_pack-0.0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vcs_p_pack',
+    version='0.0.9',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'parse_work = package.vcs_p:parse_work',
+        ],
+    },
+    install_requires=[
+        'psutil == 5.9.4',
+        'setuptools == 56.0.0'
+    ],
+)
