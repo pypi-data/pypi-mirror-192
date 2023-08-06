721173a6cd13ceff7a6e97c6913e62634cd153c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:09 +0000

    Add package cls-python/0.1.3/cls-python-0.1.3.tar.gz

---
+++ packages/cls-python/cls-python-0.1.3.tar.gz/cls-python-0.1.3/HISTORY.rst
@@ -0,0 +1,28 @@
+=======
+History
+=======
+
+0.1.3 (2023-02-17)
+------------------
+
+* Fix readme link to new gh-page.
+
+
+0.1.2 (2023-02-17)
+------------------
+
+* Update CI to include build and deploy of gh-page.
+* New badges for README.
+
+
+0.1.1 (2023-02-17)
+------------------
+
+* Update CI.
+* Exclude setup.cfg from trailing whitespace checking to work with bump2version.
+
+
+0.1.0 (2023-02-16)
+------------------
+
+* First release on PyPI.
---
+++ packages/cls-python/cls-python-0.1.3.tar.gz/cls-python-0.1.3/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = []
+
+test_requirements = [
+    "pytest>=3",
+]
+
+setup(
+    author="Jan Bessai",
+    author_email="jan.bessai@tu-dortmund.de",
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+    description="Python implementation of the CLS framework.",
+    install_requires=requirements,
+    license="Apache License (2.0)",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="cls-python",
+    name="cls-python",
+    packages=find_packages(include=["cls", "cls.*"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/cls-python/cls-python",
+    version="0.1.3",
+    zip_safe=False,
+)
