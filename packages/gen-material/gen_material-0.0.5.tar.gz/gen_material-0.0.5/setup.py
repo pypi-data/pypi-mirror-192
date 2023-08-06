2cbcfd6da5992386fbef096d87627de0c653b772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:54 +0000

    Add package gen-material/0.0.5/gen_material-0.0.5.tar.gz

---
+++ packages/gen-material/gen_material-0.0.5.tar.gz/gen_material-0.0.5/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gen_material', 'gen_material.data']
+
+package_data = \
+{'': ['*'],
+ 'gen_material': ['assets/*', 'assets/bg/*', 'assets/font/*', 'assets/frame/*']}
+
+setup_kwargs = {
+    'name': 'gen-material',
+    'version': '0.0.5',
+    'description': '5dfsfddsfd',
+    'long_description': 'HI',
+    'author': 'None',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
