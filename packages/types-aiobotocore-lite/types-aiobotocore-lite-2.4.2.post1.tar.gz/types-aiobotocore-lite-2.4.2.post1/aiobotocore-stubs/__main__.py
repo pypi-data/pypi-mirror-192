a8bd23d343cbb2c8ee00ba8671620289bb3cfa12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:42 +0000

    Add package types-aiobotocore-lite/2.4.2.post1/types-aiobotocore-lite-2.4.2.post1.tar.gz

---
+++ packages/types-aiobotocore-lite/types-aiobotocore-lite-2.4.2.post1.tar.gz/types-aiobotocore-lite-2.4.2.post1/aiobotocore-stubs/__main__.py
@@ -0,0 +1,37 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for aiobotocore 2.4.2\n"
+        "Version:         2.4.2.post1\n"
+        "Builder version: 7.12.3\n"
+        "Docs:            https://pypi.org/project/types-aiobotocore-lite/\n"
+        "Changelog:       https://github.com/youtype/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("2.4.2.post1")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
