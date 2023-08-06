2ec29d541b4c8923e55a235a0c70de573913a9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:14 +0000

    Add package dnb-urn-service/0.1.2/dnb_urn_service-0.1.2.tar.gz

---
+++ packages/dnb-urn-service/dnb_urn_service-0.1.2.tar.gz/dnb_urn_service-0.1.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2022, 2023 University Münster.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""Python API client wrapper for the DNB URN service API."""
+
+from setuptools import setup
+
+setup()
