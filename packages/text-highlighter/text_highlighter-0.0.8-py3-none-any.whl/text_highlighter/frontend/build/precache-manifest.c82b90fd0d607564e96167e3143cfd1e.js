da3b1672110bfcc7129fe2b0169a6e58fb66be93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:23 +0000

    Add package text-highlighter/0.0.8/text_highlighter-0.0.8-py3-none-any.whl

---
+++ packages/text-highlighter/text_highlighter-0.0.8-py3-none-any.whl/text_highlighter/frontend/build/precache-manifest.c82b90fd0d607564e96167e3143cfd1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e110bafd84f585e8a98f4c5b38d6d75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9199b802470fc09b657",
+    "url": "./static/js/2.b0d3c34d.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.b0d3c34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2189b22f3645c36cdbb",
+    "url": "./static/js/main.20f0490c.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
