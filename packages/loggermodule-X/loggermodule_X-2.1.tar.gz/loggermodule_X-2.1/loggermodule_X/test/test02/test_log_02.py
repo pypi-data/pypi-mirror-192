8863ffee09a4e18f58df30b1e2c68d5d08a87913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:59 +0000

    Add package loggermodule-X/2.1/loggermodule_X-2.1.tar.gz

---
+++ packages/loggermodule-X/loggermodule_X-2.1.tar.gz/loggermodule_X-2.1/loggermodule_X/test/test02/test_log_02.py
@@ -0,0 +1,18 @@
+from loggermodule_XS import logger_x
+
+loggerX = logger_x(__file__)
+log = loggerX.configLogger()
+log.info("START -----------------------------------------")
+log.info('test_log_2')
+
+class numbersum:
+    def __init__(self, num,num2):
+        self.number = num
+        self.number2=num2
+
+    def sum(self):
+        number = self.number
+        number2 = self.number2
+        sum = number+number2
+
+        return sum
