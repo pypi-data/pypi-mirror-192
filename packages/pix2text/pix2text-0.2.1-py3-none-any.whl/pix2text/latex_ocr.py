226b4b9cbd862602569c04b2193423639bdf0fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:39 +0000

    Add package pix2text/0.2.1/pix2text-0.2.1-py3-none-any.whl

---
+++ packages/pix2text/pix2text-0.2.1-py3-none-any.whl/pix2text/latex_ocr.py
@@ -0,0 +1,327 @@
+# coding: utf-8
+# Copyright (C) 2022, [Breezedeus](https://github.com/breezedeus).
+#
+# credit to: pix2tex, lukas-blecher/LaTeX-OCR
+# Adapted from https://github.com/lukas-blecher/LaTeX-OCR/blob/main/pix2tex/cli.py
+
+from typing import Tuple, Optional, Dict, Any
+import logging
+import yaml
+from pathlib import Path
+
+from PIL import Image
+from transformers import PreTrainedTokenizerFast
+from timm.models.resnetv2 import ResNetV2
+from timm.models.layers import StdConv2dSame
+
+from pix2tex.utils import *
+from pix2tex.models import get_model
+from pix2tex.dataset.transforms import test_transform
+from pix2tex.model.checkpoints.get_latest_checkpoint import (
+    download_as_bytes_with_progress,
+)
+
+from .consts import LATEX_CONFIG_FP
+from .utils import data_dir
+
+
+logger = logging.getLogger(__name__)
+
+
+def download_checkpoints(out_dl_dir):
+    # adapted from pix2tex.model.checkpoints.get_latest_checkpoint
+    tag = 'v0.0.1'  # get_latest_tag()
+    os.makedirs(out_dl_dir, exist_ok=True)
+    weights = (
+        'https://github.com/lukas-blecher/LaTeX-OCR/releases/download/%s/weights.pth'
+        % tag
+    )
+    resizer = (
+        'https://github.com/lukas-blecher/LaTeX-OCR/releases/download/%s/image_resizer.pth'
+        % tag
+    )
+    for url, name in zip([weights, resizer], ['weights.pth', 'image_resizer.pth']):
+        if not os.path.exists(os.path.join(out_dl_dir, name)):
+            file = download_as_bytes_with_progress(url, name)
+            logger.info('downloading file %s to path %s', name, out_dl_dir)
+            open(os.path.join(out_dl_dir, name), "wb").write(file)
+            logger.info(f'save {name} to path {out_dl_dir}')
+        else:
+            logger.info(f'use model {name} from path {out_dl_dir}')
+
+
+def minmax_size(
+    img: Image,
+    max_dimensions: Tuple[int, int] = None,
+    min_dimensions: Tuple[int, int] = None,
+) -> Image:
+    """Resize or pad an image to fit into given dimensions
+
+    Args:
+        img (Image): Image to scale up/down.
+        max_dimensions (Tuple[int, int], optional): Maximum dimensions. Defaults to None.
+        min_dimensions (Tuple[int, int], optional): Minimum dimensions. Defaults to None.
+
+    Returns:
+        Image: Image with correct dimensionality
+    """
+    if max_dimensions is not None:
+        ratios = [a / b for a, b in zip(img.size, max_dimensions)]
+        if any([r > 1 for r in ratios]):
+            size = np.array(img.size) // max(ratios)
+            img = img.resize(size.astype(int), Image.BILINEAR)
+    if min_dimensions is not None:
+        # hypothesis: there is a dim in img smaller than min_dimensions, and return a proper dim >= min_dimensions
+        padded_size = [
+            max(img_dim, min_dim) for img_dim, min_dim in zip(img.size, min_dimensions)
+        ]
+        if padded_size != list(img.size):  # assert hypothesis
+            padded_im = Image.new('L', padded_size, 255)
+            padded_im.paste(img, img.getbbox())
+            img = padded_im
+    return img
+
+
+def find_all_left_or_right(latex, left_or_right='left'):
+    left_bracket_infos = []
+    prefix_len = len(left_or_right) + 1
+    # 匹配出latex中所有的 '\left' 后面跟着的第一个非空格字符，定位它们所在的位置
+    for m in re.finditer(rf'\\{left_or_right}\s*\S', latex):
+        start, end = m.span()
+        # 如果最后一个字符为 "\"，则往前继续匹配，直到匹配到一个非字母的字符
+        # 如 "\left \big("
+        while latex[end - 1] in ('\\', ' '):
+            end += 1
+            while latex[end].isalpha():
+                end += 1
+        ori_str = latex[start + prefix_len : end].strip()
+        # FIXME: ori_str中可能出现多个 '\left'，此时需要分隔开
+
+        left_bracket_infos.append({'str': ori_str, 'start': start, 'end': end})
+        left_bracket_infos.sort(key=lambda x: x['start'])
+    return left_bracket_infos
+
+
+def match_left_right(left_str, right_str):
+    """匹配左右括号，如匹配 `\left(` 和 `\right)`。"""
+    left_str = left_str.strip().replace(' ', '')[len('left') + 1 :]
+    right_str = right_str.strip().replace(' ', '')[len('right') + 1 :]
+    # 去掉开头的相同部分
+    while left_str and right_str and left_str[0] == right_str[0]:
+        left_str = left_str[1:]
+        right_str = right_str[1:]
+
+    match_pairs = [
+        ('', ''),
+        ('(', ')'),
+        ('\{', '.'),  # 大括号那种
+        ('⟮', '⟯'),
+        ('[', ']'),
+        ('⟨', '⟩'),
+        ('{', '}'),
+        ('⌈', '⌉'),
+        ('┌', '┐'),
+        ('⌊', '⌋'),
+        ('└', '┘'),
+        ('⎰', '⎱'),
+        ('lt', 'gt'),
+        ('lang', 'rang'),
+        (r'langle', r'rangle'),
+        (r'lbrace', r'rbrace'),
+        ('lBrace', 'rBrace'),
+        (r'lbracket', r'rbracket'),
+        (r'lceil', r'rceil'),
+        ('lcorner', 'rcorner'),
+        (r'lfloor', r'rfloor'),
+        (r'lgroup', r'rgroup'),
+        (r'lmoustache', r'rmoustache'),
+        (r'lparen', r'rparen'),
+        (r'lvert', r'rvert'),
+        (r'lVert', r'rVert'),
+    ]
+    return (left_str, right_str) in match_pairs
+
+
+def post_post_process_latex(latex: str) -> str:
+    """对识别结果做进一步处理和修正。"""
+    # 把latex中的中文括号全部替换成英文括号
+    latex = latex.replace('（', '(').replace('）', ')')
+    # 把latex中的中文逗号全部替换成英文逗号
+    latex = latex.replace('，', ',')
+
+    left_bracket_infos = find_all_left_or_right(latex, left_or_right='left')
+    right_bracket_infos = find_all_left_or_right(latex, left_or_right='right')
+    # left 和 right 找配对，left找位置比它靠前且最靠近他的right配对
+    for left_bracket_info in left_bracket_infos:
+        for right_bracket_info in right_bracket_infos:
+            if (
+                not right_bracket_info.get('matched', False)
+                and right_bracket_info['start'] > left_bracket_info['start']
+                and match_left_right(
+                    right_bracket_info['str'], left_bracket_info['str']
+                )
+            ):
+                left_bracket_info['matched'] = True
+                right_bracket_info['matched'] = True
+                break
+
+    for left_bracket_info in left_bracket_infos:
+        # 把没有匹配的 '\left'替换为等长度的空格
+        left_len = len('left') + 1
+        if not left_bracket_info.get('matched', False):
+            start_idx = left_bracket_info['start']
+            end_idx = start_idx + left_len
+            latex = (
+                latex[: left_bracket_info['start']]
+                + ' ' * (end_idx - start_idx)
+                + latex[end_idx:]
+            )
+    for right_bracket_info in right_bracket_infos:
+        # 把没有匹配的 '\right'替换为等长度的空格
+        right_len = len('right') + 1
+        if not right_bracket_info.get('matched', False):
+            start_idx = right_bracket_info['start']
+            end_idx = start_idx + right_len
+            latex = (
+                latex[: right_bracket_info['start']]
+                + ' ' * (end_idx - start_idx)
+                + latex[end_idx:]
+            )
+
+    # 把 latex 中的连续空格替换为一个空格
+    latex = re.sub(r'\s+', ' ', latex)
+    return latex
+
+
+class LatexOCR(object):
+    """Get a prediction of an image in the easiest way"""
+
+    image_resizer = None
+    last_pic = None
+
+    @in_model_path()
+    def __init__(self, arguments: Optional[Dict[str, Any]] = None):
+        """Initialize a LatexOCR model
+
+        Args:
+            arguments (Union[Namespace, Munch], optional): Special model parameters. Defaults to None.
+        """
+        if arguments is None:
+            arguments = {
+                'config': LATEX_CONFIG_FP,
+                'checkpoint': Path(data_dir()) / 'formula' / 'weights.pth',
+                # 'no_cuda': True,
+                'no_resize': False,
+                'device': 'cpu',
+            }
+
+        arguments = Munch(arguments)
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+        with open(arguments.config, 'r') as f:
+            params = yaml.load(f, Loader=yaml.FullLoader)
+        self.args = parse_args(Munch(params))
+        self.args.update(**vars(arguments))
+        self.args.wandb = False
+        # self.args.device = (
+        #     'cuda' if torch.cuda.is_available() and not self.args.no_cuda else 'cpu'
+        # )
+        # if not os.path.exists(self.args.checkpoint):
+        download_checkpoints(os.path.dirname(arguments.checkpoint))
+        self.model = get_model(self.args)
+        self.model.load_state_dict(
+            torch.load(self.args.checkpoint, map_location=self.args.device)
+        )
+        self.model.eval()
+
+        if (
+            'image_resizer.pth' in os.listdir(os.path.dirname(self.args.checkpoint))
+            and not arguments.no_resize
+        ):
+            self.image_resizer = ResNetV2(
+                layers=[2, 3, 3],
+                num_classes=max(self.args.max_dimensions) // 32,
+                global_pool='avg',
+                in_chans=1,
+                drop_rate=0.05,
+                preact=True,
+                stem_type='same',
+                conv_layer=StdConv2dSame,
+            ).to(self.args.device)
+            self.image_resizer.load_state_dict(
+                torch.load(
+                    os.path.join(
+                        os.path.dirname(self.args.checkpoint), 'image_resizer.pth'
+                    ),
+                    map_location=self.args.device,
+                )
+            )
+            self.image_resizer.eval()
+        self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=self.args.tokenizer)
+
+    @in_model_path()
+    def __call__(self, img=None, resize=True) -> str:
+        """Get a prediction from an image
+
+        Args:
+            img (Image, optional): Image to predict. Defaults to None.
+            resize (bool, optional): Whether to call the resize model. Defaults to True.
+
+        Returns:
+            str: predicted Latex code
+        """
+        if type(img) is bool:
+            img = None
+        if img is None:
+            if self.last_pic is None:
+                print('Provide an image.')
+                return ''
+            else:
+                img = self.last_pic.copy()
+        else:
+            self.last_pic = img.copy()
+        img = minmax_size(pad(img), self.args.max_dimensions, self.args.min_dimensions)
+        if (self.image_resizer is not None and not self.args.no_resize) and resize:
+            with torch.no_grad():
+                input_image = img.convert('RGB').copy()
+                r, w, h = 1, input_image.size[0], input_image.size[1]
+                for _ in range(10):
+                    h = int(h * r)  # height to resize
+                    try:
+                        img = pad(
+                            minmax_size(
+                                input_image.resize(
+                                    (w, h),
+                                    Image.Resampling.BILINEAR
+                                    if r > 1
+                                    else Image.Resampling.LANCZOS,
+                                ),
+                                self.args.max_dimensions,
+                                self.args.min_dimensions,
+                            )
+                        )
+                        t = test_transform(image=np.array(img.convert('RGB')))['image'][
+                            :1
+                        ].unsqueeze(0)
+                        w = (
+                            self.image_resizer(t.to(self.args.device)).argmax(-1).item()
+                            + 1
+                        ) * 32
+                        logger.debug((r, img.size, (w, int(input_image.size[1] * r))))
+                    except Exception as e:
+                        logger.warning(e)
+                        break
+
+                    if w == img.size[0]:
+                        break
+                    r = w / img.size[0]
+        else:
+            img = np.array(pad(img).convert('RGB'))
+            t = test_transform(image=img)['image'][:1].unsqueeze(0)
+        im = t.to(self.args.device)
+
+        dec = self.model.generate(
+            im.to(self.args.device), temperature=self.args.get('temperature', 0.25)
+        )
+        pred = post_process(token2str(dec, self.tokenizer)[0])
+        pred = post_post_process_latex(pred)
+        return pred
---
+++ packages/pix2text/pix2text-0.2.1-py3-none-any.whl/pix2text/utils.py
@@ -0,0 +1,291 @@
+# coding: utf-8
+# Copyright (C) 2022, [Breezedeus](https://github.com/breezedeus).
+
+import hashlib
+import os
+from pathlib import Path
+import logging
+import platform
+import zipfile
+import requests
+from typing import Union
+
+from tqdm import tqdm
+from PIL import Image, ImageOps
+import numpy as np
+from numpy import random
+import torch
+from torch import Tensor
+from torchvision.utils import save_image
+
+
+fmt = '[%(levelname)s %(asctime)s %(funcName)s:%(lineno)d] %(' 'message)s '
+logging.basicConfig(format=fmt)
+logging.captureWarnings(True)
+logger = logging.getLogger()
+
+
+def set_logger(log_file=None, log_level=logging.INFO, log_file_level=logging.NOTSET):
+    """
+    Example:
+        >>> set_logger(log_file)
+        >>> logger.info("abc'")
+    """
+    log_format = logging.Formatter(fmt)
+    logger.setLevel(log_level)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    logger.handlers = [console_handler]
+    if log_file and log_file != '':
+        if not Path(log_file).parent.exists():
+            os.makedirs(Path(log_file).parent)
+        if isinstance(log_file, Path):
+            log_file = str(log_file)
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(log_file_level)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+    return logger
+
+
+def check_context(context):
+    if isinstance(context, str):
+        return any([ctx in context.lower() for ctx in ('gpu', 'cpu', 'cuda')])
+    if isinstance(context, list):
+        if len(context) < 1:
+            return False
+        return all(isinstance(ctx, torch.device) for ctx in context)
+    return isinstance(context, torch.device)
+
+
+def data_dir_default():
+    """
+
+    :return: default data directory depending on the platform and environment variables
+    """
+    system = platform.system()
+    if system == 'Windows':
+        return os.path.join(os.environ.get('APPDATA'), 'pix2text')
+    else:
+        return os.path.join(os.path.expanduser("~"), '.pix2text')
+
+
+def data_dir():
+    """
+
+    :return: data directory in the filesystem for storage, for example when downloading models
+    """
+    return os.getenv('PIX2TEXT_HOME', data_dir_default())
+
+
+def to_numpy(tensor: torch.Tensor) -> np.ndarray:
+    return (
+        tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+    )
+
+
+def check_sha1(filename, sha1_hash):
+    """Check whether the sha1 hash of the file content matches the expected hash.
+    Parameters
+    ----------
+    filename : str
+        Path to the file.
+    sha1_hash : str
+        Expected sha1 hash in hexadecimal digits.
+    Returns
+    -------
+    bool
+        Whether the file content matches the expected hash.
+    """
+    sha1 = hashlib.sha1()
+    with open(filename, 'rb') as f:
+        while True:
+            data = f.read(1048576)
+            if not data:
+                break
+            sha1.update(data)
+
+    sha1_file = sha1.hexdigest()
+    l = min(len(sha1_file), len(sha1_hash))
+    return sha1.hexdigest()[0:l] == sha1_hash[0:l]
+
+
+def download(url, path=None, overwrite=False, sha1_hash=None):
+    """Download an given URL
+    Parameters
+    ----------
+    url : str
+        URL to download
+    path : str, optional
+        Destination path to store downloaded file. By default stores to the
+        current directory with same name as in url.
+    overwrite : bool, optional
+        Whether to overwrite destination file if already exists.
+    sha1_hash : str, optional
+        Expected sha1 hash in hexadecimal digits. Will ignore existing file when hash is specified
+        but doesn't match.
+    Returns
+    -------
+    str
+        The file path of the downloaded file.
+    """
+    if path is None:
+        fname = url.split('/')[-1]
+    else:
+        path = os.path.expanduser(path)
+        if os.path.isdir(path):
+            fname = os.path.join(path, url.split('/')[-1])
+        else:
+            fname = path
+
+    if (
+        overwrite
+        or not os.path.exists(fname)
+        or (sha1_hash and not check_sha1(fname, sha1_hash))
+    ):
+        dirname = os.path.dirname(os.path.abspath(os.path.expanduser(fname)))
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        logger.info('Downloading %s from %s...' % (fname, url))
+        r = requests.get(url, stream=True)
+        if r.status_code != 200:
+            raise RuntimeError("Failed downloading url %s" % url)
+        total_length = r.headers.get('content-length')
+        with open(fname, 'wb') as f:
+            if total_length is None:  # no content length header
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+            else:
+                total_length = int(total_length)
+                for chunk in tqdm(
+                    r.iter_content(chunk_size=1024),
+                    total=int(total_length / 1024.0 + 0.5),
+                    unit='KB',
+                    unit_scale=False,
+                    dynamic_ncols=True,
+                ):
+                    f.write(chunk)
+
+        if sha1_hash and not check_sha1(fname, sha1_hash):
+            raise UserWarning(
+                'File {} is downloaded but the content hash does not match. '
+                'The repo may be outdated or download may be incomplete. '
+                'If the "repo_url" is overridden, consider switching to '
+                'the default repo.'.format(fname)
+            )
+
+    return fname
+
+
+def get_model_file(url, model_dir):
+    r"""Return location for the downloaded models on local file system.
+
+    This function will download from online model zoo when model cannot be found or has mismatch.
+    The root directory will be created if it doesn't exist.
+
+    Parameters
+    ----------
+    url: str
+    model_dir : str, default $CNOCR_HOME
+        Location for keeping the model parameters.
+
+    Returns
+    -------
+    file_path
+        Path to the requested pretrained model file.
+    """
+    model_dir = os.path.expanduser(model_dir)
+    par_dir = os.path.dirname(model_dir)
+    os.makedirs(par_dir, exist_ok=True)
+
+    zip_file_path = os.path.join(par_dir, os.path.basename(url))
+    if not os.path.exists(zip_file_path):
+        download(url, path=zip_file_path, overwrite=True)
+    with zipfile.ZipFile(zip_file_path) as zf:
+        zf.extractall(par_dir)
+    os.remove(zip_file_path)
+
+    return model_dir
+
+
+def read_tsv_file(fp, sep='\t', img_folder=None, mode='eval'):
+    img_fp_list, labels_list = [], []
+    num_fields = 2 if mode != 'test' else 1
+    with open(fp) as f:
+        for line in f:
+            fields = line.strip('\n').split(sep)
+            assert len(fields) == num_fields
+            img_fp = (
+                os.path.join(img_folder, fields[0])
+                if img_folder is not None
+                else fields[0]
+            )
+            img_fp_list.append(img_fp)
+
+            if mode != 'test':
+                labels = fields[1].split(' ')
+                labels_list.append(labels)
+
+    return (img_fp_list, labels_list) if mode != 'test' else (img_fp_list, None)
+
+
+def read_img(
+    path: Union[str, Path], return_type='Tensor'
+) -> Union[Image.Image, np.ndarray, torch.Tensor]:
+    """
+
+    Args:
+        path (str): image file path
+        return_type (str): 返回类型；
+            支持 `Tensor`，返回 torch.Tensor；`ndarray`，返回 np.ndarray；`Image`，返回 `Image.Image`
+
+    Returns: RGB Image.Image, or np.ndarray / torch.Tensor, with shape [Channel, Height, Width]
+    """
+    assert return_type in ('Tensor', 'ndarray', 'Image')
+    img = Image.open(path)
+    img = ImageOps.exif_transpose(img).convert('RGB')  # 识别旋转后的图片（pillow不会自动识别）
+    if return_type == 'Image':
+        return img
+    img = np.array(img)
+    if return_type == 'ndarray':
+        return img
+    return torch.tensor(img.transpose((2, 0, 1)))
+
+
+def save_img(img: Union[Tensor, np.ndarray], path):
+    if not isinstance(img, Tensor):
+        img = torch.from_numpy(img)
+    img = (img - img.min()) / (img.max() - img.min() + 1e-6)
+    # img *= 255
+    # img = img.to(dtype=torch.uint8)
+    save_image(img, path)
+
+    # Image.fromarray(img).save(path)
+
+
+def save_layout_img(img0, categories, one_out, save_path, key='position'):
+    import cv2
+    from cnstd.yolov7.plots import plot_one_box
+
+    """可视化版面分析结果。"""
+    if isinstance(img0, Image.Image):
+        img0 = cv2.cvtColor(np.asarray(img0.convert('RGB')), cv2.COLOR_RGB2BGR)
+
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in categories]
+    for one_box in one_out:
+        _type = one_box['type']
+        box = one_box[key]
+        xyxy = [box[0, 0], box[0, 1], box[2, 0], box[2, 1]]
+        label = f'{_type}'
+        plot_one_box(
+            xyxy,
+            img0,
+            label=label,
+            color=colors[categories.index(_type)],
+            line_thickness=1,
+        )
+
+    cv2.imwrite(save_path, img0)
+    logger.info(f" The image with the result is saved in: {save_path}")
