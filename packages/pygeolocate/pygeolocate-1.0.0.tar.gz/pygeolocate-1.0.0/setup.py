66ea7cb5fe8e1621521e32be6a6c0719f5c1597a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:34 +0000

    Add package pygeolocate/1.0.0/pygeolocate-1.0.0.tar.gz

---
+++ packages/pygeolocate/pygeolocate-1.0.0.tar.gz/pygeolocate-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'An easy way to find a countries coordinates by name'
+LONG_DESCRIPTION = 'An easy way to find a countries coordinates by name'
+
+setup(
+    name="pygeolocate",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Isabelle",
+    author_email="scrumpy@weeb.email",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='location, geolocation, geolocate',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
