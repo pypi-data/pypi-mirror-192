7dd03ce37a52051e20aa10b537d2d96cf53ba485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:44 +0000

    Add package offlinedatasci/0.1.0/offlinedatasci-0.1.0.tar.gz

---
+++ packages/offlinedatasci/offlinedatasci-0.1.0.tar.gz/offlinedatasci-0.1.0/offlinedatasci/miniCran.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly = TRUE)
+package_list = strsplit(args[2]," ")[[1]]
+pth = file.path(args[1], "miniCRAN")
+
+repo = c("https://cran.rstudio.com")
+if (!require("miniCRAN")) {
+  install.packages("miniCRAN")
+  library("miniCRAN")
+}
+types = c("source", "win.binary", "mac.binary")
+
+for (type in types) {
+    repo_bin_path <- miniCRAN:::repoBinPath(path = pth, type = type, Rversion = R.version)
+    if (!('PACKAGES' %in% list.files(repo_bin_path))) {
+        if (!(file.exists(repo_bin_path))) {
+            dir.create(repo_bin_path, recursive = TRUE)
+        }
+        file.create(file.path(repo_bin_path, 'PACKAGES'))
+    }
+    DC_pkg_tree = pkgDep(package_list, repos = repo, type = type, suggests = FALSE, Rversion = R.version)
+    local_cran_avail = pkgAvail(repos = pth, type = type, Rversion = R.version)[, "Version"]
+    pkgs_to_download = DC_pkg_tree[!DC_pkg_tree %in% names(local_cran_avail)]
+    if (length(pkgs_to_download) ==0) {
+        cat("Repository already exists, checking updates for", type, "\n")
+        updatePackages(path = pth, repos = repo, type = type, ask = FALSE)
+    
+    } 
+    else {
+        # Get package dependency trees from list of wanted packages
+        makeRepo(pkgs_to_download, path = pth, repos = repo, type = type)
+    }
+}
+
