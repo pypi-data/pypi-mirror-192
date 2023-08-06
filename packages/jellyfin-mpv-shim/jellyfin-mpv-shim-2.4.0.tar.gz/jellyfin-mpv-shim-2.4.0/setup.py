5487e26c4cfd0885a86a811476a427f7bb3784b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:24 +0000

    Add package jellyfin-mpv-shim/2.4.0/jellyfin-mpv-shim-2.4.0.tar.gz

---
+++ packages/jellyfin-mpv-shim/jellyfin-mpv-shim-2.4.0.tar.gz/jellyfin-mpv-shim-2.4.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jellyfin-mpv-shim",
+    version="2.4.0",
+    author="Ian Walton",
+    author_email="iwalton3@gmail.com",
+    description="Cast media from Jellyfin Mobile and Web apps to MPV.",
+    license="GPLv3",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jellyfin/jellyfin-mpv-shim",
+    packages=["jellyfin_mpv_shim", "jellyfin_mpv_shim.display_mirror"],
+    package_data={
+        "jellyfin_mpv_shim.display_mirror": ["*.css", "*.html"],
+        "jellyfin_mpv_shim": ["systray.png"],
+    },
+    entry_points={
+        "console_scripts": ["jellyfin-mpv-shim=jellyfin_mpv_shim.mpv_shim:main"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        "gui": ["pystray", "pillow"],
+        "mirror": ["Jinja2", "pywebview>=3.3.1"],
+        "discord": ["pypresence"],
+        "all": ["Jinja2", "pywebview>=3.3.1", "pystray", "pypresence", "pillow"],
+    },
+    python_requires=">=3.6",
+    install_requires=[
+        "python-mpv",
+        "jellyfin-apiclient-python>=1.9.2",
+        "python-mpv-jsonipc>=1.1.14",
+        "requests",
+    ],
+    include_package_data=True,
+)
