1304d2837003deb9c848385e635cfa504adef6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:09 +0000

    Add package VectorizeList/1.0.3/VectorizeList-1.0.3.tar.gz

---
+++ packages/VectorizeList/VectorizeList-1.0.3.tar.gz/VectorizeList-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+setup(name='VectorizeList',
+      version='1.0.3',
+      ext_modules = [Extension('VectorizeList', ['VectorizeList.c', 'VectorizeList.h'])],
+      license="GNU3",
+      description="Vectorize a list of strings into ints",
+      author="Julien Calenge",
+      author_email="julien.calenge@epitech.eu",
+      url="https://github.com/jclge/VectorizeList",
+      download_url="https://github.com/jclge/VectorizeList/archive/refs/tags/v1.0.tar.gz"
+      )
