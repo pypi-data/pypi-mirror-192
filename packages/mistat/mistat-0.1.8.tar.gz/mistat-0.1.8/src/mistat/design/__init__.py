d7846bfcaf9dffa43aaa032e226028c5f796a7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:20 +0000

    Add package mistat/0.1.8/mistat-0.1.8.tar.gz

---
+++ packages/mistat/mistat-0.1.8.tar.gz/mistat-0.1.8/src/mistat/design/__init__.py
@@ -0,0 +1,5 @@
+from . import doe
+from .doeUtilities import addTreatments, aliasesInSubgroup, subgroupOfDefining
+from .graphs import (FDS_Plot, calculateInteractions, calculateMainEffects,
+                     getModelMatrix, interactionPlot, mainEffectsPlot,
+                     marginalInteractionPlot)
