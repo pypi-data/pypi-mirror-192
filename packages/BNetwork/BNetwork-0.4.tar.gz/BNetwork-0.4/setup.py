9d4c8f0b8601f09d6c0a8add9d795fc9f5e0c610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:16 +0000

    Add package BNetwork/0.4/BNetwork-0.4.tar.gz

---
+++ packages/BNetwork/BNetwork-0.4.tar.gz/BNetwork-0.4/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="BNetwork",
+    packages=find_packages(),
+    version="0.4",
+    description="A Python package for building Bayesian Networks",
+    url="https://github.com/moraleseoscar/BayesianNetworks",
+    author="Oscar Estrada",
+    author_email="est20565@uvg.edu.gt",
+    license="MIT"
+    )
+
+
