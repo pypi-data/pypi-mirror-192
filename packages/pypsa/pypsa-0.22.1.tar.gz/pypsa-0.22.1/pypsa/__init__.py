7dbb6021aa6c182fb1a372ae974028540da667be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:31 +0000

    Add package pypsa/0.22.1/pypsa-0.22.1.tar.gz

---
+++ packages/pypsa/pypsa-0.22.1.tar.gz/pypsa-0.22.1/pypsa/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Python for Power Systems Analysis (PyPSA)
+
+Grid calculation library.
+"""
+
+from pypsa import (
+    components,
+    contingency,
+    descriptors,
+    examples,
+    geo,
+    io,
+    linopf,
+    linopt,
+    networkclustering,
+    opf,
+    opt,
+    optimization,
+    pf,
+    plot,
+)
+from pypsa.components import Network, SubNetwork
+
+__version__ = "0.22.1"
+
+__author__ = (
+    "PyPSA Developers, see https://pypsa.readthedocs.io/en/latest/developers.html"
+)
+__copyright__ = (
+    "Copyright 2015-2023 PyPSA Developers, see https://pypsa.readthedocs.io/en/latest/developers.html, "
+    "MIT License"
+)
