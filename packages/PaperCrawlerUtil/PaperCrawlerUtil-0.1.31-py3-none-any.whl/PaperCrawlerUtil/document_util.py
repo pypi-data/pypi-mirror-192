05abe16e0598eb18695ed3da66513f5a2456177b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:25 +0000

    Add package PaperCrawlerUtil/0.1.31/PaperCrawlerUtil-0.1.31-py3-none-any.whl

---
+++ packages/PaperCrawlerUtil/PaperCrawlerUtil-0.1.31-py3-none-any.whl/PaperCrawlerUtil/document_util.py
@@ -0,0 +1,6 @@
+from PaperCrawlerUtil.translate_util import *
+from PaperCrawlerUtil.pdf_util import *
+from PaperCrawlerUtil.office_util import *
+
+if __name__ == "__main__":
+    basic_config(logs_style=LOG_STYLE_PRINT)
