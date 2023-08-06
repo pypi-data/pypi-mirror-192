d44d7afc9536ea3212af8c5bbbd2197c30eee490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:26 +0000

    Add package django-mermaid/0.0.0/django-mermaid-0.0.0.tar.gz

---
+++ packages/django-mermaid/django-mermaid-0.0.0.tar.gz/django-mermaid-0.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+name = input("Project name: ")
+
+setuptools.setup(
+    name=name,
+    version="0.0.0",
+    url="https://pysnippet.org/",
+    author="Artyom Vancyan",
+    author_email="artyom@pysnippet.org",
+    description="The package is coming soon...",
+)
