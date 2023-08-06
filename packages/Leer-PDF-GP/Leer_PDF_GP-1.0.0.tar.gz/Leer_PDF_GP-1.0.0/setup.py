b90da5838233fae2d504496ce1a48589be89d1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:53 +0000

    Add package Leer-PDF-GP/1.0.0/Leer_PDF_GP-1.0.0.tar.gz

---
+++ packages/Leer-PDF-GP/Leer_PDF_GP-1.0.0.tar.gz/Leer_PDF_GP-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.0.0' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
+PACKAGE_NAME = 'Leer_PDF_GP' #Debe coincidir con el nombre de la carpeta 
+AUTHOR = 'Ander Valencia' #Modificar con vuestros datos
+AUTHOR_EMAIL = 'a@gmail.com' #Modificar con vuestros datos
+URL = 'https://github.com' #Modificar con vuestros datos
+
+LICENSE = 'MIT' #Tipo de licencia
+DESCRIPTION = 'Librería para leer ficheros PDFs y extraer la información en formato str' #Descripción corta
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8') #Referencia al documento README con una descripción más elaborada
+LONG_DESC_TYPE = "text/markdown"
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
