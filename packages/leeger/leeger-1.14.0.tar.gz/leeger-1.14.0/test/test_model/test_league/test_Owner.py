a00ddd93037fb075979fd6668c61a75d20a488cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:39 +0000

    Add package leeger/1.14.0/leeger-1.14.0.tar.gz

---
+++ packages/leeger/leeger-1.14.0.tar.gz/leeger-1.14.0/test/test_model/test_league/test_Owner.py
@@ -0,0 +1,35 @@
+import unittest
+
+from leeger.model.league.Owner import Owner
+
+
+class TestOwner(unittest.TestCase):
+    def test_owner_init(self):
+        owner = Owner(name="name")
+
+        self.assertEqual("name", owner.name)
+
+    def test_owner_eq_equal(self):
+        # create Owner 1
+        owner_1 = Owner(name="owner")
+
+        # create Owner 2
+        owner_2 = Owner(name="owner")
+
+        self.assertEqual(owner_1, owner_2)
+
+    def test_owner_eq_notEqual(self):
+        # create Owner 1
+        owner_1 = Owner(name="owner")
+
+        # create Owner 2
+        owner_2 = Owner(name="ownerDIF")
+
+        self.assertNotEqual(owner_1, owner_2)
+
+    def test_owner_toJson(self):
+        owner = Owner(name="owner")
+        ownerJson = owner.toJson()
+
+        self.assertIsInstance(ownerJson, dict)
+        self.assertEqual("owner", ownerJson["name"])
