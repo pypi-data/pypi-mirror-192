fe50b5a7f7287f49d6ff218b8e88aaf02371231d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:28 +0000

    Add package whatsmate/1.0/whatsmate-1.0.tar.gz

---
+++ packages/whatsmate/whatsmate-1.0.tar.gz/whatsmate-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whatsmate',
+    version='1.0',
+    description='The ultimate WhatsApp Wrapper for Python',
+    author='Harsh Vardhan Goswami',
+    author_email='me@iamharsh.dev',
+    packages=['whatsmate'],
+    long_description='With WhatsMate, you can send WhatsApp messages, from sending hilarious memes to '
+                     'your friends to spamming your ex with 1000 "I miss you" messages. Our powerful API lets you do '
+                     'it all, and with just a few lines of Python code!',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='whatsapp automation chatbot api',
+    license='MIT',
+    url='https://github.com/iamharshdev/whatsmate',
+)
