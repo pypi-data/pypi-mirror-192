ef2b88cfabba69c35b6fd00eb2035b24322e06e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:16 +0000

    Add package kretanje-cijena/0.0.3/kretanje_cijena-0.0.3-py3-none-any.whl

---
+++ packages/kretanje-cijena/kretanje_cijena-0.0.3-py3-none-any.whl/kretanje_cijena/base.py
@@ -0,0 +1,17 @@
+"""
+kretanje_cijena base module.
+
+This is the principal module of the kretanje_cijena project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "kretanje_cijena"
---
+++ packages/kretanje-cijena/kretanje_cijena-0.0.3-py3-none-any.whl/kretanje_cijena/cli.py
@@ -0,0 +1,28 @@
+"""CLI interface for kretanje_cijena project.
+"""
+
+from brds import Fetcher as _Fetcher
+from brds import FileWriter as _FileWriter
+from brds import fload as _fload
+from brds import GunzipImporter as _GunzipImporter
+
+
+def main() -> None:  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m kretanje_cijena` and `$ kretanje_cijena `.
+
+    """
+    fetcher = _Fetcher(
+        _GunzipImporter("https://kretanje-cijena.hr:50001"),
+        _FileWriter.from_environment(),
+        ["/data/public"],
+        "kretanje-cijena",
+    )
+    fetcher.fetch()
+
+    data = _fload("kretanje-cijena/data/public")
+    for data_type, values in data.items():
+        print(data_type, len(values))
+        if len(values) > 0:
+            print(values[0].keys())
