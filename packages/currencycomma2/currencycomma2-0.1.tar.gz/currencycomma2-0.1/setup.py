57f451cf64b8695c909cdc48e5f9011775e2f381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:02 +0000

    Add package currencycomma2/0.1/currencycomma2-0.1.tar.gz

---
+++ packages/currencycomma2/currencycomma2-0.1.tar.gz/currencycomma2-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='currencycomma2',
+    version='0.1',
+    description='A package for formatting currency values',
+    author='Milad Mirza',
+    author_email='Miladmirza75@gmail.com',
+    url='https://github.com/miladmirza75/currency',
+    packages=['currencycomma2'],
+    install_requires=[],
+)
