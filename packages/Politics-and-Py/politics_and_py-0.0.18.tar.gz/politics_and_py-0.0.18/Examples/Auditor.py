cd9b128ab205bf4a7f2f2012e72a7e608e6c2b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:56 +0000

    Add package Politics-and-Py/0.0.18/politics_and_py-0.0.18.tar.gz

---
+++ packages/Politics-and-Py/politics_and_py-0.0.18.tar.gz/politics_and_py-0.0.18/Examples/Auditor.py
@@ -0,0 +1,13 @@
+# for now you have to import using this line:
+from src.Politics_and_Py import \
+    Core as PnP
+
+# import PnP
+
+# Set Key
+PnP.key(input("Please input api key: "))
+
+# Get a Nation() object of the user
+User = PnP.Nation(input("What is your Nation ID?"))
+User.update_long()
+
