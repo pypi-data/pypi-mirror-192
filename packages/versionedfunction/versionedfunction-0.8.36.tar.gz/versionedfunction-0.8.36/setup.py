0168177f41347a2f2c44871cfb4ffee8b984b84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:27 +0000

    Add package versionedfunction/0.8.36/versionedfunction-0.8.36.tar.gz

---
+++ packages/versionedfunction/versionedfunction-0.8.36.tar.gz/versionedfunction-0.8.36/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['versionedfunction']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'versionedfunction',
+    'version': '0.8.36',
+    'description': 'Sometimes you want to be able to dynamically call different versions of a function',
+    'long_description': 'None',
+    'author': 'John Heintz',
+    'author_email': 'john@gistlabs.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
