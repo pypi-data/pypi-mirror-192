0440192c49b35deb92833e87b0643aced4a2db99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:24 +0000

    Add package knifes/0.8.52/knifes-0.8.52-py3-none-any.whl

---
+++ packages/knifes/knifes-0.8.52-py3-none-any.whl/knifes/constants.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class AreaEnum(Enum):
+    DOMESTIC = 1
+    OVERSEAS = 2
+
+    def __str__(self):
+        return self.name.lower()
+
+
+# TODO 废弃 迁移到了media module
+class MediaType(Enum):
+    VIDEO = 'video'
+    AUDIO = 'audio'
+    IMAGE = 'image'
+
+    def __str__(self):
+        return self.value
+
+
+APPLE_VERIFY_RECEIPT_PROD = 'https://buy.itunes.apple.com/verifyReceipt'
+APPLE_VERIFY_RECEIPT_SANDBOX = 'https://sandbox.itunes.apple.com/verifyReceipt'
+ALIPAY_SERVER_URL = 'https://openapi.alipay.com/gateway.do'
