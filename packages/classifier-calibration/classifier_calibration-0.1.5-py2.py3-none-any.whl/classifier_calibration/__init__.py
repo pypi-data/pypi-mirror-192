14ddc1acc3750ae0b18adf64bdb0434fbc906cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:37 +0000

    Add package classifier-calibration/0.1.5/classifier_calibration-0.1.5-py2.py3-none-any.whl

---
+++ packages/classifier-calibration/classifier_calibration-0.1.5-py2.py3-none-any.whl/classifier_calibration/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for classifier-calibration."""
+
+__author__ = """Conor Walsh"""
+__email__ = 'conorwalsh206@gmail.com'
+__version__ = '0.1.5'
+
+from classifier_calibration import calibration_error  
