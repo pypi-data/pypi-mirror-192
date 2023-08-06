e63d42a712280f88c1f020e36f38abc183a3299f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:20 +0000

    Add package vseg-unet/0.0.4/vseg-unet-0.0.4.tar.gz

---
+++ packages/vseg-unet/vseg-unet-0.0.4.tar.gz/vseg-unet-0.0.4/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'U-net for vessel segmentation'
+LONG_DESCRIPTION = 'U-net for vessel segmentation on large tif for spinal cord microscopy images of mice'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="vseg-unet", 
+        version=VERSION,
+        author="Olivier Tastet",
+        author_email="o.tastet33@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['torch',
+                          'opencv-python',
+                          'random',
+                          'natsort',
+                          'sklearn',
+                          'patchify',
+                          'tqdm',
+                          'scipy',
+                          'pandas',
+                          'albumentations',
+                          'pillow',
+                          'numpy',
+                          'argparse',
+                          'matplotlib',
+                          'torchvision'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'vessel segmentation'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
