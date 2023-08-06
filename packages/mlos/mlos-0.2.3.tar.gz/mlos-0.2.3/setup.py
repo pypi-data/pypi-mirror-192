7f464ef1480675d83a09eb2bf3fdaa14487ed8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:00 +0000

    Add package mlos/0.2.3/mlos-0.2.3.tar.gz

---
+++ packages/mlos/mlos-0.2.3.tar.gz/mlos-0.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+# pip3 install --no-cache-dir mlos==0.0.30
+setup(
+    name='mlos',
+    version='0.2.3',    
+    description='mlOS test Package',
+    author='Padma Polash Paul',
+    author_email='ppaul@dpami.com',    
+    license='Braintoy@2023',
+    packages=['mlos'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
