6478ad77b9555a4bca2eae9fe5c0ae6a0cb32714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:21 +0000

    Add package interval-tools/0.10.1/interval_tools-0.10.1-py3-none-any.whl

---
+++ packages/interval-tools/interval_tools-0.10.1-py3-none-any.whl/interval/ddd/event.py
@@ -0,0 +1,28 @@
+"""
+interval.ddd.event
+~~~~~~~~~~~~~~~~~~
+
+This module provides DDD event base classes.
+"""
+
+import uuid
+from dataclasses import dataclass, field
+from datetime import datetime
+
+
+@dataclass
+class DomainEvent:
+    """领域事件
+
+    Attributes:
+        id: 事件ID
+        occurred_at: 事件发生时间（包含系统本地时区）
+    """
+    id: str = field(
+        default_factory=lambda: str(uuid.uuid1()),
+        init=False
+    )
+    occurred_at: datetime = field(
+        default_factory=lambda: datetime.now().astimezone(),
+        init=False
+    )
