afe0a7939240957e6913c236b3fcfeeb5784f87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:32 +0000

    Add package paper2txt/0.0.1/paper2txt-0.0.1.tar.gz

---
+++ packages/paper2txt/paper2txt-0.0.1.tar.gz/paper2txt-0.0.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['paper2txt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pdfminer']
+
+entry_points = \
+{'console_scripts': ['paper2txt = paper2txt.executable:paper2txt_main']}
+
+setup_kwargs = {
+    'name': 'paper2txt',
+    'version': '0.0.1',
+    'description': 'Simple tool to extract text from scientific PDFs',
+    'long_description': '# Wibble - a template for simple python packages\n\nA minimalist template for a python package.\n\nProvides the bl and the blarg commands.\n',
+    'author': 'Reto Stamm',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/retospect/paper2txt',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+}
+
+
+setup(**setup_kwargs)
