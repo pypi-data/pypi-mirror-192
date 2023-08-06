33afa242f78c5235c82e07c50f8c55d3e531500a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:13 +0000

    Add package spackle-python/0.0.22/spackle-python-0.0.22.tar.gz

---
+++ packages/spackle-python/spackle-python-0.0.22.tar.gz/spackle-python-0.0.22/spackle/__init__.py
@@ -0,0 +1,18 @@
+import threading
+from spackle.customer import Customer
+from spackle.exceptions import SpackleException
+from spackle.stores import DynamoDBStore
+
+api_key = None
+api_base = "https://api.spackle.so/v1"
+store_cls = DynamoDBStore
+schema_version = 1
+
+
+_thread = threading.local()
+
+
+def get_store():
+    if not hasattr(_thread, "adapter") or not isinstance(_thread.adapter, store_cls):
+        _thread.adapter = store_cls()
+    return _thread.adapter
