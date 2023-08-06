d04f4ce23ec74fd5092cc28f22333030e780b20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:44 +0000

    Add package ubcpdk/1.20.0/ubcpdk-1.20.0.tar.gz

---
+++ packages/ubcpdk/ubcpdk-1.20.0.tar.gz/ubcpdk-1.20.0/ubcpdk/samples/test_masks.py
@@ -0,0 +1,27 @@
+"""Write all mask for the course."""
+
+import ubcpdk.samples.ubc_joaquin_matres1 as m11
+import ubcpdk.samples.ubc_helge as m12
+import ubcpdk.samples.ubc_simon as m13
+
+
+def test_masks_2023_v1():
+    """Write all masks for 2023_v1."""
+    for mask in [
+        m11.test_mask1,
+        m11.test_mask2,
+        m11.test_mask3,
+        m11.test_mask4,
+        m11.test_mask5,
+        m12.test_mask1,
+        m12.test_mask2,
+        m13.test_mask1,
+        m13.test_mask3,
+        m13.test_mask4,
+        m13.test_mask5,
+    ]:
+        m, tm = mask()
+
+
+if __name__ == "__main__":
+    test_masks_2023_v1()
