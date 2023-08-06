225c8e2694d249621e86b672697a35df7b99bdce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:43 +0000

    Add package anipy-cli/2.7.5/anipy_cli-2.7.5.tar.gz

---
+++ packages/anipy-cli/anipy_cli-2.7.5.tar.gz/anipy_cli-2.7.5/anipy_cli/player/players/__init__.py
@@ -0,0 +1,4 @@
+from anipy_cli.player.players.mpv_contrl import MpvControllable
+from anipy_cli.player.players.mpv import Mpv
+from anipy_cli.player.players.vlc import Vlc
+from anipy_cli.player.players.syncplay import Syncplay
