9df6362e5f34522aaffdfa6f5d254d5dcd76076a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:36 +0000

    Add package teachera/99/teachera-99.tar.gz

---
+++ packages/teachera/teachera-99.tar.gz/teachera-99/README.md
@@ -0,0 +1,5 @@
+```
+python3 setup.py sdist
+python3 setup.py register
+python3 setup.py sdist upload
+```
---
+++ packages/teachera/teachera-99.tar.gz/teachera-99/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='teachera',
+    version="99",
+    description='Dependency confusion demo library for learning purposes',
+    author='Practical DevSecOps',
+    author_email='info@practical-devsecops.com',
+    py_modules=['teachera']
+)
