b703a7eaa01ad72233b15a3b0216393cb7055a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:05 +0000

    Add package EventSimpleGUI/0.2.8/EventSimpleGUI-0.2.8.tar.gz

---
+++ packages/EventSimpleGUI/EventSimpleGUI-0.2.8.tar.gz/EventSimpleGUI-0.2.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+with open('README.md', 'rt') as arq:
+      readme = arq.read()
+
+keywords = [
+      'EventSimpleGUI'.lower(), 'EventSimpleGUI', 'simplegui', 'GUI', 'gui', 'events for simplegui',
+      'generate events', 'fast events', 'events'
+]
+
+setup(name='EventSimpleGUI',
+      version='0.2.8',
+      license='MIT license',
+      author='Daniel Coêlho',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      author_email='heromon.9010@gmail.com',
+      keywords=keywords,
+      description='A simple tool to create events to PySimpleGUI',
+      packages=['pysimpleevent'],
+      install_requires=['PySimpleGUI']
+)
