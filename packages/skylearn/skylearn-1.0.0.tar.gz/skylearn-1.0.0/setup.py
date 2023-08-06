13c239bed58f1b83008cce41ea4e26e632e7aab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:06 +0000

    Add package skylearn/1.0.0/skylearn-1.0.0.tar.gz

---
+++ packages/skylearn/skylearn-1.0.0.tar.gz/skylearn-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='skylearn',
+    version='1.0.0',
+    author='Crotes',
+    author_email='1910546917@qq.com',
+    description='DDDD',
+    long_description_content_type="""text/markdown""",
+    url="https://www.crotes.top",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
