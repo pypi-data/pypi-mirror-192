f3930d3ebf89d3f17ea065ece8ef4a5c13f21699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:39 +0000

    Add package imputing-final/0.0.4/imputing_final-0.0.4.tar.gz

---
+++ packages/imputing-final/imputing_final-0.0.4.tar.gz/imputing_final-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="imputing_final",
+      version="0.0.4",
+      url="https://github.com/waikoreaweatherpjt/imputing_final",
+      licence="MIT",
+      author="wai kimsanghyun",
+      author_email="waik@waikorea.com",
+      keywords=["imputing","numpy","coulmnwise"],
+      description="imputing with numpy no loop",
+      py_modules=["imputing_final"],
+      install_requires=["numpy","pandas"]
+      )
