e306a6a4e15bd9c6971b1d302a45678cfd917ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:04 +0000

    Add package pptestkrzysiekmarut/0.0.1/pptestkrzysiekmarut-0.0.1.tar.gz

---
+++ packages/pptestkrzysiekmarut/pptestkrzysiekmarut-0.0.1.tar.gz/pptestkrzysiekmarut-0.0.1/README.md
@@ -0,0 +1,23 @@
+# `pptestkrzysiekmarut` - My simple first project on PyPi
+
+* OP1
+* Op3
+* Op2
+
+# Install
+
+```python
+pip install pptestkrzysiekmarut 
+```
+
+## Usage
+
+```python
+from pptestkrzysiekmarut.m_mod_pptest import name as n 
+
+>>> n("Write your name")
+Witaj: Name Surname
+
+>>>get_numpy_arr([1, 2, 3, 4, 5])
+[1, 2, 3, 4, 5]
+```
---
+++ packages/pptestkrzysiekmarut/pptestkrzysiekmarut-0.0.1.tar.gz/pptestkrzysiekmarut-0.0.1/pptestkrzysiekmarut/m_mod_pptest.py
@@ -0,0 +1,17 @@
+import numpy
+
+def name(first, last):
+    """Retrun full data of parameters"""
+    full = first + " " + last
+    full=full.lower().title().strip()
+    return "Witaj: " + full
+
+
+def get_numpy_arr(lst):
+    """Returnes array of numpy package"""
+
+    return numpy.array(lst)
+
+if __name__ == "__main__":
+    print(name('Krzysiek', 'Marut'))
+    print(get_numpy_arr([1, 2, 3, 4, 5]))
