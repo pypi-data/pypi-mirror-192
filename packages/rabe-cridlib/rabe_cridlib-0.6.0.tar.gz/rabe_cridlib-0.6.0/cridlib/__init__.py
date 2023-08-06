d34798d9de9fc24a3d07a49f8bf56c6030943869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:02 +0000

    Add package rabe-cridlib/0.6.0/rabe_cridlib-0.6.0.tar.gz

---
+++ packages/rabe-cridlib/rabe_cridlib-0.6.0.tar.gz/rabe_cridlib-0.6.0/cridlib/__init__.py
@@ -0,0 +1,9 @@
+"""Generate RaBe Content Reference Idenitifier Spcification (CRID) Identifiers.
+
+* [`cridlib.get(timestamp=None, fragment='')`](./get/#cridlib.get.get)
+* [`cridlib.parse(value)`](./parse/#gridlib.parse.parse)
+"""
+
+from .get import get
+from .lib import CRIDError
+from .parse import parse
