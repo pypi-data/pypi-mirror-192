aaafee46c7268047a3429391b9b6d354f431595b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:32 +0000

    Add package ayaka-db/0.0.1/ayaka_db-0.0.1.tar.gz

---
+++ packages/ayaka-db/ayaka_db-0.0.1.tar.gz/ayaka_db-0.0.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ayaka_db']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ayaka_utils>=0.0.3,<0.0.4',
+ 'inflection>=0.5.1,<0.6.0',
+ 'loguru>=0.6.0,<0.7.0',
+ 'sqlmodel>=0.0.8,<0.0.9']
+
+setup_kwargs = {
+    'name': 'ayaka-db',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': '嗯嗯嗯嗯',
+    'author': 'Su',
+    'author_email': 'wxlxy316@163.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/bridgeL/ayaka_db',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
