3ae5a8d4667ca08c5412e4d48ba2ae23d3208da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package levelDetector/1.0.0/levelDetector-1.0.0.tar.gz

---
+++ packages/levelDetector/levelDetector-1.0.0.tar.gz/levelDetector-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='levelDetector',
+    packages=find_packages(),
+    version='1.0.0',
+    description='Python Opencv Operations for Level Detector',
+    author='Tharani',
+    author_email="maxeleratorfoundation@gmail.com",
+    license='Maxelerator Foundation',
+)
