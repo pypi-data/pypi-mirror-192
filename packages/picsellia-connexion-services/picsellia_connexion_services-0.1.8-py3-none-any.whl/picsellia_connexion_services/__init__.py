0df219fc668084fae622d1a534b313be43de8774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:16 +0000

    Add package picsellia-connexion-services/0.1.8/picsellia_connexion_services-0.1.8-py3-none-any.whl

---
+++ packages/picsellia-connexion-services/picsellia_connexion_services-0.1.8-py3-none-any.whl/picsellia_connexion_services/__init__.py
@@ -0,0 +1,7 @@
+from .jwt_service import JwtServiceConnexion
+from .token_service import TokenServiceConnexion
+from .mock_service_connexion import MockServiceConnexion
+from .cached_connexions import CachedConnexions
+from .mock_cached_connexions import MockCachedConnexions
+from .abstract_cached_connexions import AbstractCachedConnexions
+from .abstract_service_connexion import AbstractServiceConnexion
---
+++ packages/picsellia-connexion-services/picsellia_connexion_services-0.1.8-py3-none-any.whl/picsellia_connexion_services/token_service.py
@@ -0,0 +1,132 @@
+from typing import IO, Any, Iterable, Mapping, Tuple, Union
+from urllib.parse import urljoin
+
+import requests
+
+from .abstract_service_connexion import AbstractServiceConnexion
+from .exceptions import NetworkError
+
+
+def wrapped_request(f):
+    def decorated(*args, **kwargs):
+        try:
+            resp = f(*args, **kwargs)
+        except Exception:
+            raise NetworkError("Server is not responding, please check your host.")
+        return resp
+
+    return decorated
+
+
+class TokenServiceConnexion(AbstractServiceConnexion):
+    def __init__(
+        self,
+        host: str,
+        api_token: str,
+        authorization_key: str = "Token",
+        content_type: str = "application/json",
+    ) -> None:
+        """TokenServiceConnexion may be used to handle a Token authentication connexion with a service.
+        You need to give an api token valid, recognized by the service called
+        """
+        self.host = host
+        self.api_token = api_token
+        self.headers = {
+            "Authorization": authorization_key + " " + api_token,
+            "Content-type": content_type,
+        }
+        self.session = requests.Session()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.session is not None:
+            self.session.close()
+
+    def add_header(self, key: str, value: str):
+        if key is None or value is None:
+            raise RuntimeError("Header key and value cannot be None")
+        self.headers[key] = value
+
+    @wrapped_request
+    def get(self, path: str, params: dict = None, stream=False):
+        url = urljoin(self.host, path)
+        return self.session.get(
+            url=url, headers=self.headers, params=params, stream=stream
+        )
+
+    @wrapped_request
+    def xget(
+        self,
+        path: str,
+        data: Union[
+            None, str, bytes, Mapping[str, Any], Iterable[Tuple[str, str, None]], IO
+        ] = None,
+        params: dict = None,
+        stream=False,
+    ):
+        url = urljoin(self.host, path)
+        return self.session.request(
+            method="XGET",
+            url=url,
+            data=data,
+            headers=self.headers,
+            params=params,
+            stream=stream,
+        )
+
+    @wrapped_request
+    def post(
+        self,
+        path: str,
+        data: Union[
+            None, str, bytes, Mapping[str, Any], Iterable[Tuple[str, str, None]], IO
+        ] = None,
+        params: dict = None,
+        files: Any = None,
+    ):
+        url = urljoin(self.host, path)
+        return self.session.post(
+            url=url, data=data, headers=self.headers, params=params, files=files
+        )
+
+    @wrapped_request
+    def patch(
+        self,
+        path: str,
+        data: Union[
+            None, str, bytes, Mapping[str, Any], Iterable[Tuple[str, str, None]], IO
+        ] = None,
+        params: dict = None,
+    ):
+        url = urljoin(self.host, path)
+        return self.session.patch(
+            url=url, data=data, headers=self.headers, params=params
+        )
+
+    @wrapped_request
+    def put(
+        self,
+        path: str,
+        data: Union[
+            None, str, bytes, Mapping[str, Any], Iterable[Tuple[str, str, None]], IO
+        ] = None,
+        params: dict = None,
+    ):
+        url = urljoin(self.host, path)
+        return self.session.put(url=url, data=data, headers=self.headers, params=params)
+
+    @wrapped_request
+    def delete(
+        self,
+        path: str,
+        data: Union[
+            None, str, bytes, Mapping[str, Any], Iterable[Tuple[str, str, None]], IO
+        ] = None,
+        params: dict = None,
+    ):
+        url = urljoin(self.host, path)
+        return self.session.delete(
+            url=url, data=data, headers=self.headers, params=params
+        )
