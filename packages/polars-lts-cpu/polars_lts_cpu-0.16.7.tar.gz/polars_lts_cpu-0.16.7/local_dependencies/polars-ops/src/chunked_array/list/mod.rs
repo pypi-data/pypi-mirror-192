14ca48e772afcdc311cb96d050ab8d2638e91d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:20 +0000

    Add package polars-lts-cpu/0.16.7/polars_lts_cpu-0.16.7.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.16.7.tar.gz/polars_lts_cpu-0.16.7/local_dependencies/polars-ops/src/chunked_array/list/mod.rs
@@ -0,0 +1,26 @@
+use polars_core::prelude::*;
+
+mod count;
+#[cfg(feature = "hash")]
+pub(crate) mod hash;
+mod namespace;
+#[cfg(feature = "list_to_struct")]
+mod to_struct;
+
+#[cfg(feature = "list_count")]
+pub use count::*;
+#[cfg(not(feature = "list_count"))]
+use count::*;
+pub use namespace::*;
+#[cfg(feature = "list_to_struct")]
+pub use to_struct::*;
+
+pub trait AsList {
+    fn as_list(&self) -> &ListChunked;
+}
+
+impl AsList for ListChunked {
+    fn as_list(&self) -> &ListChunked {
+        self
+    }
+}
