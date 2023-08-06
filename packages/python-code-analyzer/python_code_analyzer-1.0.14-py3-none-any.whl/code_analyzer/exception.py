4ba7482d90bfaf7936923c1decfad232cd4a9203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:44 +0000

    Add package python-code-analyzer/1.0.14/python_code_analyzer-1.0.14-py3-none-any.whl

---
+++ packages/python-code-analyzer/python_code_analyzer-1.0.14-py3-none-any.whl/code_analyzer/exception.py
@@ -0,0 +1,31 @@
+"""
+Date created: 11/6/2022
+
+Purpose:
+
+Details:
+
+Description:
+
+Notes:
+
+IMPORTANT NOTES:
+
+Explanation:
+
+Tags:
+
+Contributors:
+    https://github.com/josephedradan
+
+Reference:
+
+"""
+
+
+class NoScopeAvailable(Exception):
+    pass
+
+
+class IllegalCall(Exception):
+    pass
