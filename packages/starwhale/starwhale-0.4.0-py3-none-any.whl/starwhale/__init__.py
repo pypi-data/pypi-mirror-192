17bf5e3549c8d1e9547868328709ecf6602f31f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:41 +0000

    Add package starwhale/0.4.0/starwhale-0.4.0-py3-none-any.whl

---
+++ packages/starwhale/starwhale-0.4.0-py3-none-any.whl/starwhale/__init__.py
@@ -0,0 +1,75 @@
+from starwhale.api.job import step, Context, pass_context
+from starwhale.version import STARWHALE_VERSION as __version__
+from starwhale.base.uri import URI, URIType
+from starwhale.api.model import PipelineHandler, PPLResultStorage, PPLResultIterator
+from starwhale.api.metric import multi_classification
+from starwhale.api.dataset import (
+    Line,
+    Link,
+    Text,
+    Audio,
+    Image,
+    Point,
+    Video,
+    Binary,
+    Dataset,
+    Polygon,
+    LinkAuth,
+    LinkType,
+    MIMEType,
+    ClassLabel,
+    S3LinkAuth,
+    BoundingBox,
+    NumpyBinary,
+    BoundingBox3D,
+    BuildExecutor,
+    GrayscaleImage,
+    get_data_loader,
+    LocalFSLinkAuth,
+    DefaultS3LinkAuth,
+    COCOObjectAnnotation,
+)
+from starwhale.api.evaluation import Evaluation
+from starwhale.core.dataset.tabular import get_dataset_consumption
+
+dataset = Dataset.dataset
+
+__all__ = [
+    "__version__",
+    "PipelineHandler",
+    "multi_classification",
+    "Dataset",
+    "dataset",
+    "URI",
+    "URIType",
+    "step",
+    "pass_context",
+    "Context",
+    "Evaluation",
+    "get_data_loader",
+    "Link",
+    "LinkAuth",
+    "DefaultS3LinkAuth",
+    "LocalFSLinkAuth",
+    "S3LinkAuth",
+    "MIMEType",
+    "LinkType",
+    "BuildExecutor",
+    "Binary",
+    "NumpyBinary",
+    "Text",
+    "Line",
+    "Point",
+    "Polygon",
+    "Audio",
+    "Video",
+    "Image",
+    "ClassLabel",
+    "BoundingBox",
+    "BoundingBox3D",
+    "GrayscaleImage",
+    "COCOObjectAnnotation",
+    "PPLResultStorage",
+    "PPLResultIterator",
+    "get_dataset_consumption",
+]
