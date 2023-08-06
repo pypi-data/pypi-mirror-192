7fb2bed76002b39fa3371ae58e07bcaaf9fdb04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:51 +0000

    Add package gyver-attrs-converter/0.3.0/gyver_attrs_converter-0.3.0.tar.gz

---
+++ packages/gyver-attrs-converter/gyver_attrs_converter-0.3.0.tar.gz/gyver_attrs_converter-0.3.0/t.py
@@ -0,0 +1,49 @@
+from gyver.attrs import define, info
+from timeit import timeit
+from gattrs_converter import make_mapping, deserialize_mapping
+
+
+@define
+class D:
+    d: str = info(alias="dKey")
+
+    def __parse_dict__(self, alias: bool):
+        return {"dKey": self.d} if alias else {"d": self.d}
+
+
+@define
+class C:
+    c: tuple[D, ...] = info(alias="cKey")
+
+    def __parse_dict__(self, alias: bool):
+        if alias:
+            return {
+                "cKey": tuple(item.__parse_dict__(alias) for item in self.c)
+            }
+        return {"c": tuple(item.__parse_dict__(alias) for item in self.c)}
+
+
+@define
+class B:
+    b: C = info(alias="bKey")
+
+    def __parse_dict__(self, alias: bool):
+        if alias:
+            return {"bKey": self.b.__parse_dict__(alias)}
+        return {"b": self.b.__parse_dict__(alias)}
+
+
+@define
+class A:
+    a: B = info(alias="aKey")
+
+    def __parse_dict__(self, alias: bool):
+        if alias:
+            return {"aKey": self.a.__parse_dict__(alias)}
+        return {"a": self.a.__parse_dict__(alias)}
+
+
+obj = A(B(C(tuple(D("other") for _ in range(10)))))
+
+print(deserialize_mapping(make_mapping(obj, True), True))
+print(timeit(lambda: deserialize_mapping(make_mapping(obj, True), True)))
