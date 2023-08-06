1cf476c6a0568ed607c1e4e54b7d2d671b312fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:03 +0000

    Add package ts-deepscan/1.1.0/ts-deepscan-1.1.0.tar.gz

---
+++ packages/ts-deepscan/ts-deepscan-1.1.0.tar.gz/ts-deepscan-1.1.0/ts_deepscan/cli.py
@@ -0,0 +1,194 @@
+import sys
+import json
+import click
+import pathlib
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+from .client import DSScanner
+from .scanner.Scan import Scan
+from .scanner.Scanner import *
+from .scanner.ParallelScanner import ParallelScanner
+
+from .analyser import get_default_analysers
+
+from ts_python_client.client import Client
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('-o', '--output',
+              type=click.Path(path_type=pathlib.Path),
+              help='Output path for the results')
+@click.option('-j', '--jobs',
+              default=-1 if sys.platform != 'win32' else 1, # Turn off multitasking due to the long spawn on Windows
+              help='Number of parallel jobs')
+@click.option('--include-copyright',
+              default=False,
+              is_flag=True,
+              help='Enables searching for copyright information in source code files')
+@click.option('--include-crypto',
+              default=False,
+              is_flag=True,
+              help='Enables searching for used cryptographic algorithms in source code files')
+@click.option('--filter-files',
+              default=False,
+              is_flag=True,
+              help='Only scan files based on commonly used names (LICENSE, README, etc.) and extensions (source code files)')
+@click.option('--pattern',
+              multiple=True,
+              default=[],
+              help='Specify Unix style file name pattern')
+@click.argument('path', type=click.Path(exists=True, path_type=pathlib.Path))
+def scan(output: pathlib.Path,
+         jobs: int,
+         include_copyright: bool,
+         include_crypto: bool,
+         filter_files: bool,
+         pattern: List[str],
+         path: pathlib.Path):
+
+    options = AnalyserOptions(includeCopyright=include_copyright,
+                              filterFiles=filter_files,
+                              filePatterns=list(pattern))
+
+    analysers = get_default_analysers()
+
+    if include_crypto:
+        if sys.platform == 'win32':
+            # Do crypto analysis without multitasking due to spawn + native libs issues on Windows
+            jobs = 1
+
+        try:
+            from .analyser.CryptoAnalyser import CryptoAnalyser
+            analysers.append(CryptoAnalyser())
+        except Exception as err:
+            print('Crypto analyser error: ', err)
+            pass
+
+    result, no_result, stats = execute(path, jobs, analysers, options)
+
+    _scan = Scan(result=result,
+                 no_result =no_result,
+                 stats = stats, # prepare_stats(result, no_result, stats)
+                 options=options)
+    _scan.compute_licenses_compatibility()
+
+    if output:
+        with output.open('w') as fp:
+            fp.write(json.dumps(_scan.to_dict(), indent=2))
+    else:
+        print(json.dumps(_scan.to_dict(), indent=2))
+
+
+
+def execute(path: Path, jobs: int, analysers: List[FileAnalyser], options: AnalyserOptions):
+    from typing import Optional
+    from progress.bar import Bar
+
+    if jobs == 1:
+        scanner = Scanner([path], analysers, options)
+    else:
+        scanner = ParallelScanner(jobs, [path], analysers, options)
+
+    no_result = []
+    def onSuccess(p, r):
+        if not r:
+            no_result.append(p)
+
+    scanner.onFileScanSuccess = onSuccess
+
+    progress_bar: Optional[Bar] = None
+    def onProgress(finishedTasks: int, totalTasks: int):
+        nonlocal progress_bar
+        if finishedTasks == 0:
+            progress_bar = Bar('Scanning', max=totalTasks)
+        elif progress_bar:
+            progress_bar.next()
+
+    scanner.onProgress = onProgress
+
+    result = scanner.run()
+    progress_bar.finish()
+
+    return result, no_result, {
+        'total': scanner.totalTasks,
+        'finished': scanner.finishedTasks
+    }
+
+
+def prepare_stats(result, no_result, stats):
+    copyrights = {}
+    copyrights_info_count = 0
+
+    files = list(result.keys())
+    files += no_result
+
+    for path, res in result.items():
+        for com in res.get('comments', []):
+            for cop in com.get('copyright', []):
+                for h in cop.get('holders', []):
+                    copyrights_info_count += 1
+                    if h in copyrights:
+                        copyrights[h].append(path)
+                    else:
+                        copyrights[h] = [path]
+                    files.remove(path)
+
+    stats['copyright_info'] = copyrights_info_count
+    stats['no_copyright_info'] = len(files)
+
+    stats['copyright'] = copyrights
+    copyrights['no_copyright'] = files
+
+    return stats
+
+
+
+###########
+
+@cli.command()
+@click.option('--project-name',
+              type=str,
+              required=True,
+              help='Project name')
+@click.option('--module-name',
+              type=str,
+              required=True,
+              help='Module name of the scan')
+@click.option('--api-key',
+              type=str,
+              required=True,
+              help='TrustSource API Key')
+@click.option('--base-url',
+              default='',
+              help='TrustSource API base URL')
+@click.option('--deepscan-base-url',
+              default='',
+              help='DeepScan API base URL')
+@click.argument('path', type=click.Path(exists=True, path_type=pathlib.Path))
+def upload(project_name: str,
+           module_name: str,
+           api_key: str,
+           base_url: str,
+           deepscan_base_url: str,
+           path: pathlib.Path):
+
+    with path.open('r') as fp:
+        _scan = Scan.from_dict(json.loads(fp.read()))
+
+    scanner = DSScanner(_scan, moduleName=module_name, deepscanBaseUrl=deepscan_base_url)
+    tool = Client('ts-deepscan', scanner)
+
+    tool.run(baseUrl=base_url,
+             apiKey=api_key,
+             projectName=project_name,
+             skipTransfer=False)
+
+
+
---
+++ packages/ts-deepscan/ts-deepscan-1.1.0.tar.gz/ts-deepscan-1.1.0/ts_deepscan/scanner/Scanner.py
@@ -0,0 +1,114 @@
+# SPDX-FileCopyrightText: 2022 EACG GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import fnmatch
+
+from pathlib import Path
+from typing import List, Tuple, Optional, Callable
+
+from ..analyser.FileAnalyser import *
+
+
+class Scanner(object):
+    __version = "0.3"
+
+    def __init__(self, paths: [Path], analysers: [FileAnalyser], options: AnalyserOptions=None, *args, **kwargs):
+        self.__paths = paths
+
+        self.analysers = analysers
+        self.options = options
+
+        # Statistics
+        self.totalTasks = 0
+        self.finishedTasks = 0
+
+        # Result callbacks
+
+        # Callback accepting relative path and results
+        self.onFileScanSuccess: Optional[Callable[[str, dict], None]] = None
+        # Callback accepting a relative path and an exception
+        self.onFileScanError: Optional[Callable[[str, Exception], None]] = None
+        # Callback accepting number of finished and total tasks
+        self.onProgress: Optional[Callable[[int, int], None]] = None
+
+
+    @staticmethod
+    def scan_file(path: Path, analysers: [FileAnalyser], options: AnalyserOptions) -> dict:
+        result = {}
+
+        if path.is_file() and path.stat().st_size > options.fileSizeLimit:
+            return {}
+
+        analysers = [a for a in analysers if a.accepts(path, options)]
+
+        for analyse in analysers:
+            res = analyse(path, options)
+            if res:
+                result[analyse.category_name] = res
+
+        return result
+
+
+    def run(self):
+        files: List[Tuple[Path, Path]]  = []
+
+        for p in self.__paths:
+            if p.is_file():
+                files.append((p, p.parent))
+
+            elif p.is_dir():
+                for root, ds, fs in os.walk(str(p)):
+                    ds[:] = [d for d in ds if not d.startswith('.')]
+                    for f in fs:
+                        if f.startswith('.'):
+                            continue
+
+                        if not self.options.filePatterns or any(fnmatch.fnmatch(f, pat) for pat in self.options.filePatterns):
+                            files.append((Path(os.path.join(root, f)), p))
+
+        self.totalTasks = len(files)
+        self._notifyProgress()
+
+        return self._do_scan(files)
+
+
+    def _do_scan(self, files: List[Tuple[Path, Path]]) -> dict:
+        results = {}
+
+        for path, root in files:
+            relpath = str(path.relative_to(root))
+            try:
+                result = Scanner.scan_file(path, self.analysers, self.options)
+                self._notifySuccess(relpath, result)
+
+                if result:
+                    results.update({
+                        relpath: result
+                    })
+
+            except Exception as err:
+                self._notifyError(relpath, err)
+
+        return results
+
+
+    def _notifySuccess(self, relpath: str, result: dict):
+        if self.onFileScanSuccess:
+            self.onFileScanSuccess(relpath, result)
+
+        self._progress()
+
+    def _notifyError(self, relpath: str, error: Exception):
+        if self.onFileScanError:
+            self.onFileScanError(relpath, error)
+
+        self._progress()
+
+    def _progress(self):
+        self.finishedTasks += 1
+        self._notifyProgress()
+
+    def _notifyProgress(self):
+        if self.onProgress:
+            self.onProgress(self.finishedTasks, self.totalTasks)
