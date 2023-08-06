edad5a63b1ab1b202cebb7f57e0c5db193f9a9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:07 +0000

    Add package edi-energy-scraper/0.2.0/edi_energy_scraper-0.2.0.tar.gz

---
+++ packages/edi-energy-scraper/edi_energy_scraper-0.2.0.tar.gz/edi_energy_scraper-0.2.0/mwe.py
@@ -0,0 +1,16 @@
+"""
+A minimal working example on how to use this package
+"""
+import asyncio
+
+from edi_energy_scraper import EdiEnergyScraper
+
+
+async def mirror():
+    scraper = EdiEnergyScraper(path_to_mirror_directory="edi_energy_de")
+    await scraper.mirror()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(mirror())
