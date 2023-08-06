65a5f89d401ed4b872b932f1ab45e06e53cea63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:40 +0000

    Add package daaja/0.0.8/daaja-0.0.8-py3-none-any.whl

---
+++ packages/daaja/daaja-0.0.8-py3-none-any.whl/daaja/augmentors/sentence/__init__.py
@@ -0,0 +1,10 @@
+from daaja.augmentors.sentence.back_translation_augmentor import \
+    BackTranslationAugmentor
+from daaja.augmentors.sentence.contextual_augmentor import ContextualAugmentor
+from daaja.augmentors.sentence.randam_delete_augmentor import \
+    RandamDeleteAugmentor
+from daaja.augmentors.sentence.randam_insert_augmentor import \
+    RandamInsertAugmentor
+from daaja.augmentors.sentence.randam_swap_augmentor import RandamSwapAugmentor
+from daaja.augmentors.sentence.synonym_replace_augmentor import \
+    SynonymReplaceAugmentor
