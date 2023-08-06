edb602ad23ca5fdabfcb106bb9ebaf9572b5285d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:44 +0000

    Add package pyxo/0.4/pyxo-0.4.tar.gz

---
+++ packages/pyxo/pyxo-0.4.tar.gz/pyxo-0.4/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from setuptools import setup,find_packages
+
+setup(
+    name='pyxo',
+    version='0.4',
+    author='Rachid Ouhammou',
+    author_email='rachidouhammou21@gmail.com',
+    description='A package that can help create an XO game easly',
+    long_description='A much longer explanation of the project and helpful resources',
+    url='https://github.com/ouhammmourachid/X_O_game.git',
+    keywords='development, XOgame, Game',
+    packages=find_packages(),
+    python_requires='>=3.7, <4',
+)
