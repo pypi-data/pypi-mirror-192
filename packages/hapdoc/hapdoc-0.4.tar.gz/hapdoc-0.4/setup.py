081e126cc200f0444f8597d8b503f566ada20770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:47 +0000

    Add package hapdoc/0.4/hapdoc-0.4.tar.gz

---
+++ packages/hapdoc/hapdoc-0.4.tar.gz/hapdoc-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('./readme.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hapdoc',
+    description='autodoc tool for everything',
+    long_description=long_description,
+    version='0.4',
+    packages=find_packages(),
+    py_modules=['hapdoc'],
+    install_requires=['click', 'colorama', 'fastapi', 'uvicorn'],
+    entry_points={
+        'console_scripts': [
+            'hapdoc = hapdoc.hapdoc:hapdoc'
+        ]
+    }
+)
