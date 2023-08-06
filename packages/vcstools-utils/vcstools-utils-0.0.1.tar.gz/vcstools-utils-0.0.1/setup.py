7da848af17ab53828885144e6f504a2a682db93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:47 +0000

    Add package vcstools-utils/0.0.1/vcstools-utils-0.0.1.tar.gz

---
+++ packages/vcstools-utils/vcstools-utils-0.0.1.tar.gz/vcstools-utils-0.0.1/setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="vcstools-utils",
+    version="0.0.1",
+    author="promobot",
+    author_email="",
+    description="test package for vcstools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gravitacia/vcsp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
