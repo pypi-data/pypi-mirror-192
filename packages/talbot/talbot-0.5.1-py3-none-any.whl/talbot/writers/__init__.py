3e8c1178868cb49e0aad20e494f51c0a84a8d74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:01 +0000

    Add package talbot/0.5.1/talbot-0.5.1-py3-none-any.whl

---
+++ packages/talbot/talbot-0.5.1-py3-none-any.whl/talbot/writers/__init__.py
@@ -0,0 +1,7 @@
+"""Writers for different metadata formats"""
+from .bibtex_writer import write_bibtex
+from .citation_writer import write_citation
+from .citeproc_writer import write_citeproc
+from .ris_writer import write_ris
+from .schema_org_writer import write_schema_org
+from .datacite_writer import write_datacite
