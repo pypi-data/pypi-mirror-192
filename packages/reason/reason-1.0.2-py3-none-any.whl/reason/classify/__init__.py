9f670a816ed701fbd2bbb7d7ac7b6449e2015d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:04 +0000

    Add package reason/1.0.2/reason-1.0.2-py3-none-any.whl

---
+++ packages/reason/reason-1.0.2-py3-none-any.whl/reason/classify/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""classify package.
+
+Classification tools using machine learning methods.
+
+API:
+
+* NaiveBayesClassifier (class): Classifier which uses naive bayes algorithm.
+* [To-Do] KNNClassifier (class): Classifier which uses KNN algorithm.
+* [To-Do] DecisionTreeClassifier (class): Classifier which uses decision tree algorithm.
+
+"""
+
+from ._naivebayes import NaiveBayesClassifier
