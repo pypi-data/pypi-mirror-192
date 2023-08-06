d8e5cad0cc6dc3fc83b38be1cfb96fa9cc3a3d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:43 +0000

    Add package hag/0.3.3/hag-0.3.3-py3-none-any.whl

---
+++ packages/hag/hag-0.3.3-py3-none-any.whl/hag/parsers/__init__.py
@@ -0,0 +1,33 @@
+from .alacritty import Alacritty
+from .bash import Bash
+from .lf import Lf
+from .mpv import Mpv
+from .neovim import Neovim
+from .qutebrowser import Qutebrowser
+from .rofi import Rofi
+from .sioyek import Sioyek
+from .sxhkd import Sxhkd
+from .sxiv import Sxiv
+from .termite import Termite
+from .vim import Vim
+from .vimiv import Vimiv
+from .zathura import Zathura
+from .zsh import Zsh
+
+__all__ = [
+    "Alacritty",
+    "Bash",
+    "Lf",
+    "Mpv",
+    "Neovim",
+    "Qutebrowser",
+    "Rofi",
+    "Sioyek",
+    "Sxhkd",
+    "Sxiv",
+    "Termite",
+    "Vim",
+    "Vimiv",
+    "Zathura",
+    "Zsh",
+]
