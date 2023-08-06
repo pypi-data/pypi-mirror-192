b03c21c5741d25209a02753a12c35b888fe498a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:11 +0000

    Add package git-batch/2.0.10/git_batch-2.0.10-py3-none-any.whl

---
+++ packages/git-batch/git_batch-2.0.10-py3-none-any.whl/gitbatch/utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Global utility methods and classes."""
+
+import os
+from distutils.util import strtobool
+
+
+def normalize_path(path):
+    if path:
+        return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+
+    return None
+
+
+def to_bool(string):
+    return bool(strtobool(str(string)))
+
+
+class Singleton(type):
+    """Meta singleton class."""
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
