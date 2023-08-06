472658621b360d49239bd4def1d3ea2595e67a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:29 +0000

    Add package voices/1.0.0a3/voices-1.0.0a3-py3-none-any.whl

---
+++ packages/voices/voices-1.0.0a3-py3-none-any.whl/VoiceS/text/__init__.py
@@ -0,0 +1,17 @@
+from .slice import Slice,Lyrics
+from . import ass
+
+from typing import Literal
+
+
+support_type = Literal["ass"]
+
+
+def load_text(text: str, text_type: support_type):
+    try:
+        if text_type == "ass":
+            return ass.load(text)
+        else:
+            raise NotImplementedError
+    except Exception as e:
+        print(e)
