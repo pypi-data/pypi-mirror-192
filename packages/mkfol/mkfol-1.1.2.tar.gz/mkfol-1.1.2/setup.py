d0510a93b0068e1eec4d0086dce0eb23d4856718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:05 +0000

    Add package mkfol/1.1.2/mkfol-1.1.2.tar.gz

---
+++ packages/mkfol/mkfol-1.1.2.tar.gz/mkfol-1.1.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name='mkfol',
+    version='1.1.2',
+    packages=["mkfol"],
+    entry_points={
+        'console_scripts': ['mkfol=mkfol.mkfol:mkfol']
+    },
+    author='Sulthan Abiyyu Hakim',
+    author_email="sabiyyuhakim@student.ub.ac.id",
+    description='Folder maker based on template',
+    install_requires=[
+        'setuptools',
+    ],
+    python_requires='>=3.5',
+)
