f8e0066168221cd3f75c2df9c5df8c1366b9b3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:38 +0000

    Add package chain-pymysql/1.0.6/chain-pymysql-1.0.6.tar.gz

---
+++ packages/chain-pymysql/chain-pymysql-1.0.6.tar.gz/chain-pymysql-1.0.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+version = '1.0.6'
+
+with open('./README.rst', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='chain-pymysql',
+    version=version,
+    url='https://github.com/Tiacx/chain-pymysql',
+    project_urls={
+        'Documentation': 'https://github.com/Tiacx/chain-pymysql',
+    },
+    description='Easy to use PyMySQL.',
+    long_description=readme,
+    author='Taic',
+    packages=['chain_pymysql'],
+    install_requires=['pymysql'],
+    classifiers=[
+        # Chose either '3 - Alpha', '4 - Beta' or '5 - Production/Stable' as the current state of your package
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Database',
+    ],
+    keywords='ChainPyMySql',
+)
