441f2b4e4f8468ea8c3cc05c01e5fd39291c3c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:09 +0000

    Add package rainel/1.0/rainel-1.0.tar.gz

---
+++ packages/rainel/rainel-1.0.tar.gz/rainel-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rainel',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['rainel'],
+      author='Felipe Rainel',
+      author_email= 'raineldias88@gmail.com',
+      zip_safe=False)
