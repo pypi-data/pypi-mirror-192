d67d3745eec178724df250ea0b38ed9dd3088263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:37 +0000

    Add package oxeekparser/3.3.0/oxeekparser-3.3.0-py3-none-any.whl

---
+++ packages/oxeekparser/oxeekparser-3.3.0-py3-none-any.whl/oxeekparser/cmd_integration.py
@@ -0,0 +1,27 @@
+import os
+
+from parser import Parser
+import json
+import sys
+
+try:
+    chrome_path = sys.argv[1]
+    profile_path = sys.argv[2]
+    sleep = int(sys.argv[3])
+    url = sys.argv[4]
+
+    #TODO: Добавить автоактивацию венва
+
+    parser = Parser(chrome_path, profile_path, sleep=sleep)
+    html = parser.handle(url).get_html()
+    response_code = parser.get_response()
+
+    result = {'html': html, 'response_code': response_code}
+except:
+    result = {}
+
+os.system('cls||clear')
+print(result)
+
+
+
