5dacfafe7591ca9732324a3d315ddb63a29948ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:45 +0000

    Add package PyMolecules/1.0.0/PyMolecules-1.0.0.tar.gz

---
+++ packages/PyMolecules/PyMolecules-1.0.0.tar.gz/PyMolecules-1.0.0/PyMolecules/__init__.py
@@ -0,0 +1,6 @@
+from PyMolecules import SingleLinkedList
+
+
+__author__ = "Yokesh Bollineni"
+__version__ = "1.0.0"
+__what_is_new__ = "Added DLL class which has more optimized implementation of methods when compare to SLL"
