274d8d1c48a86f5edf3203cc329220e291d4a1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:33 +0000

    Add package tool-for-vcs-p/0.0.1/tool_for_vcs_p-0.0.1.tar.gz

---
+++ packages/tool-for-vcs-p/tool_for_vcs_p-0.0.1.tar.gz/tool_for_vcs_p-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+	name='tool_for_vcs_p',
+	version='0.0.1',
+    description='A demo package.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'parse_work=package.vcs_p:parse_work'
+        ]
+    },
+)
