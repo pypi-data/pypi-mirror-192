1ad8c7a5805c7239f598258941e22be5b6d5ee86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:11 +0000

    Add package indonesian.BMKG/0.3/indonesian.BMKG-0.3.tar.gz

---
+++ packages/indonesian.BMKG/indonesian.BMKG-0.3.tar.gz/indonesian.BMKG-0.3/README.md
@@ -0,0 +1,8 @@
+# Indonesian Earthquake Update
+this is an package of latest indonesian earthquake
+
+## HOW IT WORK?
+This package uses beautifulsoup4 and request, the result of this package is the latest earthquake update information in the Indonesian region
+
+## AUTHOR
+Muhammad Afrizal
---
+++ packages/indonesian.BMKG/indonesian.BMKG-0.3.tar.gz/indonesian.BMKG-0.3/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.3'
+DESCRIPTION = 'this is an package of latest indonesian earthquake'
+LONG_DESCRIPTION = 'this package uses beautifulsoup4 and request, the result of this package is the latest earthquake update information in the Indonesian region'
+# Setting up
+setup(
+    name="indonesian.BMKG",
+    version=VERSION,
+    author="Muhammad Afrizal",
+    author_email="muhamadafrizal5@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
