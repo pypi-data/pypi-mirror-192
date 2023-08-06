34554ebd46c34c95dd09af270dded748f4fc98f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:15 +0000

    Add package miniKeras/1.0.0/miniKeras-1.0.0.tar.gz

---
+++ packages/miniKeras/miniKeras-1.0.0.tar.gz/miniKeras-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="miniKeras",
+    version="1.0.0",
+    description="This is a scalar only supported mini-version of keras",
+    long_description="Helps you build neural networks with keras like api and works only on scalar values",
+    author="Tilak raj",
+    author_email="tilakraj7050@gmail.com",
+    packages=setuptools.find_packages(),
+)
