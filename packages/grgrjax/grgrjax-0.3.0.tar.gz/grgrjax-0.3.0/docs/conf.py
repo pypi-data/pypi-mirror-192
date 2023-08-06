b676cdab3fdc5fd0e8f7abfb1ecb61dfc1bbb513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:16 +0000

    Add package grgrjax/0.3.0/grgrjax-0.3.0.tar.gz

---
+++ packages/grgrjax/grgrjax-0.3.0.tar.gz/grgrjax-0.3.0/docs/conf.py
@@ -0,0 +1,92 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.'))
+# must be called AFTER the above:
+from grgrjax import __version__
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'grgrjax'
+copyright = '2022, Gregor Boehl'
+author = 'Gregor Boehl'
+
+# The full version, including alpha/beta/rc tags
+version = __version__
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+# extensions = [
+# "sphinx.ext.autodoc",
+# ]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "IPython.sphinxext.ipython_console_highlighting",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates", "**.ipynb_checkpoints"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = "sphinx_book_theme"
+html_copy_source = True
+html_show_sourcelink = True
+html_sourcelink_suffix = ""
+html_title = "grgrjax"
+html_static_path = ["_static"]
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/gboehl/grgrjax",
+    "repository_branch": "main",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "notebook_interface": "classic",
+    },
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_download_button": True,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+autoclass_content = "both"
+autodoc_member_order = "bysource"
+latex_use_parts = False
+
+
+def setup(app):
+    app.add_css_file('css/custom.css')
+
+
+master_doc = 'index'
---
+++ packages/grgrjax/grgrjax-0.3.0.tar.gz/grgrjax-0.3.0/grgrjax/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import jax
+import jax.numpy as jnp
+
+from .helpers import *
+from .newton import *
+
+__version__ = '0.3.0'
+__all__ = ["newton_jax_jit", "newton_jax", "callback_func", "jax_print",
+           "jvp_vmap", "vjp_vmap", "val_and_jacfwd", "val_and_jacrev", "amax"]
+
+
+def jax_print(w):
+    """Print in jax compiled functions. Wrapper around `jax.experimental.host_callback.id_print`.
+    """
+    return jax.experimental.host_callback.id_print(w)
+
+
+def amax(x):
+    """Return the maximum absolute value.
+    """
+    return jnp.abs(x).max()
