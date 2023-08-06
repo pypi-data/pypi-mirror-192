0d3a7215b45689bcd071f648e5718ec90290f673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:37 +0000

    Add package decentra-network-api/0.44.0/decentra_network_api-0.44.0.tar.gz

---
+++ packages/decentra-network-api/decentra_network_api-0.44.0.tar.gz/decentra_network_api-0.44.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from setuptools import setup
+
+setup(
+    name="decentra_network_api",
+    version="0.44.0",
+    description="""This is API mode installer for Decentra Network""",
+    url="https://docs.decentranetwork.net/",
+    author="Decentra Network Developers",
+    author_email="onur@decentranetwork.net",
+    license="MPL-2.0",
+    install_requires="""
+flask==2.0.0
+waitress==2.1.2
+werkzeug==2.0.3
+flask_cors==3.0.10
+""",
+    python_requires=">=3.8",
+    zip_safe=False,
+)
