0d184c3e6dd9e618e6b4c7e06b7949099f1198e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:30 +0000

    Add package curvlinops-for-pytorch/1.1.0/curvlinops_for_pytorch-1.1.0-py3-none-any.whl

---
+++ packages/curvlinops-for-pytorch/curvlinops_for_pytorch-1.1.0-py3-none-any.whl/test/utils.py
@@ -0,0 +1,27 @@
+"""Utility functions to test `curvlinops`."""
+
+from torch import cuda, device, rand, randint
+
+
+def get_available_devices():
+    """Return CPU and, if present, GPU device.
+
+    Returns:
+        [device]: Available devices for `torch`.
+    """
+    devices = [device("cpu")]
+
+    if cuda.is_available():
+        devices.append(device("cuda"))
+
+    return devices
+
+
+def classification_targets(size, num_classes):
+    """Create random targets for classes 0, ..., `num_classes - 1`."""
+    return randint(size=size, low=0, high=num_classes)
+
+
+def regression_targets(size):
+    """Create random targets for regression."""
+    return rand(*size)
