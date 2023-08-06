43b80384e758ff80b1b6d82d802cd4e8a045292c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:29 +0000

    Add package python-bed-lookup/0.1.0/python_bed_lookup-0.1.0.tar.gz

---
+++ packages/python-bed-lookup/python_bed_lookup-0.1.0.tar.gz/python_bed_lookup-0.1.0/setup.py
@@ -0,0 +1,56 @@
+"""
+Setup Script for bed_sqlite
+"""
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bed_lookup',
+    version='1.4',
+    description='Lookup a gene by coordinate from a bed',
+    long_description=long_description,
+    url='https://github.com/MikeDacre/python_bed_sqlite',
+    author='Michael Dacre',
+    author_email='mike.dacre@gmail.com',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+        'Operating System :: Unix',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+
+    keywords='bed',
+
+    install_requires=['Cython', 'numpy'],
+    ext_modules=cythonize(["bed_lookup/*.pyx"], language='c++'),
+    scripts=['bin/bed_location_lookup'],
+    packages=['bed_lookup']
+
+)
