34cd61a2d2841aa1003f2986761fa2cfed151d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:04 +0000

    Add package xtyping/0.6.2/xtyping-0.6.2.tar.gz

---
+++ packages/xtyping/xtyping-0.6.2.tar.gz/xtyping-0.6.2/xtyping/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "xtyping"
+__description__ = "xtyping = typing + typing_extensions"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.6.2"
