e528667661d8ba37f419be85cb837b099450658c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:39 +0000

    Add package pydantic-ome-ngff/0.1.1/pydantic_ome_ngff-0.1.1-py3-none-any.whl

---
+++ packages/pydantic-ome-ngff/pydantic_ome_ngff-0.1.1-py3-none-any.whl/pydantic_ome_ngff/base.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import TypeVar, Sequence, Dict
+
+
+class StrictBaseModel(BaseModel):
+    """
+    A pydantic basemodel that prevents extra fields.
+    """
+
+    class Config:
+        extra = "forbid"
+
+
+T = TypeVar("T")
+
+
+def census(values: Sequence[T]) -> Dict[T, int]:
+    """
+    Generate a dictionary of value : frequency pairs from a
+    sequence of (hashable) values.
+    """
+    return {k: values.count(k) for k in set(values)}
+
+
+def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
+    """
+    Format a warning so that it doesn't show source code
+    """
+    return f"{filename}:{lineno} {category.__name__}{message}\n"
