d8c6681d4066f34141b3be9b4a30e055775e5d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:09 +0000

    Add package dist-probabilty/1.1/dist_probabilty-1.1.tar.gz

---
+++ packages/dist-probabilty/dist_probabilty-1.1.tar.gz/dist_probabilty-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_probabilty',
+      version='1.1',
+      description='Gaussian and Binomail distributions',
+      packages=['dist_probabilty'],
+      author = "Carter Wright",
+      author_email = "random@gmail.com",
+      zip_safe=False)
