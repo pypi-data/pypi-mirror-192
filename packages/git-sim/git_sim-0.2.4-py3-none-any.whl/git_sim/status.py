ec4472ff04cba0062a02a423f48b452dc99d9fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package git-sim/0.2.4/git_sim-0.2.4-py3-none-any.whl

---
+++ packages/git-sim/git_sim-0.2.4-py3-none-any.whl/git_sim/status.py
@@ -0,0 +1,33 @@
+from git_sim.animations import handle_animations
+from git_sim.git_sim_base_command import GitSimBaseCommand
+from git_sim.settings import settings
+
+
+class Status(GitSimBaseCommand):
+    def __init__(self):
+        super().__init__()
+        try:
+            self.selected_branches.append(self.repo.active_branch.name)
+        except TypeError:
+            pass
+
+    def construct(self):
+        if not settings.stdout:
+            print(f"{settings.INFO_STRING } {type(self).__name__.lower()}")
+        self.show_intro()
+        self.get_commits()
+        self.parse_commits(self.commits[0])
+        self.recenter_frame()
+        self.scale_frame()
+        self.vsplit_frame()
+        self.setup_and_draw_zones()
+        self.fadeout()
+        self.show_outro()
+
+
+def status():
+    settings.hide_first_tag = True
+    settings.allow_no_commits = True
+
+    scene = Status()
+    handle_animations(scene=scene)
