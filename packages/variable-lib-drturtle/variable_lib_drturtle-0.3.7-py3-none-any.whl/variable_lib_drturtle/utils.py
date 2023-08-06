e71b29364ffa1833eb96c570a20c2132aae82356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:31 +0000

    Add package variable-lib-drturtle/0.3.7/variable_lib_drturtle-0.3.7-py3-none-any.whl

---
+++ packages/variable-lib-drturtle/variable_lib_drturtle-0.3.7-py3-none-any.whl/variable_lib_drturtle/utils.py
@@ -0,0 +1,31 @@
+G = 6.674 * (10**-11)  # constant G
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def Gravity(m1: float, m2: float, pos1: np.ndarray, pos2: np.ndarray):
+    """Calculate gravitational force between two objects"""
+
+    # setting up radius vector (sun to earth)
+    r = pos2 - pos1  # creates the radius vector of sun to earth
+    rmag = np.linalg.norm(r)  # normalizes the radius vecotr
+    rhat = r / rmag  # unit vector for radius
+
+    # define force object 2 exerts on object 1
+    return (G * m1 * m2) / (rmag**2) * rhat
+
+
+def sct(x, y, x_axis, y_axis, title, **kwargs):
+    plt.scatter(x, y, **kwargs)
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.title(title)
+    return plt
+
+
+def quiver(x, y, u, v, x_axis, y_axis, title, **kwargs):
+    plt.quiver(x, y, u, v**kwargs)
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.title(title)
+    return plt
