99121591fb3ac8116ca4ed024f860ec895df6eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:00 +0000

    Add package bitfount/0.5.57/bitfount-0.5.57-py3-none-any.whl

---
+++ packages/bitfount/bitfount-0.5.57-py3-none-any.whl/bitfount/exceptions.py
@@ -0,0 +1,19 @@
+"""Custom exceptions for the Bitfount codebase."""
+from typing import List
+
+__all__: List[str] = [
+    "BitfountEngineError",
+    "BitfountError",
+]
+
+
+class BitfountError(Exception):
+    """Base exception class that all others should inherit from."""
+
+    pass
+
+
+class BitfountEngineError(BitfountError):
+    """Exception for any issues relating to the backend engine."""
+
+    pass
