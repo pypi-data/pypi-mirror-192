59526748e8e4ce41227cd82901227edd0d25b6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:44 +0000

    Add package python-code-analyzer/1.0.14/python_code_analyzer-1.0.14.tar.gz

---
+++ packages/python-code-analyzer/python_code_analyzer-1.0.14.tar.gz/python_code_analyzer-1.0.14/code_analyzer/__init__.py
@@ -0,0 +1,27 @@
+"""
+Date created: 6/11/2022
+
+Purpose:
+
+Details:
+
+Description:
+
+Notes:
+
+IMPORTANT NOTES:
+
+Explanation:
+
+Tags:
+
+Contributors:
+    https://github.com/josephedradan
+
+Reference:
+
+"""
+from code_analyzer.code_analyzer import CodeAnalyzer
+
+# Pre-made instance
+code_analyzer = CodeAnalyzer()
