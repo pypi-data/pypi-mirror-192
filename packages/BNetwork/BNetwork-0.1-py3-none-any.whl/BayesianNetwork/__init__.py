d0a43c332982cb41d6056a2e3a891585a9917f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:51 +0000

    Add package BNetwork/0.1/BNetwork-0.1-py3-none-any.whl

---
+++ packages/BNetwork/BNetwork-0.1-py3-none-any.whl/BayesianNetwork/__init__.py
@@ -0,0 +1,8 @@
+from .bayesianNetwork import BNetwork
+
+bayesianNetwork = BNetwork()
+
+createBNetwork = bayesianNetwork.createBNetwork()
+insertProbability = bayesianNetwork.insertProbability()
+descriptionCheck = bayesianNetwork.descriptionCheck()
+showCompactRepresentation = bayesianNetwork.showCompactRepresentation()
