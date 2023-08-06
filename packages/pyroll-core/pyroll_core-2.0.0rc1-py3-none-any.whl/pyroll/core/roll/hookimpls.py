4487a0e56c4d0f37420686997fed2cfb10070457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package pyroll-core/2.0.0rc1/pyroll_core-2.0.0rc1-py3-none-any.whl

---
+++ packages/pyroll-core/pyroll_core-2.0.0rc1-py3-none-any.whl/pyroll/core/roll/hookimpls.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+from .roll import Roll
+from ..config import GROOVE_PADDING
+
+
+@Roll.working_radius
+def working_radius(self: Roll):
+    return self.nominal_radius - self.contour_line.centroid.y
+
+
+@Roll.min_radius
+def min_radius(self: Roll):
+    return self.max_radius - self.contour_line.bounds[3]
+
+
+@Roll.max_radius
+def max_radius(self: Roll):
+    return self.nominal_radius
+
+
+@Roll.roll_power
+def roll_power(self: Roll):
+    return self.roll_torque * self.rotational_frequency
+
+
+@Roll.surface_velocity
+def surface_velocity(self: Roll):
+    return self.rotational_frequency * self.nominal_radius
+
+
+@Roll.rotational_frequency
+def rotational_frequency(self: Roll):
+    return self.surface_velocity / self.nominal_radius
+
+
+@Roll.width
+def width(self: Roll):
+    return self.groove.width * (1 + 2 * GROOVE_PADDING)
+
+
+@Roll.contour_points
+def contour_points(self: Roll):
+    return self.groove.contour_points
+
+
+@Roll.surface_x
+def surface_x(self: Roll):
+    half_pi = np.pi / 2
+    return -self.min_radius * np.sin(np.linspace(-half_pi, half_pi, 101))
+
+
+@Roll.surface_z
+def surface_z(self: Roll):
+    return self.contour_points[:, 0]
+
+
+@Roll.surface_y
+def surface_y(self: Roll):
+    local_radii = self.max_radius - self.contour_points[:, 1]
+    return self.max_radius - np.sqrt(local_radii.reshape(-1, 1) ** 2 - self.surface_x ** 2)
