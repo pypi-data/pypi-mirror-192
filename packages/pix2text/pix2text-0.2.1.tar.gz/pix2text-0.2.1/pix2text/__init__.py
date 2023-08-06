171cd95a4a18116fd9e6e1e6dc6a63ff6609af72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:39 +0000

    Add package pix2text/0.2.1/pix2text-0.2.1.tar.gz

---
+++ packages/pix2text/pix2text-0.2.1.tar.gz/pix2text-0.2.1/pix2text/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Copyright (C) 2022, [Breezedeus](https://github.com/breezedeus).
+
+from .utils import read_img, set_logger
+from .render import render_html
+from .pix_to_text import Pix2Text
