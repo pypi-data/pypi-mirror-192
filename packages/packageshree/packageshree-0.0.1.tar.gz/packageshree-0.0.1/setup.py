338da0646ab9810b28e8ffd4343d4dc2472322ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:35 +0000

    Add package packageshree/0.0.1/packageshree-0.0.1.tar.gz

---
+++ packages/packageshree/packageshree-0.0.1.tar.gz/packageshree-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+setup(
+    name = "packageshree",
+    version = "0.0.1",
+    author = "Shridhar",
+    author_email = "shridhar5697@gmail.com",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    
+    packages=[]
+)
