8602db13374565aee1f901a5c71f54bc03f858d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:58 +0000

    Add package lalsuite/7.11.1.dev20230217/lalsuite-7.11.1.dev20230217-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/lalsuite/lalsuite-7.11.1.dev20230217-cp310-cp310-macosx_10_15_x86_64.whl/lalframe/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .lalframe import *
+
+__version__ = "2.0.3.1"
+
+## \addtogroup lalframe_python
+"""This package provides Python wrappings and extensions to LALFrame"""
