4c40daa737e4b110dc1b37e5c5042e49b458e6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:27 +0000

    Add package tower.py/0.0.0/tower.py-0.0.0.tar.gz

---
+++ packages/tower.py/tower.py-0.0.0.tar.gz/tower.py-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from setuptools import setup
+
+setup(
+    name="tower.py",
+    version="0.0.0",
+    author="Fabien Taxil",
+    author_email="lixaft@gmail.com",
+    url="https://github.com/lixaft/tower.py",
+)
