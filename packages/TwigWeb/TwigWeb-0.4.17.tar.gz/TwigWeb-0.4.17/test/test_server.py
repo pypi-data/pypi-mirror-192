0f93451e0823c51b1c273317306132b31193e69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:12 +0000

    Add package TwigWeb/0.4.17/TwigWeb-0.4.17.tar.gz

---
+++ packages/TwigWeb/TwigWeb-0.4.17.tar.gz/TwigWeb-0.4.17/test/test_server.py
@@ -0,0 +1,21 @@
+import unittest
+from src.TwigWeb.backend import Server, ContentType
+from src.TwigWeb.backend.response import Response
+
+class ServerTest(unittest.TestCase):
+    def test_server(self):
+        app = Server("", debug=True)
+
+        @app.route("")
+        def index(headers):
+            return Response("test", ContentType.html)
+
+        @app.route("page/[num]")
+        def index(headers, params):
+            return Response(f"num: {params['num']}", ContentType.html)
+        
+        @app.route("page")
+        def index(headers):
+            return Response(f"page", ContentType.html)
+        
+        app.run()
