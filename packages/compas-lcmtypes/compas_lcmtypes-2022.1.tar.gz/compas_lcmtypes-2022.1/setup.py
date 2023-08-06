412f03b42e475e292779c448b8dc15ad300b4d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:00 +0000

    Add package compas-lcmtypes/2022.1/compas_lcmtypes-2022.1.tar.gz

---
+++ packages/compas-lcmtypes/compas_lcmtypes-2022.1.tar.gz/compas_lcmtypes-2022.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['compas_lcmtypes',
+ 'compas_lcmtypes.bot_core',
+ 'compas_lcmtypes.navlcm',
+ 'compas_lcmtypes.oi',
+ 'compas_lcmtypes.senlcm']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'compas-lcmtypes',
+    'version': '2022.1',
+    'description': 'CoMPAS LCM types',
+    'long_description': 'None',
+    'author': 'Kevin Barnard',
+    'author_email': 'kbarnard@mbari.org',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
