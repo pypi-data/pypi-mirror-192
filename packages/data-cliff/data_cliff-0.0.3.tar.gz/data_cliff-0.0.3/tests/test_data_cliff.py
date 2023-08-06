b0458915d39594ecab2bbe5cdb598439794f169e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:22 +0000

    Add package data-cliff/0.0.3/data_cliff-0.0.3.tar.gz

---
+++ packages/data-cliff/data_cliff-0.0.3.tar.gz/data_cliff-0.0.3/tests/test_data_cliff.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `data_cliff` package."""
+
+import pytest
+
+from data_cliff import data_cliff
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_():
+    assert data_cliff is not None
