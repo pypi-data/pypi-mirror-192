996821f451117c9b1f479e85ad30f9f6e1ca17ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:39 +0000

    Add package h-transport-materials/0.10/h_transport_materials-0.10-py3-none-any.whl

---
+++ packages/h-transport-materials/h_transport_materials-0.10-py3-none-any.whl/tests/test_system.py
@@ -0,0 +1,43 @@
+import h_transport_materials as htm
+
+import pint
+
+
+def test_that_groups_are_not_empty():
+    for group in [htm.diffusivities, htm.solubilities, htm.recombination_coeffs]:
+        assert len(group) > 0
+
+
+def test_all_solubilities_are_solubility():
+    for sol in htm.solubilities:
+        assert isinstance(sol, htm.Solubility)
+
+
+def test_all_diffusivities_are_diffusivity():
+    for diff in htm.diffusivities:
+        assert isinstance(diff, htm.Diffusivity)
+
+
+def test_all_recomb_are_recombinationcoeff():
+    for diff in htm.recombination_coeffs:
+        assert isinstance(diff, htm.RecombinationCoeff)
+
+
+def test_all_dissociations_are_dissociationncoeff():
+    for diff in htm.dissociation_coeffs:
+        assert isinstance(diff, htm.DissociationCoeff)
+
+
+def test_all_properties_have_required_attributes():
+    for prop in htm.database:
+        assert prop.material
+        assert isinstance(prop.pre_exp, pint.Quantity)
+        assert isinstance(prop.act_energy, pint.Quantity)
+        assert prop.isotope
+
+
+def test_all_properties_have_references():
+    for prop in htm.database:
+        assert prop.author
+        assert prop.year
+        assert prop.source
