d1c0feceaae36c9241423e89cd0ad6cfa298b3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:49 +0000

    Add package cyberpay-utils-lib/0.0.6/cyberpay-utils-lib-0.0.6.tar.gz

---
+++ packages/cyberpay-utils-lib/cyberpay-utils-lib-0.0.6.tar.gz/cyberpay-utils-lib-0.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cyberpay-utils-lib",
+    version="0.0.6",
+    description="Cyberpay utils library",
+    author="MADI SPACE",
+    license="MIT",
+    packages=find_packages(where="."),
+    package_data={
+        "cyberpay_utils.billing.proto": ["*.proto", "*.pyi"],
+    },
+    zip_safe=False,
+)
