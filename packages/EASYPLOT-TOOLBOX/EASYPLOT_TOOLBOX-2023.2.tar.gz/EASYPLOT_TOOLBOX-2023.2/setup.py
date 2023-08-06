2dc81fbcfac085b6a8ced23106d2649bb8b329f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:00 +0000

    Add package EASYPLOT-TOOLBOX/2023.2/EASYPLOT_TOOLBOX-2023.2.tar.gz

---
+++ packages/EASYPLOT-TOOLBOX/EASYPLOT_TOOLBOX-2023.2.tar.gz/EASYPLOT_TOOLBOX-2023.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    	name = 'EASYPLOT_TOOLBOX',
+    	version = '2023.2',
+		url = 'https://wmpjrufg.github.io/EASYPLOT_TOOLBOX/',
+    	license = 'Apache License 2.0',
+    	author_email = 'wanderlei_junior@ufcat.edu.br',
+    	packages = ['EASYPLOT_TOOLBOX'],
+    	install_requires = ["Matplotlib", "Seaborn"]
+    )
+
