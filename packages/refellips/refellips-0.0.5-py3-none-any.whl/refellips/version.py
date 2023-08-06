7e8dc6252181678c663d0fa7c77112acc1f696ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:59 +0000

    Add package refellips/0.0.5/refellips-0.0.5-py3-none-any.whl

---
+++ packages/refellips/refellips-0.0.5-py3-none-any.whl/refellips/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM refellips SETUP.PY
+short_version = '0.0.5'
+version = '0.0.5'
+full_version = '0.0.5'
+git_revision = 'e56f86ed3826a499e63f1f484fe1d9065cac621b'
+release = True
+if not release:
+    version = full_version
