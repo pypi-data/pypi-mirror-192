abc8c5404c706a163860689f997fcbf2fceefe96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:40 +0000

    Add package OSFramework/1.0.0/OSFramework-1.0.0.tar.gz

---
+++ packages/OSFramework/OSFramework-1.0.0.tar.gz/OSFramework-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OSFramework',
+    version='1.0.0',
+    description='Now its possible make OSes',
+    author='JonDev2023',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
