2d353b31cf2d2171131a7583c5aad42fd90e5c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:57 +0000

    Add package sdfspu/0.2.2/sdfspu-0.2.2.tar.gz

---
+++ packages/sdfspu/sdfspu-0.2.2.tar.gz/sdfspu-0.2.2/tests/test_sdf_text.py
@@ -0,0 +1,35 @@
+from sdfspu.sdf_text import (
+    randstring,
+    split_name,
+    obsc_email,
+    path_is_url,
+    dict_to_json_file,
+)
+
+
+def test_randstring():
+    pass
+
+
+def test_randstring():
+    assert isinstance(randstring(), str)
+    assert len(randstring(k_=20)) == 20
+    assert len(randstring(k_=120)) == 120
+    assert " " not in randstring()
+
+
+def test_split_name():
+    assert split_name("ok simple") == ("ok", "simple")
+    assert split_name("solo") == ("", "solo")
+    assert split_name("three part name") == ("three part", "name")
+    assert split_name("name with-hyphen") == ("name", "with-hyphen")
+    assert split_name("") == ("", "")
+
+
+def test_obsc_email():
+    assert obsc_email("something@example.com") == "som...@e..."
+
+
+def test_path_is_url():
+    assert path_is_url("https://github.com/soundmaking/sdfspu")
+    assert not path_is_url("/home/pi/gitclones/sdfspu/tests/test_sdf_text.py")
