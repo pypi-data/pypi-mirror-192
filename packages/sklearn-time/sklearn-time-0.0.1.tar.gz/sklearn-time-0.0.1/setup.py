edb51e34a6855da21c5d715dc05e929205548129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:33 +0000

    Add package sklearn-time/0.0.1/sklearn-time-0.0.1.tar.gz

---
+++ packages/sklearn-time/sklearn-time-0.0.1.tar.gz/sklearn-time-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = ["patrickzib"]
+
+from distutils.core import setup
+
+setup(name='sklearn-time',
+      version='0.0.1',
+      description='Python Scikit-Time',
+      author='patrickzib',
+      author_email='patrick.schaefer@hu-berlin.de',
+      url='https://github.com/patrickzib/dictionary',
+     )
