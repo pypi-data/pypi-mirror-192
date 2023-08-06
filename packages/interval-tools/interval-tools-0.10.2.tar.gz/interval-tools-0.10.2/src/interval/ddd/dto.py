375bfbb08c56aea1f4e0e52890008b8d7e8f9b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:33 +0000

    Add package interval-tools/0.10.2/interval-tools-0.10.2.tar.gz

---
+++ packages/interval-tools/interval-tools-0.10.2.tar.gz/interval-tools-0.10.2/src/interval/ddd/dto.py
@@ -0,0 +1,14 @@
+"""
+interval.ddd.dto
+~~~~~~~~~~~~~~~~
+
+This module provides DDD data transfer object base classes.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class UseCaseDTO:
+    """用例返回的数据传输对象"""
+    pass
