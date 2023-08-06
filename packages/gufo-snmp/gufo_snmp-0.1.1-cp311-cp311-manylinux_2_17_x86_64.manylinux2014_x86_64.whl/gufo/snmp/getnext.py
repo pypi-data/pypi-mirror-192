e5f383992f14db8edc7825a4e170c2249d702800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:45 +0000

    Add package gufo-snmp/0.1.1/gufo_snmp-0.1.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/gufo-snmp/gufo_snmp-0.1.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/gufo/snmp/getnext.py
@@ -0,0 +1,74 @@
+# ---------------------------------------------------------------------
+# Gufo SNMP: GetNextIter
+# ---------------------------------------------------------------------
+# Copyright (C) 2023, Gufo Labs
+# See LICENSE.md for details
+# ---------------------------------------------------------------------
+
+"""GetNextIter iterator."""
+
+
+# Python modules
+import asyncio
+from typing import Tuple
+
+# Gufo Labs Modules
+from ._fast import GetNextIter as _Iter
+from ._fast import SnmpClientSocket
+from .typing import ValueType
+
+
+class GetNextIter(object):
+    """Wrap the series of the GetNext requests.
+
+    Args:
+        oid: Base oid.
+        timeout: Request timeout.
+    """
+
+    def __init__(
+        self: "GetNextIter", sock: SnmpClientSocket, oid: str, timeout: float
+    ) -> None:
+        self._sock = sock
+        self._ctx = _Iter(oid)
+        self._fd = sock.get_fd()
+        self._timeout = timeout
+
+    def __aiter__(self: "GetNextIter") -> "GetNextIter":
+        """Return asynchronous iterator."""
+        return self
+
+    async def __anext__(self: "GetNextIter") -> Tuple[str, ValueType]:
+        """Get next value."""
+
+        async def get_response() -> Tuple[str, ValueType]:
+            while True:
+                # Wait until data will be available
+                r_ev = asyncio.Event()
+                loop.add_reader(self._fd, r_ev.set)
+                try:
+                    await r_ev.wait()
+                finally:
+                    loop.remove_reader(self._fd)
+                # Read data or get BlockingIOError
+                # if no valid data available.
+                try:
+                    return self._sock.recv_getresponse_next(self._ctx)
+                except BlockingIOError:
+                    continue
+
+        loop = asyncio.get_running_loop()
+        # Wait for socket became writable
+        w_ev = asyncio.Event()
+        loop.add_writer(self._fd, w_ev.set)
+        try:
+            await w_ev.wait()
+        finally:
+            loop.remove_writer(self._fd)
+        # Send request
+        self._sock.send_getnext(self._ctx)
+        # Await response or timeout
+        try:
+            return await asyncio.wait_for(get_response(), self._timeout)
+        except asyncio.TimeoutError as e:
+            raise TimeoutError from e  # Remap the error
