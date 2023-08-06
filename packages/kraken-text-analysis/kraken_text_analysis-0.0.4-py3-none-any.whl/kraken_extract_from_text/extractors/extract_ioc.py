5b45453769466006fa683127ad79a3a578ffca75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:26 +0000

    Add package kraken-text-analysis/0.0.4/kraken_text_analysis-0.0.4-py3-none-any.whl

---
+++ packages/kraken-text-analysis/kraken_text_analysis-0.0.4-py3-none-any.whl/kraken_extract_from_text/extractors/extract_ioc.py
@@ -0,0 +1,38 @@
+from ioc_finder import find_iocs
+
+
+
+def get(content):
+
+    iocs = find_iocs(content)    
+
+    records = []
+    
+    # Get domains
+    for i in iocs.get('domains', []):
+        record = {
+            '@type': 'schema:WebSite',
+            'schema:url': 'https://' + str(i)
+        }
+        records.append(record)
+
+    # Get urls
+    for i in iocs.get('urls', []):
+        record = {
+            '@type': 'schema:WebPage',
+            'schema:url': i
+        }
+        records.append(record)
+
+
+    # Get emails
+    for i in iocs.get('email_addresses', []):
+        record = {
+            '@type': 'schema:contactPoint',
+            'schema:email': i
+        }
+        records.append(record)
+
+        
+    return records
+
