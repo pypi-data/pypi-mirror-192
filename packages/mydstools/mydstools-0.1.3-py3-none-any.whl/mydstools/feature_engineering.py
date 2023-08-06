e87f8475206ecc7bc828da8e4fa15eaf102c5b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:36 +0000

    Add package mydstools/0.1.3/mydstools-0.1.3-py3-none-any.whl

---
+++ packages/mydstools/mydstools-0.1.3-py3-none-any.whl/mydstools/feature_engineering.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def mean_encode(df: pd.DataFrame, groupby: str, target: str):
+    """
+    Groups a Pandas DataFrame via a given column and return
+    the mean of the target variable for that grouping.
+
+    Args:
+        df (pd.DataFrame): Pandas DataFrame.
+        groupby (str): Column to group by.
+        target (str): Target variable column.
+
+    Returns:
+        pandas.Dataframe: Mean for the target variable across the group.
+    """
+    mean_encoded = df.groupby(groupby)[target].mean()
+    return df[groupby].map(mean_encoded)
