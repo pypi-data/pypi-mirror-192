862c3d78ef0fb223f9450d297a81d96c3b252f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:43 +0000

    Add package sime-library/0.2/sime_library-0.2.tar.gz

---
+++ packages/sime-library/sime_library-0.2.tar.gz/sime_library-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='sime_library',
+    version='0.2',
+    description='A simple library for basic math operations',
+    py_modules=['sime_library'],
+    license='MIT',
+    author='Tudjewuj',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/sime_library',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
