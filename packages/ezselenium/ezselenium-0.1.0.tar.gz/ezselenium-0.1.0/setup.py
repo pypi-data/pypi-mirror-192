147801e944adaf26860e629475aca4e5720907ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:32 +0000

    Add package ezselenium/0.1.0/ezselenium-0.1.0.tar.gz

---
+++ packages/ezselenium/ezselenium-0.1.0.tar.gz/ezselenium-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='ezselenium',
+    version='0.1.0',
+    description='Common selenium functions',
+    author='Ben Brunyee',
+    author_email='brunyeeb@gmail.com',
+    url='https://github.com/benbrunyee/ezselenium',
+    packages=['ezselenium'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
